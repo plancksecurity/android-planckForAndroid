b471d91c3a18d580e93bd917f6752c28820fbcb8
Dietz Proepper <dietz.github@rotfl.franken.de>
2016-01-08 12:35:49 +0100

    pEp message construction should be fine so far.

--- k9mail/src/main/java/com/fsck/k9/pEp/PEpMessageBuilder.java
+++ k9mail/src/main/java/com/fsck/k9/pEp/PEpMessageBuilder.java
@@ -77,7 +77,7 @@ class PEpMessageBuilder {
 
                 Blob blob = new Blob();
                 blob.filename = MimeUtility.getHeaderParameter(mbp.getContentType(), "filename");     // TODO: test wether this works
-                if(blob.filename == null) blob.filename = "empty";
+                 if(blob.filename == null) blob.filename = "empty";
                 blob.mime_type = mbp.getMimeType();
                 blob.data = part.getData();
                 attachments.add(blob);
