2c98fdacc7d1e83d6089ce1f937ff508fbc34572
Hussein Kasem <huss@pep-project.org>
2016-03-30 10:14:49 +0200

    fixed status icon override

--- k9mail/src/main/java/com/fsck/k9/fragment/MessageListFragment.java
+++ k9mail/src/main/java/com/fsck/k9/fragment/MessageListFragment.java
@@ -1921,6 +1921,11 @@ public class MessageListFragment extends Fragment implements OnItemClickListener
                 holder.from.setTypeface(Typeface.create(holder.from.getTypeface(), maybeBoldTypeface));
                 if (mSenderAboveSubject) {
                     if (hasAttachments) holder.attachment.setVisibility(View.VISIBLE);
+                    holder.from.setCompoundDrawablesWithIntrinsicBounds(
+                            statusHolder, // left
+                            null, // top
+                            null, // right
+                            null); // bottom
 
                     holder.from.setText(displayName);
                 } else {
@@ -1931,6 +1936,11 @@ public class MessageListFragment extends Fragment implements OnItemClickListener
             if (holder.subject != null ) {
                 if (!mSenderAboveSubject) {
                     if (hasAttachments) holder.attachment.setVisibility(View.VISIBLE);
+                    holder.subject.setCompoundDrawablesWithIntrinsicBounds(
+                            statusHolder, // left
+                            null, // top
+                            null, // right
+                            null); // bottom
                 }
 
                 holder.subject.setTypeface(Typeface.create(holder.subject.getTypeface(), maybeBoldTypeface));
