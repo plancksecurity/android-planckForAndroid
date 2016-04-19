cb82d9a5d088a28951346e5648b37af1eda89dd4
Vincent Breitmoser <valodim@mugenguild.com>
2016-04-19 15:38:53 +0200

    messageview: refactor OpenPgpResultAnnotation into immutable CryptoResultAnnotation

--- k9mail/src/main/java/com/fsck/k9/view/MessageHeader.java
+++ k9mail/src/main/java/com/fsck/k9/view/MessageHeader.java
@@ -56,6 +56,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
+
 public class MessageHeader extends LinearLayout implements OnClickListener, OnLongClickListener {
     private Context mContext;
     private TextView mFromView;
