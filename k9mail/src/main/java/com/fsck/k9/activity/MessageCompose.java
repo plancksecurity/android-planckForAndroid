43a37394fec3ca617d976962a519e932f436418d
Philip Whitehouse <philip@whiuk.com>
2016-04-19 22:42:27 +0100

    Move IdentityAdapter and Action creation static methods and classes out of MessageCompose

--- k9mail/src/main/java/com/fsck/k9/activity/MessageCompose.java
+++ k9mail/src/main/java/com/fsck/k9/activity/MessageCompose.java
@@ -117,6 +117,7 @@ public class MessageCompose extends K9Activity implements OnClickListener,
 
     public static final String EXTRA_ACCOUNT = "account";
     public static final String EXTRA_MESSAGE_REFERENCE = "message_reference";
+
     public static final String EXTRA_PEP_COLOR = "pEpColor";
     public static final String EXTRA_MESSAGE_DECRYPTION_RESULT  = "message_decryption_result";
     public static final String EXTRA_MESSAGE_BODY  = "messageBody";
