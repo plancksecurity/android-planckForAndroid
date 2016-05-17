3134cf2f59c3760ed9ca0148d2a0fffe70e790f3
Vincent Breitmoser <valodim@mugenguild.com>
2016-05-17 12:35:02 +0200

    compose: small refactorings for readability in RecipientPresenter

--- k9mail/src/main/java/com/fsck/k9/activity/compose/RecipientPresenter.java
+++ k9mail/src/main/java/com/fsck/k9/activity/compose/RecipientPresenter.java
@@ -28,7 +28,6 @@ import com.fsck.k9.mail.Address;
 import com.fsck.k9.mail.Flag;
 import com.fsck.k9.mail.Message;
 import com.fsck.k9.mail.Message.RecipientType;
-import com.fsck.k9.mail.MessagingException;
 import com.fsck.k9.mailstore.LocalMessage;
 import com.fsck.k9.message.PgpMessageBuilder;
 import com.fsck.k9.pEp.PEpProvider;
