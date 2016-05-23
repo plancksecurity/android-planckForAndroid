jeffreyj100/k-9

compose: save inline mode setting as flag for drafts

k9mail/src/main/java/com/fsck/k9/activity/compose/RecipientPresenter.java
@@ -28,6 +28,7 @@ import com.fsck.k9.helper.MailTo;
 import com.fsck.k9.helper.ReplyToParser;
 import com.fsck.k9.helper.Utility;
 import com.fsck.k9.mail.Address;
+import com.fsck.k9.mail.Flag;
 import com.fsck.k9.mail.Message;
 import com.fsck.k9.mail.Message.RecipientType;
 import com.fsck.k9.mail.MessagingException;
@@ -210,6 +211,11 @@ public class RecipientPresenter implements PermissionPingCallback {
     }
 
     public void initFromDraftMessage(LocalMessage message) {
+        initRecipientsFromDraftMessage(message);
+        initOpenPgpCompatFromDraftMessage(message);
+    }
+
+    private void initRecipientsFromDraftMessage(LocalMessage message) {
         try {
             addToAddresses(message.getRecipients(RecipientType.TO));
 
@@ -222,6 +228,10 @@ public class RecipientPresenter implements PermissionPingCallback {
             // can't happen, we know the recipient types exist
             throw new AssertionError(e);
         }
+    }
+
+    private void initOpenPgpCompatFromDraftMessage(LocalMessage message) {
+        cryptoEnableCompat = message.isSet(Flag.X_DRAFT_OPENPGP_INLINE);
     }
 
     void addToAddresses(Address... toAddresses) {

plancksecurity/android-planckForAndroid

k9mail/src/main/java/com/fsck/k9/activity/compose/RecipientPresenter.java
@@ -23,6 +23,7 @@ import com.fsck.k9.helper.MailTo;
 import com.fsck.k9.helper.ReplyToParser;
 import com.fsck.k9.helper.Utility;
 import com.fsck.k9.mail.Address;
+import com.fsck.k9.mail.Flag;
 import com.fsck.k9.mail.Message;
 import com.fsck.k9.mail.Message.RecipientType;
 import com.fsck.k9.mail.MessagingException;
@@ -213,6 +214,11 @@ public class RecipientPresenter implements PermissionPingCallback {
     }
 
     public void initFromDraftMessage(LocalMessage message) {
+        initRecipientsFromDraftMessage(message);
+        initPgpInlineFromDraftMessage(message);
+    }
+
+    private void initRecipientsFromDraftMessage(LocalMessage message) {
         try {
             addToAddresses(message.getRecipients(RecipientType.TO));
 
@@ -225,6 +231,10 @@ public class RecipientPresenter implements PermissionPingCallback {
             // can't happen, we know the recipient types exist
             throw new AssertionError(e);
         }
+    }
+
+    private void initPgpInlineFromDraftMessage(LocalMessage message) {
+        cryptoEnablePgpInline = message.isSet(Flag.X_DRAFT_OPENPGP_INLINE);
     }
 
     void addToAddresses(Address... toAddresses) {