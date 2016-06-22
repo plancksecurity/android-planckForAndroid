ffbd75497e1e11c12b7673e4f0290ae74876c37f
Hussein Kasem <huss@pep-project.org>
2016-06-22 12:25:29 +0200

    import key dialog finished, solved not showing on accounts list, applied design, and moved from K9Activity to K9ActivityCommon, deleting the key locally and remote, only diplaying the dialog if is not the current key.

--- k9mail/src/main/java/com/fsck/k9/activity/K9ActivityCommon.java
+++ k9mail/src/main/java/com/fsck/k9/activity/K9ActivityCommon.java
@@ -1,19 +1,26 @@
 package com.fsck.k9.activity;
 
-import java.util.Locale;
-
 import android.app.Activity;
-import android.content.Context;
+import android.app.AlertDialog;
+import android.content.*;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.text.TextUtils;
-import android.view.GestureDetector;
-import android.view.MotionEvent;
-
+import android.util.Log;
+import android.view.*;
+import android.widget.TextView;
+import android.widget.Toast;
 import com.fsck.k9.K9;
+import com.fsck.k9.R;
 import com.fsck.k9.activity.misc.SwipeGestureDetector;
 import com.fsck.k9.activity.misc.SwipeGestureDetector.OnSwipeGestureListener;
+import com.fsck.k9.mail.Address;
+import com.fsck.k9.pEp.PEpProvider;
+import com.fsck.k9.pEp.PEpUtils;
+import org.pEp.jniadapter.Identity;
+
+import java.util.Locale;
 
 
 /**
@@ -68,11 +75,18 @@ public class K9ActivityCommon {
     private Activity mActivity;
     private GestureDetector mGestureDetector;
 
+    private BroadcastReceiver receiver;
+    private IntentFilter filter;
 
     private K9ActivityCommon(Activity activity) {
         mActivity = activity;
         setLanguage(mActivity, K9.getK9Language());
         mActivity.setTheme(K9.getK9ThemeResourceId());
+        filter = new IntentFilter();
+        filter.addAction("PRIVATE_KEY");
+        filter.setPriority(1);
+        receiver = new PrivateKeyReceiver();
+        mActivity.registerReceiver(receiver, filter);
     }
 
     /**
@@ -111,4 +125,69 @@ public class K9ActivityCommon {
         mGestureDetector = new GestureDetector(mActivity,
                 new SwipeGestureDetector(mActivity, listener));
     }
+
+    private static void displayKeyImportAlert(final Context context, final String fpr, final String address, final String username, String from)
+    {
+        ContextThemeWrapper ctw = new ContextThemeWrapper(context, R.style.TextViewCustomFont);
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(ctw);
+        LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.import_key_dialog, null);
+        String formatedFpr = PEpUtils.formatFpr(fpr);
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(formatedFpr.substring(0, formatedFpr.length()/2-1))
+                .append("\n")
+                .append(formatedFpr.substring(formatedFpr.length()/2, formatedFpr.length()/2+ PEpProvider.HALF_FINGERPRINT_LENGTH));
+        ((TextView) dialogView.findViewById(R.id.tvFpr)).setText(stringBuilder.toString());
+        ((TextView) dialogView.findViewById(R.id.tvAddress)).setText(String.format(context.getString(R.string.user_address_format), address));
+        ((TextView) dialogView.findViewById(R.id.tvUsername)).setText(String.format(context.getString(R.string.user_name_format), username));
+        ((TextView) dialogView.findViewById(R.id.tvFrom)).setText(String.format(context.getString(R.string.from_format), from));
+
+        alertDialogBuilder.setView(dialogView)
+//                .setTitle("Secret key replace")
+//                .setMessage(detail)
+                .setCancelable(false)
+                .setPositiveButton("Accept", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(context, "Key replaced", Toast.LENGTH_LONG).show();
+                        Identity id = PEpUtils.createIdentity(new Address(address, username), context);
+                        id.fpr = fpr;
+                        ((K9) context.getApplicationContext()).getpEpProvider().myself(id);
+                    }
+                }).setNegativeButton("Reject", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(context, "Key rejected", Toast.LENGTH_LONG).show();
+            }
+        });
+        AlertDialog dialog = alertDialogBuilder.create();
+//        TextView textView = (TextView) dialog.findViewById(android.R.id.message);
+//        Typeface face=Typeface.createFromAsset(getAssets(),"fonts/CourierNew.ttf");
+//        textView.setTypeface(face);
+        dialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
+        dialog.show();
+    }
+
+    public static class PrivateKeyReceiver extends BroadcastReceiver {
+        public PrivateKeyReceiver() {
+        }
+
+        @Override
+        public void onReceive(final Context context, Intent intent) {
+            Log.w("dec", "onReceive: ");
+            abortBroadcast();
+            displayKeyImportAlert(
+                    context,
+                    intent.getExtras().getString(PEpProvider.PEP_PRIVATE_KEY_FPR),
+                    intent.getExtras().getString(PEpProvider.PEP_PRIVATE_KEY_ADDRESS),
+                    intent.getExtras().getString(PEpProvider.PEP_PRIVATE_KEY_USERNAME),
+                    intent.getExtras().getString(PEpProvider.PEP_PRIVATE_KEY_FROM));
+        }
+    }
+
+
+    public void onDestroy() {
+        mActivity.unregisterReceiver(receiver);
+    }
+
 }
