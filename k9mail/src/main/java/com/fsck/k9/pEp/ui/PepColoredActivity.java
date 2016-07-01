a711b4ac5bfcba512cee384243cd27df982b7fc2
Hussein Kasem <huss@pep-project.org>
2016-07-01 13:30:21 +0200

    Pepstatus and trustwords action bar color consistency.

---
+++ k9mail/src/main/java/com/fsck/k9/pEp/ui/PepColoredActivity.java
@@ -0,0 +1,68 @@
+/*
+Created by Helm  01/07/16.
+*/
+
+
+package com.fsck.k9.pEp.ui;
+
+import android.app.ActionBar;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import com.fsck.k9.K9;
+import com.fsck.k9.activity.K9Activity;
+import com.fsck.k9.pEp.PEpProvider;
+import com.fsck.k9.pEp.PEpUtils;
+import com.fsck.k9.pEp.PePUIArtefactCache;
+import org.pEp.jniadapter.Color;
+
+public class PepColoredActivity extends K9Activity {
+    public static final String CURRENT_COLOR = "current_color";
+    private Color m_pEpColor = Color.pEpRatingB0rken;
+    PePUIArtefactCache uiCache;
+    private PEpProvider pEp;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    protected void colorActionBar() {
+        ActionBar actionBar = getActionBar() ;
+        if (actionBar != null) {
+            PEpUtils.colorActionBar(uiCache, actionBar, m_pEpColor);
+        }
+    }
+
+    protected void loadPepColor() {
+        final Intent intent = getIntent();
+        String colorString;
+        if (intent.hasExtra(CURRENT_COLOR)) {
+            colorString = intent.getStringExtra(CURRENT_COLOR);
+            Log.d(K9.LOG_TAG, "Got color:" + colorString);
+            m_pEpColor = Color.valueOf(colorString);
+        } else {
+            throw new RuntimeException("Cannot retrieve pEpColor");
+        }
+    }
+
+    public void setpEpColor(Color pEpColor) {
+        this.m_pEpColor = pEpColor;
+    }
+    public Color getpEpColor() {
+        return m_pEpColor;
+    }
+
+    protected void initPep() {
+        uiCache = PePUIArtefactCache.getInstance(getApplicationContext());
+        pEp = ((K9) getApplication()).getpEpProvider();
+    }
+
+    public PEpProvider getpEp() {
+        return pEp;
+    }
+
+    public PePUIArtefactCache getUiCache() {
+        return uiCache;
+    }
+}
