benkibbey/k-9

debug: add debug operation to clear a message locally

k9mail/src/main/java/com/fsck/k9/mailstore/LocalMessage.java
@@ -1,5 +1,6 @@
 package com.fsck.k9.mailstore;
 
+
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.Date;
@@ -304,6 +305,39 @@ public class LocalMessage extends MimeMessage {
                     } catch (MessagingException e) {
                         throw new WrappedException(e);
                     }
+
+                    return null;
+                }
+            });
+        } catch (WrappedException e) {
+            throw (MessagingException) e.getCause();
+        }
+
+        localStore.notifyChange();
+    }
+
+    public void debugClearLocalData() throws MessagingException {
+        if (!K9.DEBUG) {
+            throw new AssertionError("method must only be used in debug build!");
+        }
+
+        try {
+            localStore.database.execute(true, new DbCallback<Void>() {
+                @Override
+                public Void doDbWork(final SQLiteDatabase db) throws WrappedException, MessagingException {
+                    ContentValues cv = new ContentValues();
+                    cv.putNull("message_part_id");
+
+                    db.update("messages", cv, "id = ?", new String[] { Long.toString(mId) });
+
+                    try {
+                        ((LocalFolder) mFolder).deleteMessagePartsAndDataFromDisk(messagePartId);
+                    } catch (MessagingException e) {
+                        throw new WrappedException(e);
+                    }
+
+                    setFlag(Flag.X_DOWNLOADED_FULL, false);
+                    setFlag(Flag.X_DOWNLOADED_PARTIAL, false);
 
                     return null;
                 }

plancksecurity/android-planckForAndroid

k9mail/src/main/java/com/fsck/k9/mailstore/LocalMessage.java
@@ -1,6 +1,9 @@
 package com.fsck.k9.mailstore;
 
 
+import java.io.IOException;
+import java.io.OutputStream;
+
 import java.io.ByteArrayInputStream;
 import java.util.Date;
 
@@ -297,6 +300,39 @@ public class LocalMessage extends MimeMessage {
                     } catch (MessagingException e) {
                         throw new WrappedException(e);
                     }
+
+                    return null;
+                }
+            });
+        } catch (WrappedException e) {
+            throw (MessagingException) e.getCause();
+        }
+
+        localStore.notifyChange();
+    }
+
+    public void debugClearLocalData() throws MessagingException {
+        if (!BuildConfig.DEBUG) {
+            throw new AssertionError("method must only be used in debug build!");
+        }
+
+        try {
+            localStore.database.execute(true, new DbCallback<Void>() {
+                @Override
+                public Void doDbWork(final SQLiteDatabase db) throws WrappedException, MessagingException {
+                    ContentValues cv = new ContentValues();
+                    cv.putNull("message_part_id");
+
+                    db.update("messages", cv, "id = ?", new String[] { Long.toString(mId) });
+
+                    try {
+                        ((LocalFolder) mFolder).deleteMessagePartsAndDataFromDisk(messagePartId);
+                    } catch (MessagingException e) {
+                        throw new WrappedException(e);
+                    }
+
+                    setFlag(Flag.X_DOWNLOADED_FULL, false);
+                    setFlag(Flag.X_DOWNLOADED_PARTIAL, false);
 
                     return null;
                 }