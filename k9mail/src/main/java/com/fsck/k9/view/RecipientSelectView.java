d7f2a3105cc944e7ebbfc30684fc6829ce83c657
Vincent Breitmoser <look@my.amazin.horse>
2016-08-09 13:35:24 +0200

    compose: small fixes for alternate recipient adapter
    this slightly alters behavior when a non-contact address is picked, to
    make sure a crypto status is shown in the alternate dropdown.

--- k9mail/src/main/java/com/fsck/k9/view/RecipientSelectView.java
+++ k9mail/src/main/java/com/fsck/k9/view/RecipientSelectView.java
@@ -338,8 +338,7 @@ public class RecipientSelectView extends TokenCompleteTextView<Recipient> implem
                 if (contactLookupUri != null) {
                     return new RecipientLoader(getContext(), cryptoProvider, contactLookupUri, true);
                 } else {
-                    String address = alternatesPopupRecipient.address.getAddress();
-                    return new RecipientLoader(getContext(), cryptoProvider, address);
+                    return new RecipientLoader(getContext(), cryptoProvider, alternatesPopupRecipient.address);
                 }
             }
         }
@@ -455,7 +454,7 @@ public class RecipientSelectView extends TokenCompleteTextView<Recipient> implem
 
         RecipientTokenSpan[] recipientSpans = text.getSpans(0, text.length(), RecipientTokenSpan.class);
         for (RecipientTokenSpan recipientSpan : recipientSpans) {
-            if (recipientSpan.getToken() == currentRecipient) {
+            if (recipientSpan.getToken().equals(currentRecipient)) {
                 return recipientSpan.view;
             }
         }
