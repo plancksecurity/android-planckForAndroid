c16a2e80557c09a7220d09bee92fa364950871ea
Hussein Kasem <huss@pep-project.org>
2016-09-02 17:03:32 +0200

    K9-111 added global setting to warn when forwarded message is weaker than original (formerly encrypted)

--- k9mail/src/main/java/com/fsck/k9/preferences/GlobalSettings.java
+++ k9mail/src/main/java/com/fsck/k9/preferences/GlobalSettings.java
@@ -277,6 +277,9 @@ public class GlobalSettings {
         s.put("pep_subject_unprotected", Settings.versions(
                 new V(46, new BooleanSetting(true))
         ));
+        s.put("pep_forward_warning", Settings.versions(
+                new V(47, new BooleanSetting(false))
+        ));
             ));
         s.put("pgpInlineDialogCounter", Settings.versions(
                 new V(43, new IntegerRangeSetting(0, Integer.MAX_VALUE, 0))
