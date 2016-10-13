ea077548a29db34b769b62e55db01bf54f63cf98
Arturet <arturo@pep-project.org>
2016-10-13 16:44:31 +0200

    Materialized Welcome Screen
    Added logo and version

--- k9mail/src/main/java/com/fsck/k9/activity/setup/WelcomeMessage.java
+++ k9mail/src/main/java/com/fsck/k9/activity/setup/WelcomeMessage.java
@@ -2,6 +2,8 @@ package com.fsck.k9.activity.setup;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
 import android.view.View;
@@ -12,11 +14,20 @@ import com.fsck.k9.R;
 import com.fsck.k9.activity.Accounts;
 import com.fsck.k9.activity.K9Activity;
 import com.fsck.k9.helper.HtmlConverter;
+import com.fsck.k9.pEp.PEpUtils;
+import com.fsck.k9.pEp.PePUIArtefactCache;
+
+import org.pEp.jniadapter.Rating;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Displays a welcome message when no accounts have been created yet.
  */
 public class WelcomeMessage extends K9Activity implements OnClickListener{
+
+    @Bind(R.id.welcome_app_version) TextView appDescription;
 
     public static void showWelcomeMessage(Context context) {
         Intent intent = new Intent(context, WelcomeMessage.class);
@@ -27,8 +38,18 @@ public class WelcomeMessage extends K9Activity implements OnClickListener{
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         setContentView(R.layout.welcome_message);
+        ButterKnife.bind(this);
 
         initializeToolbar(false, R.string.welcome_message_title);
+        PEpUtils.colorToolbar(PePUIArtefactCache.getInstance(getApplicationContext()), getToolbar(), Rating.pEpRatingTrustedAndAnonymized);
+
+        try {
+            PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            String description = getString(R.string.app_k9_pep_name) + pInfo.versionName + "\n" + getString(R.string.pep_app_description);
+            appDescription.setText(description);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
 
         TextView welcome = (TextView) findViewById(R.id.welcome_message);
         welcome.setText(HtmlConverter.htmlToSpanned(getString(R.string.accounts_welcome)));
