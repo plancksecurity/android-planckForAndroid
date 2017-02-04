fc4435eb9e720208a156fdf287b00f85ff332bb9
cketti <cketti@gmail.com>
2017-02-04 22:50:06 +0100

    P4A-492 Avoid exception in MigrationTo43 during database tests
    (cherry picked from commit a52e0bd)

--- k9mail/src/main/java/com/fsck/k9/mailstore/LocalStore.java
+++ k9mail/src/main/java/com/fsck/k9/mailstore/LocalStore.java
@@ -272,6 +272,10 @@ public class LocalStore extends Store implements Serializable {
         database.switchProvider(newStorageProviderId);
     }
 
+    Context getContext() {
+        return context;
+    }
+
     protected Account getAccount() {
         return mAccount;
     }
