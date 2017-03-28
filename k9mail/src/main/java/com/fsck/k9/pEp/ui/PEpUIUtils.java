26402e29050e65a56e1fddb6c14a9ad48e591ae6
Arturo Jiménez <arturo@pep-project.org>
2017-03-28 11:24:10 +0200

    P4A-402 StringIndexOutOfBounds in activity ComponentInfo{pep.android.k9.materials/com.fsck.k9.activity.MessageList}

--- k9mail/src/main/java/com/fsck/k9/pEp/ui/PEpUIUtils.java
+++ k9mail/src/main/java/com/fsck/k9/pEp/ui/PEpUIUtils.java
@@ -3,6 +3,9 @@ package com.fsck.k9.pEp.ui;
 public class PEpUIUtils {
 
     public static String firstLetterOf(String word) {
-        return word.substring(0,1);
+        if (word != null && !word.isEmpty()) {
+            return word.substring(0,1);
+        }
+        return "?";
     }
 }
