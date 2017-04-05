0e973a63875c0040bb6f11c7e51842e80a39b50b
Arturo Jiménez <arturo@pep-project.org>
2017-04-05 11:40:29 +0200

    WIP FIXME

--- k9mail/src/main/java/com/fsck/k9/pEp/UIUtils.java
+++ k9mail/src/main/java/com/fsck/k9/pEp/UIUtils.java
@@ -6,6 +6,10 @@ import java.util.Locale;
 
 public class UIUtils {
 
+    public static final String UTF_8_Q = "=?utf-8?Q?";
+    public static final String UTF_8_B = "=?utf-8?B?";
+    public static final String UTF_ENDING = "?=";
+
     @NonNull
     public static CharSequence[] prettifyLanguages(CharSequence[] pEpLocales) {
         CharSequence[] pEpLanguages = new CharSequence[pEpLocales.length];
@@ -23,5 +27,15 @@ public class UIUtils {
     @NonNull
     private static String uppercaseFirstCharacter(String locale) {
         return locale.substring(0, 1).toUpperCase() + locale.substring(1);
+    }
+
+    public static String prettifyAddressName(String addressName) {
+        if (addressName != null && addressName.contains(UTF_8_Q)) {
+            addressName = addressName.replace(UTF_8_Q, "").replace(UTF_ENDING, "");
+        }
+        if (addressName != null && addressName.contains(UTF_8_B)) {
+            addressName = addressName.replace("UTF_8_B", "").replace(UTF_ENDING, "");
+        }
+        return addressName;
     }
 }
