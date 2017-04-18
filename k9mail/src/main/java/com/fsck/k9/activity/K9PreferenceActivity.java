3fe18b0b6512225b0e10566317722f259d7f5ddd
Arturo Jiménez <arturo@pep-project.org>
2017-04-18 10:47:54 +0200

    p4a-395 pEp for Android - Material - White titles in light grey title are not readable

--- k9mail/src/main/java/com/fsck/k9/activity/K9PreferenceActivity.java
+++ k9mail/src/main/java/com/fsck/k9/activity/K9PreferenceActivity.java
@@ -193,11 +193,13 @@ public class K9PreferenceActivity extends PreferenceActivity {
             setUpNestedScreen((PreferenceScreen) preference);
         }
 
-        return false;
+        return true;
     }
 
     public void setUpNestedScreen(PreferenceScreen preferenceScreen) {
         final Dialog dialog = preferenceScreen.getDialog();
+
+        setTheme(K9.getK9ThemeResourceId());
 
         Toolbar bar;
 
@@ -215,40 +217,22 @@ public class K9PreferenceActivity extends PreferenceActivity {
             }
             content.setPadding(0, height, 0, 0);
             root.addView(bar, 0); // insert at top
-        } else if (Build.VERSION.SDK_INT < Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
-            ViewGroup root = (ViewGroup) dialog.findViewById(android.R.id.content);
-            ListView content = (ListView) root.getChildAt(0);
-
-            root.removeAllViews();
-
-            bar = (Toolbar) LayoutInflater.from(this).inflate(R.layout.toolbar, root, false);
-
-            int height;
-            TypedValue tv = new TypedValue();
-            if (getTheme().resolveAttribute(R.attr.actionBarSize, tv, true)) {
-                height = TypedValue.complexToDimensionPixelSize(tv.data, getResources().getDisplayMetrics());
-            }else{
-                height = bar.getHeight();
-            }
-
-            content.setPadding(0, height, 0, 0);
-
-            root.addView(content);
-            root.addView(bar);
         } else {
             LinearLayout root = (LinearLayout) dialog.findViewById(android.R.id.list).getParent().getParent();
             bar = (Toolbar) LayoutInflater.from(this).inflate(R.layout.toolbar, root, false);
             root.addView(bar, 0); // insert at top
         }
+        toolbar = bar;
 
-        bar.setTitle(preferenceScreen.getTitle());
-        bar.setTitleTextColor(getResources().getColor(R.color.white));
-
-        bar.setNavigationOnClickListener(new View.OnClickListener() {
+        toolbar.setTitle(preferenceScreen.getTitle());
+        toolbar.setTitleTextColor(getResources().getColor(R.color.white));
+        toolbar.setBackgroundColor(getResources().getColor(R.color.pep_green));
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 dialog.dismiss();
             }
         });
+        setStatusBarPepColor();
     }
 }
