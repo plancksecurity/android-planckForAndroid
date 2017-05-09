b3b4b07f8568012aa1e03cca011f7753e6877b7a
Hussein Kasem <huss@pep-project.org>
2017-05-09 16:54:35 +0200

    P4A-369 fix transparent background

--- k9mail/src/main/java/com/fsck/k9/pEp/ui/keysync/PEpAddDevice.java
+++ k9mail/src/main/java/com/fsck/k9/pEp/ui/keysync/PEpAddDevice.java
@@ -4,12 +4,15 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -108,6 +111,8 @@ public class PEpAddDevice extends PepActivity implements AddDeviceView {
                 presenter.initialize(this, getpEp(), partnerIdentity, accounts);
             }
         }
+
+        setupFloatingWindow();
     }
 
     @Override
@@ -325,7 +330,7 @@ public class PEpAddDevice extends PepActivity implements AddDeviceView {
 
     @OnClick(R.id.add_device_background)
     public void onClickOutside() {
-        finish();
+        presenter.cancelHandshake();
     }
 
     public class DismissKeysyncDialogReceiver extends BroadcastReceiver {
@@ -336,5 +341,9 @@ public class PEpAddDevice extends PepActivity implements AddDeviceView {
         public void onReceive(final Context context, Intent intent) {
             PEpAddDevice.this.finish();
         }
+    }
+
+    protected void setupFloatingWindow() {
+        getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
     }
 }
