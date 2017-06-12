3d1e4d034c7f49034a3e00dabd12520f3b067e26
Arturo Jiménez <arturo@pep-project.org>
2017-06-12 08:03:05 +0200

    P4A-494 No suitable parent found from the given view
    This is due to the view passed is null, probably due to the destruction of the context while a background operation is still loading

--- k9mail/src/main/java/com/fsck/k9/pEp/ui/tools/FeedbackTools.java
+++ k9mail/src/main/java/com/fsck/k9/pEp/ui/tools/FeedbackTools.java
@@ -6,10 +6,14 @@ import android.view.View;
 public class FeedbackTools {
 
     public static void showShortFeedback(View rootView, String message) {
-        Snackbar.make(rootView, message, Snackbar.LENGTH_SHORT).show();
+        if (rootView != null) {
+            Snackbar.make(rootView, message, Snackbar.LENGTH_SHORT).show();
+        }
     }
 
     public static void showLongFeedback(View rootView, String message) {
-        Snackbar.make(rootView, message, Snackbar.LENGTH_LONG).show();
+        if (rootView != null) {
+            Snackbar.make(rootView, message, Snackbar.LENGTH_LONG).show();
+        }
     }
 }
