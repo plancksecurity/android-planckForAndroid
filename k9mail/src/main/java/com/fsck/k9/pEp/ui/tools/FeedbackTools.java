0206a90e61eb21ebd97bc7d98061fe3ddfe95c16
Arturo Jiménez <arturo@pep-project.org>
2017-06-15 10:42:40 +0200

    P4A -494 Added Timber as a fallback case

--- k9mail/src/main/java/com/fsck/k9/pEp/ui/tools/FeedbackTools.java
+++ k9mail/src/main/java/com/fsck/k9/pEp/ui/tools/FeedbackTools.java
@@ -3,17 +3,23 @@ package com.fsck.k9.pEp.ui.tools;
 import android.support.design.widget.Snackbar;
 import android.view.View;
 
+import timber.log.Timber;
+
 public class FeedbackTools {
 
     public static void showShortFeedback(View rootView, String message) {
         if (rootView != null) {
             Snackbar.make(rootView, message, Snackbar.LENGTH_SHORT).show();
+        } else {
+            Timber.e(message);
         }
     }
 
     public static void showLongFeedback(View rootView, String message) {
         if (rootView != null) {
             Snackbar.make(rootView, message, Snackbar.LENGTH_LONG).show();
+        } else {
+            Timber.e(message);
         }
     }
 }
