e0bd1c0a92d4fd7fd6d3cee35f2a07f791afe3f1
Arturo Jiménez <arturo@pep-project.org>
2017-09-18 13:40:15 +0200

    P4A-577 Tests for PEpExtraKeysPresenter

--- k9mail/src/test/java/com/fsck/k9/pEp/ui/keys/PepExtraKeysPresenterTest.java
+++ k9mail/src/test/java/com/fsck/k9/pEp/ui/keys/PepExtraKeysPresenterTest.java
@@ -1,8 +1,45 @@
-import static org.junit.Assert.*;
+package com.fsck.k9.pEp.ui.keys;
 
-/**
- * Created by arturo on 9/18/17.
- */
+import com.fsck.k9.pEp.PEpProvider;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.verify;
+
 public class PepExtraKeysPresenterTest {
 
+    private PepExtraKeysPresenter presenter;
+    @Mock private PepExtraKeysView view;
+    @Mock PEpProvider provider;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        presenter = new PepExtraKeysPresenter();
+    }
+
+    @Test
+    public void shouldGetMasterKeysInfoWhenSetupMasterKeys() throws Exception {
+        presenter.initialize(view, provider, keys());
+
+        verify(provider).getMasterKeysInfo();
+    }
+
+    @Test
+    public void shouldShowKeysInfoWhenSetupMasterKeys() throws Exception {
+        presenter.initialize(view, provider, keys());
+
+        verify(view).showKeys(any());
+    }
+
+    private List<String> keys() {
+        return Collections.emptyList();
+    }
 }
