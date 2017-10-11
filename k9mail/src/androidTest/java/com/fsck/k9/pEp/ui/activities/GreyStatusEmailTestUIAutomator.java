696ec5c8d151e89252767c345f4a8a6b06592bf9
juan <juan@pep-security.net>
2017-10-11 14:28:55 +0200

    P4A-582 Add static final values

--- k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/GreyStatusEmailTestUIAutomator.java
+++ k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/GreyStatusEmailTestUIAutomator.java
@@ -2,9 +2,8 @@ package com.fsck.k9.pEp.ui.activities;
 
 import android.support.test.filters.SdkSuppress;
 import android.support.test.runner.AndroidJUnit4;
-
+import android.support.test.uiautomator.UiDevice;
 import org.junit.runner.RunWith;
-
 /**
  * Created by juan on 11/10/17.
  */
@@ -12,4 +11,12 @@ import org.junit.runner.RunWith;
 @RunWith(AndroidJUnit4.class)
 @SdkSuppress(minSdkVersion = 18)
 public class GreyStatusEmailTestUIAutomator {
+
+    private static final String BASIC_SAMPLE_PACKAGE = "com.fsck.k9.pEp.ui.activities";
+    private static final int TIME = 2000;
+    private static final String DESCRIPTION = "tester one";
+    private static final String USER_NAME = "testerJ";
+    private static final String EMAIL = "newemail@mail.es";
+    private static final int LAUNCH_TIMEOUT = 5000;
+    private UiDevice mDevice;
 }
