cc5f267d8dacf44a1192f15a598258be49f61ac5
juan <juan@pep-security.net>
2017-10-11 14:39:55 +0200

    P4A-582 Add @Before

--- k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/GreyStatusEmailTestUIAutomator.java
+++ k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/GreyStatusEmailTestUIAutomator.java
@@ -1,13 +1,25 @@
 package com.fsck.k9.pEp.ui.activities;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.support.annotation.NonNull;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.filters.SdkSuppress;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.By;
 import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.Until;
 
 import com.fsck.k9.BuildConfig;
 
+import org.junit.Before;
 import org.junit.runner.RunWith;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
 /**
  * Created by juan on 11/10/17.
  */
@@ -24,6 +36,27 @@ public class GreyStatusEmailTestUIAutomator {
     private static final int LAUNCH_TIMEOUT = 5000;
     private UiDevice mDevice;
 
+    @Before
+    public void startMainActivityFromHomeScreen() {
+        mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+        mDevice.pressHome();
+        final String launcherPackage = getLauncherPackageName();
+        assertThat(launcherPackage, notNullValue());
+        mDevice.wait(Until.hasObject(By.pkg(launcherPackage).depth(0)), LAUNCH_TIMEOUT);
+        Context context = InstrumentationRegistry.getContext();
+        final Intent intent = context.getPackageManager().getLaunchIntentForPackage(BASIC_SAMPLE_PACKAGE);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        context.startActivity(intent);
+        mDevice.wait(Until.hasObject(By.pkg(BASIC_SAMPLE_PACKAGE).depth(0)), LAUNCH_TIMEOUT);
+    }
+
+    private String getLauncherPackageName() {
+        final Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        PackageManager pm = InstrumentationRegistry.getContext().getPackageManager();
+        ResolveInfo resolveInfo = pm.resolveActivity(intent, PackageManager.MATCH_DEFAULT_ONLY);
+        return resolveInfo.activityInfo.packageName;
+    }
 
     @NonNull
     private String getEmail() {return BuildConfig.PEP_TEST_EMAIL_ADDRESS;}
