abba38372bc9db1520058199a7eb10ad4efe2596
juan <juan@pep-security.net>
2017-10-11 14:27:48 +0200

    P4A-582 Add dependencies in Module

--- k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/GreyStatusEmailTestUIAutomator.java
+++ k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/GreyStatusEmailTestUIAutomator.java
@@ -2,9 +2,8 @@ package com.fsck.k9.pEp.ui.activities;
 
 import android.support.test.filters.SdkSuppress;
 import android.support.test.runner.AndroidJUnit4;
-
+import android.support.test.uiautomator.UiDevice;
 import org.junit.runner.RunWith;
-
 /**
  * Created by juan on 11/10/17.
  */
@@ -12,4 +11,6 @@ import org.junit.runner.RunWith;
 @RunWith(AndroidJUnit4.class)
 @SdkSuppress(minSdkVersion = 18)
 public class GreyStatusEmailTestUIAutomator {
+
+    private UiDevice mDevice;
 }
