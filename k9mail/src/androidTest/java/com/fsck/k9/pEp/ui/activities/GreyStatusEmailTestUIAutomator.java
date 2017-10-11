53c565065a4ef373a4a9e90a44504959ac9928ac
juan <juan@pep-security.net>
2017-10-11 14:16:52 +0200

    P4A-582 Create UI Automator file

---
+++ k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/GreyStatusEmailTestUIAutomator.java
@@ -0,0 +1,15 @@
+package com.fsck.k9.pEp.ui.activities;
+
+import android.support.test.filters.SdkSuppress;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.runner.RunWith;
+
+/**
+ * Created by juan on 11/10/17.
+ */
+
+@RunWith(AndroidJUnit4.class)
+@SdkSuppress(minSdkVersion = 18)
+public class GreyStatusEmailTestUIAutomator {
+}
