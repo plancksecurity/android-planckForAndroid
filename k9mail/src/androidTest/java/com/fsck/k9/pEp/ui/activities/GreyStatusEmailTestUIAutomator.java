bd52648babed2979f39fde153178859b06ee39ff
juan <juan@pep-security.net>
2017-10-17 10:56:46 +0200

    P4A-582 Select Accept from dialog

--- k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/GreyStatusEmailTestUIAutomator.java
+++ k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/GreyStatusEmailTestUIAutomator.java
@@ -185,15 +185,19 @@ public class GreyStatusEmailTestUIAutomator {
         doWait(TIME);
         longClick("accounts_list");
         selectRemoveAccount();
-        doWait(TIME);
-        BySelector selector = By.clazz("android.widget.Button");
-        mDevice.findObjects(selector).get(1).click();
+        selectAcceptButton();
     }
 
     private void selectRemoveAccount(){
         doWait(TIME);
         BySelector selector = By.clazz("android.widget.TextView");
         mDevice.findObjects(selector).get(5).click();
+    }
+
+    private void selectAcceptButton(){
+        doWait(TIME);
+        BySelector selector = By.clazz("android.widget.Button");
+        mDevice.findObjects(selector).get(1).click();
     }
 
     private void longClick(String view){
