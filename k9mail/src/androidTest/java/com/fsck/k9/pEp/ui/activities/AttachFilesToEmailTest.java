f0ce40804f002dd0fc5e4d05c867f4755e0bffa5
juan <juan@pep-security.net>
2017-11-17 14:20:55 +0100

    P4A-582 Attach files to Email Test completed

---
+++ k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/AttachFilesToEmailTest.java
@@ -0,0 +1,57 @@
+package com.fsck.k9.pEp.ui.activities;
+
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.UiDevice;
+
+import com.fsck.k9.R;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
+public class AttachFilesToEmailTest {
+
+    private static final String DESCRIPTION = "tester one";
+    private static final String USER_NAME = "testerJ";
+    private static final String EMAIL = "juan@miau.xyz";
+
+    private UiDevice device;
+    private TestUtils testUtils;
+
+    @Rule
+    public IntentsTestRule<SplashActivity> splashActivityTestRule = new IntentsTestRule<>(SplashActivity.class);
+
+    @Before
+    public void startMainActivityFromHomeScreen() {
+        device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+        testUtils = new TestUtils(device);
+        testUtils.startActivity();
+    }
+
+    @Test 
+    public void attachFilesToEmail() {
+        testUtils.increaseTimeoutWait();
+        accountConfiguration();
+        testUtils.accountDescription(DESCRIPTION, USER_NAME);
+        testUtils.accountListSelect(DESCRIPTION);
+        testUtils.composseMessageButton();
+        testUtils.fillEmail(EMAIL, "Subject", "Message");
+        testUtils.sendEmail();
+        testUtils.removeAccount();
+    }
+
+    private void accountConfiguration(){
+        onView(withId(R.id.skip)).perform(click());
+        testUtils.newEmailAccount();
+        //testUtils.gmailAccount();
+    }
+
+}
