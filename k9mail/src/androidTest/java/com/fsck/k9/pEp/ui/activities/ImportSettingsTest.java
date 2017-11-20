fc09ff4177e4fa7c8ec11c4b9b505bd49b8701ad
juan <juan@pep-security.net>
2017-11-20 15:46:42 +0100

    P4A-568 Move methods to TestUtils

--- k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/ImportSettingsTest.java
+++ k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/ImportSettingsTest.java
@@ -2,10 +2,8 @@ package com.fsck.k9.pEp.ui.activities;
 
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
-import android.support.test.uiautomator.By;
-import android.support.test.uiautomator.BySelector;
 import android.support.test.uiautomator.UiDevice;
-import android.support.test.uiautomator.Until;
+import android.test.suitebuilder.annotation.LargeTest;
 
 import com.fsck.k9.R;
 
@@ -19,15 +17,12 @@ import static android.support.test.espresso.Espresso.openActionBarOverflowOrOpti
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
-
 @RunWith(AndroidJUnit4.class)
 public class ImportSettingsTest {
 
-    private static final String PACKAGE = "pep.android.k9";
     private static final String DESCRIPTION = "tester one";
     private static final String USER_NAME = "testerJ";
 
-    private UiDevice device;
     private TestUtils testUtils;
 
     @Before
@@ -50,58 +45,15 @@ public class ImportSettingsTest {
             testUtils.newEmailAccount();
         }
         testUtils.accountDescription(DESCRIPTION, USER_NAME);
-        device.waitForIdle();
+        testUtils.doWait();
         openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
-        device.waitForIdle();
-        selectSettingsImportExport();
-        device.waitForIdle();
-        waitForMenu();
-        selectSettingsImport();
-        getActivityInstance();
+        testUtils.doWait();
+        testUtils.selectSettingsFromMenu(R.string.import_export_action);
+        testUtils.doWait();
+        testUtils.doWaitForResource(R.string.settings_import);
+        testUtils.selectSettingsFromMenu(R.string.settings_import);
+        testUtils.getActivityInstance();
         testUtils.removeAccount("accounts_list");
-        device.waitForIdle();
-    }
-
-    private void waitForMenu(){
-        device.wait(Until.hasObject(By.desc(InstrumentationRegistry.getTargetContext().getResources().getString(R.string.settings_import))), 1);
-    }
-
-    private void selectSettingsImportExport(){
-        BySelector selector = By.clazz("android.widget.TextView");
-        int size = device.findObjects(selector).size();
-        for (int i = 0; i < size; i++) {
-            if (device.findObjects(selector).get(i).getText().equals(InstrumentationRegistry.getTargetContext().getResources().getString(R.string.import_export_action))){
-                device.findObjects(selector).get(i).click();
-                break;
-            }
-        }
-    }
-
-    private void selectSettingsImport(){
-        BySelector selector = By.clazz("android.widget.TextView");
-        int size = device.findObjects(selector).size();
-        for (int i = 0; i < size; i++) {
-            if (device.findObjects(selector).get(i).getText().equals(InstrumentationRegistry.getTargetContext().getResources().getString(R.string.settings_import))){
-                device.findObjects(selector).get(i).click();
-                break;
-            }
-        }}
-
-
-    private void getActivityInstance(){
-        waitForExternalApp();
-        goBackToOriginalApp();
-    }
-
-    private void waitForExternalApp(){
-        while (PACKAGE.equals(device.getCurrentPackageName())){
-            device.waitForIdle();
-        }
-    }
-
-    private void goBackToOriginalApp(){
-        while (!PACKAGE.equals(device.getCurrentPackageName())){
-            device.pressBack();
-        }
+        testUtils.doWait();
     }
 }
