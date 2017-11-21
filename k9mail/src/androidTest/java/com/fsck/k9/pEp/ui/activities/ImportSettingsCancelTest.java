1ece2e861723cc67c887e5cd860abe5e695937a4
juan <juan@pep-security.net>
2017-11-21 10:38:46 +0100

    P4A-568 Test problem fixed

--- k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/ImportSettingsCancelTest.java
+++ k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/ImportSettingsCancelTest.java
@@ -45,6 +45,8 @@ public class ImportSettingsCancelTest {
         testUtils.createAccount(isGmail);
         testUtils.pressBack();
         testUtils.doWait();
+        testUtils.pressBack();
+        testUtils.doWait();
         testUtils.openOptionsMenu();
         testUtils.doWait();
         testUtils.selectFromMenu(R.string.import_export_action);
