e4932f0927b1940eb60c85a4b2f34392f1c373ae
juan <juan@pep-security.net>
2017-11-23 16:29:58 +0100

    P4A-633 ImportSettingsTest & TestUtils changes

---
+++ k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/ImportSettingsTest.java
@@ -0,0 +1,72 @@
+package com.fsck.k9.pEp.ui.activities;
+
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.ViewAssertion;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.uiautomator.UiDevice;
+
+import com.fsck.k9.R;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+
+public class ImportSettingsTest {
+
+    private static final String DESCRIPTION = "tester one";
+    private static final String USER_NAME = "testerJ";
+
+    private TestUtils testUtils;
+
+    @Rule
+    public IntentsTestRule<SplashActivity> splashActivityTestRule = new IntentsTestRule<>(SplashActivity.class);
+
+    @Before
+    public void startMainActivityFromHomeScreen() {
+        testUtils = new TestUtils(UiDevice.getInstance(InstrumentationRegistry.getInstrumentation()));
+        testUtils.startActivity();
+    }
+
+    @Test
+    public void importSettings() {
+        importSettingsTest(false);
+    }
+
+    private void importSettingsTest(boolean isGmail) {
+        testUtils.increaseTimeoutWait();
+        testUtils.externalAppRespondWithFile(R.raw.settings);
+        onView(withId(R.id.skip)).perform(click());
+        if (isGmail) {
+            testUtils.gmailAccount();
+        } else {
+            testUtils.newEmailAccount();
+        }
+        testUtils.accountDescription(DESCRIPTION, USER_NAME);
+        testUtils.pressBack();
+        testUtils.doWait();
+        testUtils.openOptionsMenu();
+        testUtils.doWait();
+        testUtils.selectFromMenu(R.string.import_export_action);
+        testUtils.doWait();
+        testUtils.doWaitForResource(R.string.settings_import);
+        testUtils.selectFromMenu(R.string.settings_import);
+        testUtils.doWait();
+        testUtils.doWaitForAlertDialog(splashActivityTestRule, R.string.settings_import_selection);
+        testUtils.selectAcceptButton();
+        testUtils.doWaitForAlertDialog(splashActivityTestRule, R.string.settings_import_success_header);
+        testUtils.selectAcceptButton();
+        testUtils.doWaitForAlertDialog(splashActivityTestRule, R.string.settings_import_activate_account_header);
+        testUtils.selectCancelButton();
+        testUtils.doWait();
+        testUtils.removeLastAccount();
+        testUtils.doWait();
+        testUtils.removeLastAccount();
+    }
+}
