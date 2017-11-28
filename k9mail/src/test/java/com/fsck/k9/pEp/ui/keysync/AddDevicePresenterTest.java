c5016791cdedb2310c106a40efd70ce82b848215
Arturo Jiménez <arturo@pep-project.org>
2017-11-28 11:54:26 +0100

    Fix Add Device Presenter tests

--- k9mail/src/test/java/com/fsck/k9/pEp/ui/keysync/AddDevicePresenterTest.java
+++ k9mail/src/test/java/com/fsck/k9/pEp/ui/keysync/AddDevicePresenterTest.java
@@ -1,8 +1,7 @@
 package com.fsck.k9.pEp.ui.keysync;
 
-import android.test.mock.MockApplication;
-
 import com.fsck.k9.Account;
+import com.fsck.k9.K9RobolectricTestRunner;
 import com.fsck.k9.pEp.PEpProvider;
 
 import org.junit.Before;
@@ -11,26 +10,24 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.pEp.jniadapter.Identity;
-import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
 import java.util.Collections;
 import java.util.List;
 
 import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-@RunWith(RobolectricTestRunner.class)
-@Config(manifest = "src/main/AndroidManifest.xml", sdk = 21, application = MockApplication.class)
+@RunWith(K9RobolectricTestRunner.class)
+@Config(manifest = "src/main/AndroidManifest.xml", sdk = 21)
 public class AddDevicePresenterTest {
     public static final String PARTNER_USER_ID = "partner_user_id";
     public static final String PARTNER_ADDRESS = "partner_address";
     private AddDevicePresenter addDevicePresenter;
     @Mock AddDeviceView view;
-    //@Mock PEpProvider pEpProvider;
+    @Mock PEpProvider pEpProvider;
 
     @Before
     public void setUp() throws Exception {
@@ -39,29 +36,57 @@ public class AddDevicePresenterTest {
     }
 
     @Test
-    public void shouldNotFilterIdentitiesIfNoAccountsRemoved() throws Exception {
-        //when(pEpProvider.updateIdentity(any(Identity.class))).thenReturn(identity());
-        //addDevicePresenter.initialize(view, pEpProvider, PARTNER_USER_ID, PARTNER_ADDRESS, accounts());
+    public void shouldAcceptHandshakeOnEngineWhenAccepting() throws Exception {
+        addDevicePresenter.initialize(view, pEpProvider, identity(), accounts());
+        addDevicePresenter.acceptHandshake();
 
-        List<Identity> identities = identities();
-        /*doAnswer(invocation -> {
-            PEpProvider.ResultCallback<List<Identity>> callback =
-                    (PEpProvider.ResultCallback<List<Identity>>) invocation.getArguments()[0];
-            callback.onLoaded(identities);
-            return null;
-        }).when(pEpProvider).loadOwnIdentities(any(PEpProvider.ResultCallback.class));
-*/
-        verify(view).showIdentities(identities);
+        verify(pEpProvider).acceptHandshake(any(Identity.class));
+    }
+
+    @Test
+    public void shouldCancelHandshakeOnEngineWhenCancelling() throws Exception {
+        addDevicePresenter.initialize(view, pEpProvider, identity(), accounts());
+        addDevicePresenter.acceptHandshake();
+
+        verify(pEpProvider).acceptHandshake(any(Identity.class));
+    }
+
+    @Test
+    public void shouldRejectHandshakeOnEngineWhenRejecting() throws Exception {
+        addDevicePresenter.initialize(view, pEpProvider, identity(), accounts());
+        addDevicePresenter.rejectHandshake();
+
+        verify(pEpProvider).rejectHandshake(any(Identity.class));
+    }
+
+    @Test
+    public void shouldCloseViewWhenAccepting() throws Exception {
+        addDevicePresenter.initialize(view, pEpProvider, identity(), accounts());
+        addDevicePresenter.acceptHandshake();
+
+        verify(view).close();
+    }
+
+    @Test
+    public void shouldCloseViewWhenRejecting() throws Exception {
+        addDevicePresenter.initialize(view, pEpProvider, identity(), accounts());
+        addDevicePresenter.rejectHandshake();
+
+        verify(view).close();
+    }
+
+    @Test
+    public void shouldCloseViewWhenCancelling() throws Exception {
+        addDevicePresenter.initialize(view, pEpProvider, identity(), accounts());
+        addDevicePresenter.cancelHandshake();
+
+        verify(view).goBack();
     }
 
     private List<Account> accounts() {
         Account account = mock(Account.class);
         when(account.getEmail()).thenReturn(PARTNER_ADDRESS);
         return Collections.singletonList(account);
-    }
-
-    private List<Identity> identities() {
-        return Collections.singletonList(identity());
     }
 
     private Identity identity() {
@@ -72,7 +97,7 @@ public class AddDevicePresenterTest {
         partner.fpr = "fpr";
         partner.address = PARTNER_ADDRESS;
         partner.lang = "ES";
-        partner.me = false;
         return partner;
     }
+
 }
