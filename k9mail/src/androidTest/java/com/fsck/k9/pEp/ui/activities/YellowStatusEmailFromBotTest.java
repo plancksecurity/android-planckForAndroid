b84bb08c21e359fbd7a69c79da955694232239cb
juan <juan@pep-security.net>
2017-12-19 10:45:29 +0100

    P4A-636 Change assert status color order

--- k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/YellowStatusEmailFromBotTest.java
+++ k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/YellowStatusEmailFromBotTest.java
@@ -95,8 +95,8 @@ public class YellowStatusEmailFromBotTest {
         clickMailStatus();
         testUtils.doWaitForResource(R.id.my_recycler_view);
         uiDevice.waitForIdle();
-        onView(withRecyclerView(R.id.my_recycler_view).atPosition(0)).check(matches(withBackgroundColor(R.color.pep_no_color)));
-        onView(withRecyclerView(R.id.my_recycler_view).atPosition(1)).check(matches(withBackgroundColor(R.color.pep_yellow)));
+        onView(withRecyclerView(R.id.my_recycler_view).atPosition(0)).check(matches(withBackgroundColor(R.color.pep_yellow)));
+        onView(withRecyclerView(R.id.my_recycler_view).atPosition(1)).check(matches(withBackgroundColor(R.color.pep_no_color)));
     }
 
     private void fillComposeFields() {
