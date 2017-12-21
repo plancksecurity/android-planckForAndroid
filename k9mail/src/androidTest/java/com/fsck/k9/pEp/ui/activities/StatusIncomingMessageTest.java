19495b1786e2caacd5f08ba513d15e58d7b01646
juan <juan@pep-security.net>
2017-12-21 12:28:26 +0100

    P4A-276 Use testUtils methods in StatusIncomingMessageTest

--- k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/StatusIncomingMessageTest.java
+++ k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/StatusIncomingMessageTest.java
@@ -25,10 +25,8 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.longClick;
-import static android.support.test.espresso.action.ViewActions.swipeDown;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 
 public class StatusIncomingMessageTest {
@@ -40,9 +38,6 @@ public class StatusIncomingMessageTest {
     private UiDevice uiDevice;
     private TestUtils testUtils;
     private String messageTo;
-    private String lastMessageReceivedDate;
-    private int lastMessageReceivedPosition;
-    private BySelector textViewSelector;
 
     @Rule
     public ActivityTestRule<SplashActivity> splashActivityTestRule = new ActivityTestRule<>(SplashActivity.class);
@@ -52,7 +47,6 @@ public class StatusIncomingMessageTest {
         uiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
         testUtils = new TestUtils(uiDevice);
         testUtils.increaseTimeoutWait();
-        textViewSelector = By.clazz("android.widget.TextView");
         messageTo = Long.toString(System.currentTimeMillis()) + "@" + HOST;
         testUtils.startActivity();
     }
@@ -68,20 +62,20 @@ public class StatusIncomingMessageTest {
     }
 
     private void assertPartnerStatusIsTrusted() {
-        getLastMessageReceived();
+        testUtils.getLastMessageReceived();
         testUtils.composeMessageButton();
         uiDevice.waitForIdle();
         testUtils.fillMessage(new TestUtils.BasicMessage("", MESSAGE_SUBJECT, MESSAGE_BODY, messageTo), false);
         testUtils.sendMessage();
         uiDevice.waitForIdle();
-        waitForMessageWithText("bot", "bot (" + messageTo + ")");
-        clickLastMessageReceived();
-        assertMessageStatus(Rating.pEpRatingReliable.value);
+        testUtils.waitForMessageWithText("bot", "bot (" + messageTo + ")");
+        testUtils.clickLastMessageReceived();
+        testUtils.assertMessageStatus(Rating.pEpRatingReliable.value);
         uiDevice.waitForIdle();
         onView(withId(R.id.handshake_button_text)).perform(click());
         onView(withId(R.id.confirmTrustWords)).perform(click());
         testUtils.pressBack();
-        assertMessageStatus(Rating.pEpRatingTrusted.value);
+        testUtils.assertMessageStatus(Rating.pEpRatingTrusted.value);
     }
 
     private void assertIncomingTrustedPartnerMessageIsGreen() {
@@ -95,13 +89,6 @@ public class StatusIncomingMessageTest {
         onView(withRecyclerView(R.id.my_recycler_view).atPosition(0)).check(matches(withBackgroundColor(R.color.pep_green)));
     }
 
-    private void assertMessageStatus(int status) {
-        uiDevice.waitForIdle();
-        clickMessageStatus();
-        uiDevice.waitForIdle();
-        onView(withId(R.id.pEpTitle)).check(matches(withText(testUtils.getResourceString(R.array.pep_title, status))));
-    }
-
     private void fillMessage() {
         uiDevice.waitForIdle();
         testUtils.fillMessage(new TestUtils.BasicMessage("", MESSAGE_SUBJECT, MESSAGE_BODY, messageTo), false);
@@ -113,63 +100,6 @@ public class StatusIncomingMessageTest {
     private static UtilsPackage.RecyclerViewMatcher withRecyclerView(final int recyclerViewId) {
 
         return new UtilsPackage.RecyclerViewMatcher(recyclerViewId);
-    }
-
-    private void clickMessageStatus() {
-        uiDevice.waitForIdle();
-        onView(withId(R.id.tvPep)).perform(click());
-        uiDevice.waitForIdle();
-    }
-
-    private void clickLastMessageReceived() {
-        uiDevice.waitForIdle();
-        uiDevice.findObjects(textViewSelector).get(lastMessageReceivedPosition).click();
-    }
-
-    private void waitForMessageWithText(String textInMessage, String preview) {
-        boolean messageSubject;
-        boolean messageDate;
-        boolean messagePreview;
-        boolean emptyMessageList;
-        do {
-            messageSubject = testUtils.getTextFromTextViewThatContainsText(textInMessage).equals(uiDevice.findObjects(textViewSelector).get(lastMessageReceivedPosition).getText());
-            messageDate = !(lastMessageReceivedDate.equals(uiDevice.findObjects(textViewSelector).get(lastMessageReceivedPosition + 1).getText()));
-            messagePreview = testUtils.getTextFromTextViewThatContainsText(preview).equals(uiDevice.findObjects(textViewSelector).get(lastMessageReceivedPosition + 2).getText());
-            emptyMessageList = uiDevice.findObjects(textViewSelector).size() <= lastMessageReceivedPosition;
-            uiDevice.waitForIdle();
-        } while (!(!(emptyMessageList)
-                && (messageSubject && messageDate && messagePreview)));
-    }
-
-    private void getLastMessageReceived() {
-        uiDevice.waitForIdle();
-        lastMessageReceivedPosition = getLastMessageReceivedPosition();
-        onView(withId(R.id.message_list))
-                .perform(swipeDown());
-        if (lastMessageReceivedPosition != -1) {
-            lastMessageReceivedDate = uiDevice.findObjects(textViewSelector).get(lastMessageReceivedPosition + 1).getText();
-        } else {
-            lastMessageReceivedDate = "";
-            lastMessageReceivedPosition = uiDevice.findObjects(textViewSelector).size();
-        }
-    }
-
-    private int getLastMessageReceivedPosition() {
-        int size = uiDevice.findObjects(textViewSelector).size();
-        for (int position = 0; position < size; position++) {
-            String textAtPosition = uiDevice.findObjects(textViewSelector).get(position).getText();
-            if (textAtPosition != null && textAtPosition.contains("@")) {
-                position++;
-                while (uiDevice.findObjects(textViewSelector).get(position).getText() == null) {
-                    position++;
-                    if (position >= size) {
-                        return -1;
-                    }
-                }
-                return position;
-            }
-        }
-        return size;
     }
 
     private static Matcher<View> withBackgroundColor(final int colorId) {
