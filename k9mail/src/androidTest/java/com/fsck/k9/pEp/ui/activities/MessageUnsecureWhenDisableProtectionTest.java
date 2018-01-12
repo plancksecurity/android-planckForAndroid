305c22310c28019d073db118bbcec3977f27060d
juan <juan@pep-security.net>
2018-01-12 13:31:23 +0100

    P4A-645 Methods: removeMessages, waitForMessage(new) and getLasMessage

--- k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/MessageUnsecureWhenDisableProtectionTest.java
+++ k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/MessageUnsecureWhenDisableProtectionTest.java
@@ -2,6 +2,7 @@ package com.fsck.k9.pEp.ui.activities;
 
 
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.NoMatchingViewException;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.test.uiautomator.By;
@@ -16,6 +17,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.pEp.jniadapter.Rating;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.swipeDown;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
 @RunWith(AndroidJUnit4.class)
 public class MessageUnsecureWhenDisableProtectionTest {
     private UiDevice uiDevice;
@@ -23,7 +32,10 @@ public class MessageUnsecureWhenDisableProtectionTest {
     private String messageTo = "";
     private static final String MESSAGE_SUBJECT = "Subject";
     private static final String MESSAGE_BODY = "Message";
-    private BySelector selector;
+    private BySelector textViewSelector;
+    private String messageReceivedDate[];
+
+    int lastMessageReceivedPosition;
 
     @Rule
     public ActivityTestRule<SplashActivity> splashActivityTestRule = new ActivityTestRule<>(SplashActivity.class);
@@ -33,17 +45,20 @@ public class MessageUnsecureWhenDisableProtectionTest {
         uiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
         testUtils = new TestUtils(uiDevice);
         testUtils.increaseTimeoutWait();
-        selector = By.clazz("android.widget.TextView");
+        textViewSelector = By.clazz("android.widget.TextView");
+        messageReceivedDate = new String[5];
         testUtils.startActivity();
     }
 
     @Test
     public void sendMessageToYourselfWithDisabledProtectionAndCheckReceivedMessageIsUnsecure() {
         //testUtils.getLastMessageReceived();
+        getLastMessageReceived();
         testUtils.composeMessageButton();
         uiDevice.waitForIdle();
         messageTo = testUtils.getTextFromTextViewThatContainsText("@");
         testUtils.fillMessage(new TestUtils.BasicMessage("", MESSAGE_SUBJECT, MESSAGE_BODY, messageTo), false);
+        uiDevice.waitForIdle();
         testUtils.checkStatus(Rating.pEpRatingTrusted);
         testUtils.pressBack();
         testUtils.openOptionsMenu();
@@ -51,8 +66,115 @@ public class MessageUnsecureWhenDisableProtectionTest {
         uiDevice.waitForIdle();
         testUtils.checkStatus(Rating.pEpRatingUnencrypted);
         testUtils.pressBack();
+        uiDevice.waitForIdle();
         testUtils.sendMessage();
         uiDevice.waitForIdle();
+        //testUtils.waitForMessageWithText(MESSAGE_BODY, MESSAGE_SUBJECT);
+        waitForMessageWithText(MESSAGE_BODY, MESSAGE_SUBJECT);
+        //testUtils.clickLastMessageReceived();
+        clickLastMessageReceived();
+        uiDevice.waitForIdle();
+        //testUtils.checkStatus(Rating.pEpRatingUnencrypted);
+        onView(withId(R.id.tvPep)).perform(click());
+        onView(withId(R.id.pEpTitle)).check(matches(withText(testUtils.getResourceString(R.array.pep_title, Rating.pEpRatingUnencrypted.value))));
+        uiDevice.waitForIdle();
+        testUtils.pressBack();
+        uiDevice.waitForIdle();
+        testUtils.pressBack();
+        //removeMessagesFromList();
+    }
+    public void getLastMessageReceived() {
+        uiDevice.waitForIdle();
+        onView(withId(R.id.message_list))
+                .perform(swipeDown());
+        uiDevice.waitForIdle();
+        lastMessageReceivedPosition = getLastMessageReceivedPosition();
+        int message = 0;
+        if (lastMessageReceivedPosition != -1){
+            removeMessagesFromList();
+            int size = uiDevice.findObjects(textViewSelector).size();
+            for (; (message < 5) && (lastMessageReceivedPosition + 1 + message * 3 < size); message++) {
+                messageReceivedDate[message] = uiDevice.findObjects(textViewSelector).get(lastMessageReceivedPosition + 1 + message * 3).getText();
+            }
+        } else {
+            for (; message < 5; message++) {
+                messageReceivedDate[message] = "";
+            }
+            lastMessageReceivedPosition = uiDevice.findObjects(textViewSelector).size();
+        }
+    }
 
+    public void removeMessagesFromList(){
+        uiDevice.waitForIdle();
+        if (uiDevice.findObjects(textViewSelector).size() > lastMessageReceivedPosition) {
+            uiDevice.waitForIdle();
+            uiDevice.findObjects(textViewSelector).get(lastMessageReceivedPosition).click();
+            boolean emptyList = false;
+            do {
+                try {
+                    uiDevice.waitForIdle();
+                    onView(withId(R.id.delete)).check(matches(isDisplayed()));
+                    onView(withId(R.id.delete)).perform(click());
+                } catch (NoMatchingViewException e) {
+                    emptyList = true;
+                }
+            } while (!emptyList);
+        }
+    }
+
+    public int getLastMessageReceivedPosition() {
+        int size = uiDevice.findObjects(textViewSelector).size();
+        for (int position = 0; position < size; position++) {
+            String textAtPosition = uiDevice.findObjects(textViewSelector).get(position).getText();
+            if (textAtPosition != null && textAtPosition.contains("@")) {
+                position++;
+                while (uiDevice.findObjects(textViewSelector).get(position).getText() == null) {
+                    position++;
+                    if (position >= size) {
+                        return -1;
+                    }
+                }
+                return position;
+            }
+        }
+        return size;
+    }
+
+    public void waitForMessageWithText(String textInMessage, String preview) {
+        boolean messageSubject = false;
+        boolean messagePreview = false;
+        boolean emptyMessageList;
+        emptyMessageList = uiDevice.findObjects(textViewSelector).size() <= lastMessageReceivedPosition;
+        if (!emptyMessageList) {
+            do {
+                boolean newMessage = false;
+                do {
+                    uiDevice.waitForIdle();
+                    int size = uiDevice.findObjects(textViewSelector).size();
+                    for (int message = 0; (message < 5) && (lastMessageReceivedPosition + 1 + message * 3 < size); message++) {
+                        if (!(uiDevice.findObjects(textViewSelector).get(lastMessageReceivedPosition + 1 + message * 3).getText())
+                                .equals(messageReceivedDate[message])) {
+                            newMessage = true;
+                            break;
+                        }
+                    }
+                } while (!newMessage);
+                if (uiDevice.findObjects(textViewSelector).size() >= lastMessageReceivedPosition + 2) {
+                    messageSubject = testUtils.getTextFromTextViewThatContainsText(textInMessage)
+                            .equals(uiDevice.findObjects(textViewSelector).get(lastMessageReceivedPosition).getText());
+                    messagePreview = testUtils.getTextFromTextViewThatContainsText(preview)
+                            .equals(uiDevice.findObjects(textViewSelector).get(lastMessageReceivedPosition + 2).getText());
+                }
+            } while (!(messageSubject && messagePreview));
+        } else {
+            while (emptyMessageList) {
+                emptyMessageList = uiDevice.findObjects(textViewSelector).size() <= lastMessageReceivedPosition;
+            }
+        }
+    }
+
+    public void clickLastMessageReceived() {
+        uiDevice.waitForIdle();
+        uiDevice.findObjects(textViewSelector).get(lastMessageReceivedPosition).click();
     }
 }
