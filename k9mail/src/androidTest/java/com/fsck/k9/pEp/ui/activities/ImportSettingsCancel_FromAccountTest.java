eff03cec3c29c779c892cee6eefdbf6ef5fbbf5f
juan <juan@pep-security.net>
2018-01-16 16:39:22 +0100

    P4A-648 Remove useless imports

--- k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/ImportSettingsCancel_FromAccountTest.java
+++ k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/ImportSettingsCancel_FromAccountTest.java
@@ -1,19 +1,16 @@
 package com.fsck.k9.pEp.ui.activities;
 
 import android.support.test.InstrumentationRegistry;
-import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.test.uiautomator.UiDevice;
 
 import com.fsck.k9.R;
 
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
