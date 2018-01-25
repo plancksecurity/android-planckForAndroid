326a08ddacb2a80bc14f1c28818ec67b0ecddd83
juan <juan@pep-security.net>
2018-01-25 11:33:27 +0100

    P4A-645 Remove useless imports

--- k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/MessageUnsecureWhenDisableProtectionTest.java
+++ k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/MessageUnsecureWhenDisableProtectionTest.java
@@ -2,7 +2,6 @@ package com.fsck.k9.pEp.ui.activities;
 
 
 import android.support.test.InstrumentationRegistry;
-import android.support.test.espresso.NoMatchingViewException;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.test.uiautomator.UiDevice;
@@ -18,7 +17,6 @@ import org.pEp.jniadapter.Rating;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
