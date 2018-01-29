f89e3f7d93d29300ae6a7254ae0721ccc00e043b
Arturo Jiménez <arturo@pep-project.org>
2018-01-29 12:21:28 +0100

    P4A-554 Ask for permissions when manually turned off

--- k9mail/src/main/java/com/fsck/k9/pEp/PepActivity.java
+++ k9mail/src/main/java/com/fsck/k9/pEp/PepActivity.java
@@ -1,6 +1,9 @@
 package com.fsck.k9.pEp;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 
 import com.fsck.k9.K9;
 import com.fsck.k9.activity.K9Activity;
@@ -9,6 +12,13 @@ import com.fsck.k9.pEp.infrastructure.components.DaggerPEpComponent;
 import com.fsck.k9.pEp.infrastructure.components.PEpComponent;
 import com.fsck.k9.pEp.infrastructure.modules.ActivityModule;
 import com.fsck.k9.pEp.infrastructure.modules.PEpModule;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import java.util.List;
 
 public abstract class PepActivity extends K9Activity {
     private PEpProvider pEp;
@@ -55,5 +65,32 @@ public abstract class PepActivity extends K9Activity {
 
     public PEpComponent getpEpComponent() {
         return pEpComponent;
+    }
+
+    public void simplePermissionsCheck() {
+        if (!isPermissionGranted(Manifest.permission.WRITE_EXTERNAL_STORAGE) || !isPermissionGranted(Manifest.permission.READ_CONTACTS)) {
+            Dexter.withActivity(this)
+                    .withPermissions(
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                            Manifest.permission.READ_CONTACTS
+                    ).withListener(new MultiplePermissionsListener() {
+                @Override public void onPermissionsChecked(MultiplePermissionsReport report) {
+
+                }
+                @Override public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+                    token.continuePermissionRequest();
+                }
+            }).check();
+        }
+    }
+
+    private boolean isPermissionGranted(String readContacts) {
+        return ContextCompat.checkSelfPermission(this, readContacts) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        simplePermissionsCheck();
     }
 }
