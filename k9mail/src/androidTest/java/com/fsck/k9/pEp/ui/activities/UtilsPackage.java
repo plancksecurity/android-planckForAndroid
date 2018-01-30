4306593a77b499078e337a4468b22a7da07bb31a
juan <juan@pep-security.net>
2018-01-30 17:02:28 +0100

    P4A-607 Add two matchers to UtilsPackage

--- k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/UtilsPackage.java
+++ k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/UtilsPackage.java
@@ -3,7 +3,9 @@ package com.fsck.k9.pEp.ui.activities;
 import android.content.res.Resources;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ListView;
 
+import org.hamcrest.BaseMatcher;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
@@ -64,6 +66,41 @@ public class UtilsPackage {
                         return view == targetView;
                     }
 
+                }
+            };
+        }
+    }
+    private static Matcher<View> getElementFromMatchAtPosition(final Matcher<View> matcher, final int position) {
+        return new BaseMatcher<View>() {
+            int counter = 0;
+            @Override
+            public boolean matches(final Object item) {
+                if (matcher.matches(item)) {
+                    if(counter == position) {
+                        counter++;
+                        return true;
+                    }
+                    counter++;
+                }
+                return false;
+            }
+
+            @Override
+            public void describeTo(final Description description) {
+                description.appendText("Element at hierarchy position "+position);
+            }
+        };
+    }
+
+    public class Matchers {
+        public Matcher<View> withListSize(final int size) {
+            return new TypeSafeMatcher<View> () {
+                @Override public boolean matchesSafely (final View view) {
+                    return ((ListView) view).getCount () == size;
+                }
+
+                @Override public void describeTo (final Description description) {
+                    description.appendText ("ListView should have " + size + " items");
                 }
             };
         }
