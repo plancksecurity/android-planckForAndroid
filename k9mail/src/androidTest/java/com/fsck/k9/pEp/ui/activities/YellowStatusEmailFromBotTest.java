cdd9fa4aa8ae0d01a935d93d4b9718c2cad4b3c9
juan <juan@pep-security.net>
2018-02-07 15:01:35 +0100

    P4A-601 Use UtilsPackage matcher

--- k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/YellowStatusEmailFromBotTest.java
+++ k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/YellowStatusEmailFromBotTest.java
@@ -108,9 +108,9 @@ public class YellowStatusEmailFromBotTest {
         uiDevice.waitForIdle();
     }
 
-    public static RecyclerViewMatcher withRecyclerView(final int recyclerViewId) {
+    public static UtilsPackage.RecyclerViewMatcher withRecyclerView(final int recyclerViewId) {
 
-        return new RecyclerViewMatcher(recyclerViewId);
+        return new UtilsPackage.RecyclerViewMatcher(recyclerViewId);
     }
 
     private void goBackToMessageList() {
@@ -190,60 +190,4 @@ public class YellowStatusEmailFromBotTest {
         return activity[0];
     }
 
-    public static class RecyclerViewMatcher {
-        private final int recyclerViewId;
-
-        RecyclerViewMatcher(int recyclerViewId) {
-            this.recyclerViewId = recyclerViewId;
-        }
-
-        Matcher<View> atPosition(final int position) {
-            return atPositionOnView(position, -1);
-        }
-
-        Matcher<View> atPositionOnView(final int position, final int targetViewId) {
-
-            return new TypeSafeMatcher<View>() {
-                Resources resources = null;
-                View childView;
-
-                public void describeTo(Description description) {
-                    String idDescription = Integer.toString(recyclerViewId);
-                    if (this.resources != null) {
-                        try {
-                            idDescription = this.resources.getResourceName(recyclerViewId);
-                        } catch (Resources.NotFoundException var4) {
-                            idDescription = String.format("%s (resource name not found)",
-                                    recyclerViewId);
-                        }
-                    }
-
-                    description.appendText("with id: " + idDescription);
-                }
-
-                public boolean matchesSafely(View view) {
-
-                    this.resources = view.getResources();
-
-                    if (childView == null) {
-                        RecyclerView recyclerView =
-                                view.getRootView().findViewById(recyclerViewId);
-                        if (recyclerView != null && recyclerView.getId() == recyclerViewId) {
-                            childView = recyclerView.findViewHolderForAdapterPosition(position).itemView;
-                        } else {
-                            return false;
-                        }
-                    }
-
-                    if (targetViewId == -1) {
-                        return view == childView;
-                    } else {
-                        View targetView = childView.findViewById(targetViewId);
-                        return view == targetView;
-                    }
-
-                }
-            };
-        }
-    }
 }
