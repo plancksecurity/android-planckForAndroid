637fb98bc2c4b6b35385c5bd372a0645e9e0e83b
juan <juan@pep-security.net>
2018-04-12 11:19:20 +0200

    P4A-673 Register IdlingResources in SendMessageIsBlockedAfterChecking...

--- k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/SendMessageIsBlockedAfterChekingpEpStatus.java
+++ k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/SendMessageIsBlockedAfterChekingpEpStatus.java
@@ -2,19 +2,26 @@ package com.fsck.k9.pEp.ui.activities;
 
 import android.app.Instrumentation;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.IdlingRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.test.uiautomator.UiDevice;
 
 import com.fsck.k9.R;
+import com.fsck.k9.pEp.EspressoTestingIdlingResource;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.pEp.jniadapter.Rating;
 
+import timber.log.Timber;
+
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -38,7 +45,13 @@ public class SendMessageIsBlockedAfterChekingpEpStatus {
         instrumentation = InstrumentationRegistry.getInstrumentation();
         testUtils = new TestUtils(uiDevice, instrumentation);
         testUtils.increaseTimeoutWait();
+        IdlingRegistry.getInstance().register(EspressoTestingIdlingResource.getIdlingResource());
         testUtils.startActivity();
+    }
+
+    @After
+    public void unregisterIdlingResource() {
+        IdlingRegistry.getInstance().unregister(EspressoTestingIdlingResource.getIdlingResource());
     }
 
     @Test (timeout = TIMEOUT_TEST)
@@ -48,6 +61,7 @@ public class SendMessageIsBlockedAfterChekingpEpStatus {
         testUtils.checkStatus(Rating.pEpRatingTrusted);
         testUtils.pressBack();
         disableProtection();
+        onView(withId(R.id.subject)).perform(typeText(" "));
         testUtils.checkStatus(Rating.pEpRatingUnencrypted);
         testUtils.pressBack();
         testUtils.sendMessage();
@@ -56,6 +70,7 @@ public class SendMessageIsBlockedAfterChekingpEpStatus {
         composeSelfMessage();
         testUtils.sendMessage();
         uiDevice.waitForIdle();
+        testUtils.waitForNewMessage();
         testUtils.doWaitForResource(R.id.actionbar_title_first);
         onView(withId(R.id.actionbar_title_first)).check(matches(isDisplayed()));
         uiDevice.waitForIdle();
@@ -73,7 +88,6 @@ public class SendMessageIsBlockedAfterChekingpEpStatus {
     private void disableProtection(){
         testUtils.openOptionsMenu();
         testUtils.selectFromScreen(R.string.pep_force_unprotected);
-        uiDevice.waitForIdle();
         instrumentation.waitForIdleSync();
     }
 }
