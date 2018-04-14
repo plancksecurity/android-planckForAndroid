e91db6efa1d9c128ca4e854d9ceb65ef083a4ea2
Vincent Breitmoser <look@my.amazin.horse>
2018-04-14 13:43:36 +0200

    Add key transfer preference item
    (cherry picked from commit 96a184f)

--- k9mail/src/main/java/com/fsck/k9/activity/setup/AccountSettings.java
+++ k9mail/src/main/java/com/fsck/k9/activity/setup/AccountSettings.java
@@ -46,6 +46,7 @@ import com.fsck.k9.pEp.ui.keys.PepExtraKeys;
 import com.fsck.k9.service.MailServiceLegacy;
 
 import org.openintents.openpgp.util.OpenPgpAppPreference;
+import com.fsck.k9.ui.endtoend.AutocryptKeyTransferActivity;
 import org.openintents.openpgp.OpenPgpApiManager;
 import org.openintents.openpgp.util.OpenPgpKeyPreference;
 
@@ -68,6 +69,7 @@ public class AccountSettings extends K9PreferenceActivity {
     private static final int SELECT_AUTO_EXPAND_FOLDER = 1;
 
     private static final int ACTIVITY_MANAGE_IDENTITIES = 2;
+    private static final int ACTIVITY_CRYPTO_TRANSFER_KEY = 3;
 
     private static final String PREFERENCE_SCREEN_MAIN = "main";
     private static final String PREFERENCE_SCREEN_COMPOSING = "composing";
@@ -124,6 +126,7 @@ public class AccountSettings extends K9PreferenceActivity {
     private static final String PREFERENCE_CRYPTO_KEY = "openpgp_key";
     private static final String PREFERENCE_CRYPTO_HIDE_SIGN_ONLY = "openpgp_hide_sign_only";
     private static final String PREFERENCE_AUTOCRYPT_PREFER_ENCRYPT = "autocrypt_prefer_encrypt";
+    private static final String PREFERENCE_AUTOCRYPT_TRANSFER = "autocrypt_transfer";
     private static final String PREFERENCE_CLOUD_SEARCH_ENABLED = "remote_search_enabled";
     private static final String PREFERENCE_REMOTE_SEARCH_NUM_RESULTS = "account_remote_search_num_results";
     private static final String PREFERENCE_REMOTE_SEARCH_FULL_TEXT = "account_remote_search_full_text";
