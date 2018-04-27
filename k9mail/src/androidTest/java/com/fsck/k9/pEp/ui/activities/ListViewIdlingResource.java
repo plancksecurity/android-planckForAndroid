9d3db46ff910e7693c71e26a6f3ce98ba411983d
juan <juan@pep-security.net>
2018-04-27 11:00:43 +0200

    P4A-673 Remove/Modify comments in ListViewIdlingResource

--- k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/ListViewIdlingResource.java
+++ k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/ListViewIdlingResource.java
@@ -5,8 +5,6 @@ import android.support.annotation.Nullable;
 import android.support.test.espresso.IdlingResource;
 import android.widget.ListView;
 
-import timber.log.Timber;
-
 public class ListViewIdlingResource implements IdlingResource {
 
     private ResourceCallback callback;
@@ -20,10 +18,8 @@ public class ListViewIdlingResource implements IdlingResource {
 
     @Override
     public boolean isIdleNow() {
-        Timber.i("Entra en el bucle isIdleNow");
         if (listView != null && listView.getAdapter().getCount() > 0) {
             if (callback != null) {
-                Timber.i("Entra en callbak");
                 callback.onTransitionToIdle();
             }
             return true;
@@ -39,6 +35,6 @@ public class ListViewIdlingResource implements IdlingResource {
 
     @Override
     public String getName() {
-        return "Recycler idling resource";
+        return "ListView idling resource";
     }
 }
