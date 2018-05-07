302a74d332708ba43bb795f739de1c4aae8b6ad3
juan <juan@pep-security.net>
2018-05-07 11:23:46 +0200

    P4A-673 Disable Animation in TestUtils

--- k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/TestUtils.java
+++ k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/TestUtils.java
@@ -85,6 +85,8 @@ import static org.junit.Assert.assertThat;
 
 class TestUtils {
 
+    private static final String ANIMATION_PERMISSION = "android.permission.SET_ANIMATION_SCALE";
+    private static final float ANIMATION_DISABLED = 0.0f;
     private static final String APP_ID = BuildConfig.APPLICATION_ID;
     private static final int LAUNCH_TIMEOUT = 5000;
     private static final String DESCRIPTION = "tester one";
@@ -796,8 +798,44 @@ class TestUtils {
         if (intent != null) {
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
         }
+        disableAllAnimations();
         context.startActivity(intent);
         device.wait(Until.hasObject(By.pkg(APP_ID).depth(0)), LAUNCH_TIMEOUT);
+    }
+
+    private void disableAllAnimations() {
+        if (getAnimationPermissionStatus() == PackageManager.PERMISSION_GRANTED) {
+            setSystemAnimationsScale(ANIMATION_DISABLED);
+        } else {
+            Log.e(TAG, "Not granted permission to change animation scale.");
+        }
+    }
+
+    private int getAnimationPermissionStatus() {
+        Context context = InstrumentationRegistry.getTargetContext();
+        return context.checkCallingOrSelfPermission(ANIMATION_PERMISSION);
+    }
+
+    private void setSystemAnimationsScale(float animationScale) {
+        try {
+            Class<?> windowManagerStubClazz = Class.forName("android.view.IWindowManager$Stub");
+            Method asInterface = windowManagerStubClazz.getDeclaredMethod("asInterface", IBinder.class);
+            Class<?> serviceManagerClazz = Class.forName("android.os.ServiceManager");
+            Method getService = serviceManagerClazz.getDeclaredMethod("getService", String.class);
+            Class<?> windowManagerClazz = Class.forName("android.view.IWindowManager");
+            Method setAnimationScales = windowManagerClazz.getDeclaredMethod("setAnimationScales", float[].class);
+            Method getAnimationScales = windowManagerClazz.getDeclaredMethod("getAnimationScales");
+
+            IBinder windowManagerBinder = (IBinder) getService.invoke(null, "window");
+            Object windowManagerObj = asInterface.invoke(null, windowManagerBinder);
+            float[] currentScales = (float[]) getAnimationScales.invoke(windowManagerObj);
+            for (int i = 0; i < currentScales.length; i++) {
+                currentScales[i] = animationScale;
+            }
+            setAnimationScales.invoke(windowManagerObj, new Object[]{currentScales});
+        } catch (Exception ex) {
+            Log.e(TAG, "Could not use reflection to change animation scale to: " + animationScale, ex);
+        }
     }
 
     @NonNull
