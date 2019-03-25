1ee4a9dd75c9b01ebd28c08063b3a15f8771f38b
juan <juan@pep-security.net>
2019-03-25 10:11:48 +0100

    SER 266 Modify idlingResource to start counter only when test is running

--- k9mail/src/main/java/com/fsck/k9/pEp/EspressoTestingIdlingResource.java
+++ k9mail/src/main/java/com/fsck/k9/pEp/EspressoTestingIdlingResource.java
@@ -10,15 +10,18 @@ public class EspressoTestingIdlingResource {
 
     private static CountingIdlingResource mCountingIdlingResource;
     private static int contador = 0;
+    private static boolean testStarted;
 
     public EspressoTestingIdlingResource(){
         mCountingIdlingResource =
                 new CountingIdlingResource(RESOURCE);
+        testStarted = false;
     }
 
     public static void increment() {
         if (mCountingIdlingResource != null) {
             contador++;
+            testStarted = true;
             Timber.i("Contador: " + contador);
             mCountingIdlingResource.increment();
         }
@@ -26,7 +29,7 @@ public class EspressoTestingIdlingResource {
 
     public static void decrement() {
         Timber.i("Contador entra en decrement");
-        if (mCountingIdlingResource != null) {
+        if (mCountingIdlingResource != null && testStarted) {
             contador--;
             Timber.i("Contador: " + contador);
             mCountingIdlingResource.decrement();
