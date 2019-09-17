thunderbird/thunderbird-android

Replace usages of getLastNonConfigurationInstance()
After changing to AppCompat we need to use
getLastCustomNonConfigurationInstance() instead of
getLastNonConfigurationInstance().

k9mail/src/main/java/com/fsck/k9/activity/Accounts.java
@@ -434,7 +434,7 @@ public class Accounts extends K9ListActivity implements OnItemClickListener {
         handler.setViewTitle();
 
         // Handle activity restarts because of a configuration change (e.g. rotating the screen)
-        nonConfigurationInstance = (NonConfigurationInstance) getLastNonConfigurationInstance();
+        nonConfigurationInstance = (NonConfigurationInstance) getLastCustomNonConfigurationInstance();
         if (nonConfigurationInstance != null) {
             nonConfigurationInstance.restore(this);
         }

plancksecurity/android-planckForAndroid

(cherry picked from commit bc2b734)

k9mail/src/main/java/com/fsck/k9/activity/Accounts.java
@@ -435,7 +435,7 @@ public class Accounts extends PEpImporterActivity {
         mHandler.setViewTitle();
 
         // Handle activity restarts because of a configuration change (e.g. rotating the screen)
-        mNonConfigurationInstance = (NonConfigurationInstance) getLastNonConfigurationInstance();
+        mNonConfigurationInstance = (NonConfigurationInstance) getLastCustomNonConfigurationInstance();
         if (mNonConfigurationInstance != null) {
             mNonConfigurationInstance.restore(this);
         }