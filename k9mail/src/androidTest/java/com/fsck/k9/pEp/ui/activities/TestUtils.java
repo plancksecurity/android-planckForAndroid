f70307bdd140b9b8f5245120caa354f77a167e8f
juan <juan@pep-security.net>
2019-11-15 16:36:44 +0100

    SER 266 Modify number of accounts to create to 1 Native code still has a memory problem and with more than 1 account tests will fail

--- k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/TestUtils.java
+++ k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/TestUtils.java
@@ -525,7 +525,7 @@ public class TestUtils {
                         Timber.i("Ignored", "Ignored exception");
                     }
                 }
-                createNAccounts(totalAccounts);
+                createNAccounts(1);
         } catch (Exception ex) {
             if (!exists(onView(withId(R.id.accounts_list)))) {
                 readConfigFile();
