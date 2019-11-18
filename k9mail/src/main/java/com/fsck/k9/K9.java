1a47d2f51e829279aff70ea17f5fdb1231b6a56e
Hussein Kasem <huss@pep-project.org>
2019-11-18 19:15:13 +0100

    P4A-5 Ensure fast poll is stopped once grouped.

--- k9mail/src/main/java/com/fsck/k9/K9.java
+++ k9mail/src/main/java/com/fsck/k9/K9.java
@@ -1834,6 +1834,7 @@ public class K9 extends MultiDexApplication {
                 case SyncNotifySole:
                     break;
                 case SyncNotifyInGroup:
+                    needsFastPoll = false;
                     break;
             }
 
