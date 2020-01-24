ebfbb7f072435b513636302b6bad46126c385132
francisco <fc@pep.security>
2020-01-24 12:59:57 +0100

    test-cleanup - Fix AuthenticationErrorNotificationsTest

--- k9mail/src/test/java/com/fsck/k9/notification/AuthenticationErrorNotificationsTest.java
+++ k9mail/src/test/java/com/fsck/k9/notification/AuthenticationErrorNotificationsTest.java
@@ -1,118 +1,145 @@
 package com.fsck.k9.notification;
 
 
-//@RunWith(K9RobolectricTestRunner.class)
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Context;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationCompat.Builder;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.test.core.app.ApplicationProvider;
+
+import com.fsck.k9.Account;
+import com.fsck.k9.K9RobolectricTestRunner;
+import com.fsck.k9.MockHelper;
+import com.fsck.k9.R;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+
+@RunWith(K9RobolectricTestRunner.class)
 public class AuthenticationErrorNotificationsTest {
-//    private static final boolean INCOMING = true;
-//    private static final boolean OUTGOING = false;
-//    private static final int ACCOUNT_NUMBER = 1;
-//    private static final String ACCOUNT_NAME = "TestAccount";
-//
-//
-//    private NotificationManagerCompat notificationManager;
-//    private NotificationCompat.Builder builder;
-//    private NotificationController controller;
-//    private Account account;
-//    private AuthenticationErrorNotifications authenticationErrorNotifications;
-//    private PendingIntent contentIntent;
-//
-//
-//    @Before
-//    public void setUp() throws Exception {
-//        notificationManager = createFakeNotificationManager();
-//        builder = createFakeNotificationBuilder();
-//        controller = createFakeNotificationController(notificationManager, builder);
-//        account = createFakeAccount();
-//        contentIntent = createFakeContentIntent();
-//
-//        authenticationErrorNotifications = new TestAuthenticationErrorNotifications();
-//    }
-//
-//    @Test
-//    public void showAuthenticationErrorNotification_withIncomingServer_shouldCreateNotification() throws Exception {
-//        int notificationId = NotificationIds.getAuthenticationErrorNotificationId(account, INCOMING);
-//
-//        authenticationErrorNotifications.showAuthenticationErrorNotification(account, INCOMING);
-//
-//        verify(notificationManager).notify(eq(notificationId), any(Notification.class));
-//        assertAuthenticationErrorNotificationContents();
-//    }
-//
-//    @Test
-//    public void clearAuthenticationErrorNotification_withIncomingServer_shouldCancelNotification() throws Exception {
-//        int notificationId = NotificationIds.getAuthenticationErrorNotificationId(account, INCOMING);
-//
-//        authenticationErrorNotifications.clearAuthenticationErrorNotification(account, INCOMING);
-//
-//        verify(notificationManager).cancel(notificationId);
-//    }
-//
-//    @Test
-//    public void showAuthenticationErrorNotification_withOutgoingServer_shouldCreateNotification() throws Exception {
-//        int notificationId = NotificationIds.getAuthenticationErrorNotificationId(account, OUTGOING);
-//
-//        authenticationErrorNotifications.showAuthenticationErrorNotification(account, OUTGOING);
-//
-//        verify(notificationManager).notify(eq(notificationId), any(Notification.class));
-//        assertAuthenticationErrorNotificationContents();
-//    }
-//
-//    @Test
-//    public void clearAuthenticationErrorNotification_withOutgoingServer_shouldCancelNotification() throws Exception {
-//        int notificationId = NotificationIds.getAuthenticationErrorNotificationId(account, OUTGOING);
-//
-//        authenticationErrorNotifications.clearAuthenticationErrorNotification(account, OUTGOING);
-//
-//        verify(notificationManager).cancel(notificationId);
-//    }
-//
-//    private void assertAuthenticationErrorNotificationContents() {
-//        verify(builder).setSmallIcon(R.drawable.notification_icon_warning);
-//        verify(builder).setTicker("Authentication failed");
-//        verify(builder).setContentTitle("Authentication failed");
-//        verify(builder).setContentText("Authentication failed for " + ACCOUNT_NAME + ". Update your server settings.");
-//        verify(builder).setContentIntent(contentIntent);
-//        verify(builder).setVisibility(NotificationCompat.VISIBILITY_PUBLIC);
-//    }
-//
-//    private NotificationManagerCompat createFakeNotificationManager() {
-//        return mock(NotificationManagerCompat.class);
-//    }
-//
-//    private Builder createFakeNotificationBuilder() {
-//        return MockHelper.mockBuilder(NotificationCompat.Builder.class);
-//    }
-//
-//    private NotificationController createFakeNotificationController(NotificationManagerCompat notificationManager,
-//            NotificationCompat.Builder builder) {
-//        NotificationController controller = mock(NotificationController.class);
-//        when(controller.getContext()).thenReturn(ApplicationProvider.getApplicationContext());
-//        when(controller.getNotificationManager()).thenReturn(notificationManager);
-//        when(controller.createNotificationBuilder()).thenReturn(builder);
-//        return controller;
-//    }
-//
-//    private Account createFakeAccount() {
-//        Account account = mock(Account.class);
-//        when(account.getAccountNumber()).thenReturn(ACCOUNT_NUMBER);
-//        when(account.getDescription()).thenReturn(ACCOUNT_NAME);
-//
-//        return account;
-//    }
-//
-//    private PendingIntent createFakeContentIntent() {
-//        return mock(PendingIntent.class);
-//    }
-//
-//
-//    class TestAuthenticationErrorNotifications extends AuthenticationErrorNotifications {
-//        public TestAuthenticationErrorNotifications() {
-//            super(controller);
-//        }
-//
-//        @Override
-//        PendingIntent createContentIntent(Context context, Account account, boolean incoming) {
-//            return contentIntent;
-//        }
-//    }
+    private static final boolean INCOMING = true;
+    private static final boolean OUTGOING = false;
+    private static final int ACCOUNT_NUMBER = 1;
+    private static final String ACCOUNT_NAME = "TestAccount";
+
+
+    private NotificationManagerCompat notificationManager;
+    private NotificationCompat.Builder builder;
+    private NotificationController controller;
+    private Account account;
+    private AuthenticationErrorNotifications authenticationErrorNotifications;
+    private PendingIntent contentIntent;
+
+
+    @Before
+    public void setUp() throws Exception {
+        notificationManager = createFakeNotificationManager();
+        builder = createFakeNotificationBuilder();
+        controller = createFakeNotificationController(notificationManager, builder);
+        account = createFakeAccount();
+        contentIntent = createFakeContentIntent();
+
+        authenticationErrorNotifications = new TestAuthenticationErrorNotifications();
+    }
+
+    @Test
+    public void showAuthenticationErrorNotification_withIncomingServer_shouldCreateNotification() {
+        int notificationId = NotificationIds.getAuthenticationErrorNotificationId(account, INCOMING);
+
+        authenticationErrorNotifications.showAuthenticationErrorNotification(account, INCOMING);
+
+        verify(notificationManager).notify(eq(notificationId), any(Notification.class));
+        assertAuthenticationErrorNotificationContents();
+    }
+
+    @Test
+    public void clearAuthenticationErrorNotification_withIncomingServer_shouldCancelNotification() {
+        int notificationId = NotificationIds.getAuthenticationErrorNotificationId(account, INCOMING);
+
+        authenticationErrorNotifications.clearAuthenticationErrorNotification(account, INCOMING);
+
+        verify(notificationManager).cancel(notificationId);
+    }
+
+    @Test
+    public void showAuthenticationErrorNotification_withOutgoingServer_shouldCreateNotification() {
+        int notificationId = NotificationIds.getAuthenticationErrorNotificationId(account, OUTGOING);
+
+        authenticationErrorNotifications.showAuthenticationErrorNotification(account, OUTGOING);
+
+        verify(notificationManager).notify(eq(notificationId), any(Notification.class));
+        assertAuthenticationErrorNotificationContents();
+    }
+
+    @Test
+    public void clearAuthenticationErrorNotification_withOutgoingServer_shouldCancelNotification() {
+        int notificationId = NotificationIds.getAuthenticationErrorNotificationId(account, OUTGOING);
+
+        authenticationErrorNotifications.clearAuthenticationErrorNotification(account, OUTGOING);
+
+        verify(notificationManager).cancel(notificationId);
+    }
+
+    private void assertAuthenticationErrorNotificationContents() {
+        verify(builder).setSmallIcon(R.drawable.notification_icon_warning);
+        verify(builder).setTicker("Authentication failed");
+        verify(builder).setContentTitle("Authentication failed");
+        verify(builder).setContentText("Authentication failed for " + ACCOUNT_NAME + ". Update your server settings.");
+        verify(builder).setContentIntent(contentIntent);
+        verify(builder).setVisibility(NotificationCompat.VISIBILITY_PUBLIC);
+    }
+
+    private NotificationManagerCompat createFakeNotificationManager() {
+        return mock(NotificationManagerCompat.class);
+    }
+
+    private Builder createFakeNotificationBuilder() {
+        return MockHelper.mockBuilder(NotificationCompat.Builder.class);
+    }
+
+    private NotificationController createFakeNotificationController(NotificationManagerCompat notificationManager,
+                                                                    NotificationCompat.Builder builder) {
+        NotificationController controller = mock(NotificationController.class);
+        when(controller.getContext()).thenReturn(ApplicationProvider.getApplicationContext());
+        when(controller.getNotificationManager()).thenReturn(notificationManager);
+        when(controller
+                .createNotificationBuilder(any(Account.class), any(NotificationChannelManager.ChannelType.class)))
+                .thenReturn(builder);
+        return controller;
+    }
+
+    private Account createFakeAccount() {
+        Account account = mock(Account.class);
+        when(account.getAccountNumber()).thenReturn(ACCOUNT_NUMBER);
+        when(account.getDescription()).thenReturn(ACCOUNT_NAME);
+
+        return account;
+    }
+
+    private PendingIntent createFakeContentIntent() {
+        return mock(PendingIntent.class);
+    }
+
+
+    class TestAuthenticationErrorNotifications extends AuthenticationErrorNotifications {
+        public TestAuthenticationErrorNotifications() {
+            super(controller);
+        }
+
+        @Override
+        PendingIntent createContentIntent(Context context, Account account, boolean incoming) {
+            return contentIntent;
+        }
+    }
 }
