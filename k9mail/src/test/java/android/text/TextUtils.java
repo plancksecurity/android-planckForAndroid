948c5a1f6aa74f5ef309701e2167defb8a30f49f
francisco <fc@pep.security>
2020-01-27 14:54:53 +0100

    test-cleanup - Add join method for TextUtils class

--- k9mail/src/test/java/android/text/TextUtils.java
+++ k9mail/src/test/java/android/text/TextUtils.java
@@ -2,6 +2,8 @@ package android.text;
 
 import androidx.annotation.NonNull;
 
+import java.util.Iterator;
+
 public class TextUtils {
   public static boolean isEmpty(CharSequence str) {
     return str == null || str.length() == 0;
@@ -26,6 +28,29 @@ public class TextUtils {
     for (int i = 1; i < length; i++) {
       sb.append(delimiter);
       sb.append(tokens[i]);
+    }
+    return sb.toString();
+  }
+
+  /**
+   * Returns a string containing the tokens joined by delimiters.
+   *
+   * @param delimiter a CharSequence that will be inserted between the tokens. If null, the string
+   *     "null" will be used as the delimiter.
+   * @param tokens an array objects to be joined. Strings will be formed from the objects by
+   *     calling object.toString(). If tokens is null, a NullPointerException will be thrown. If
+   *     tokens is empty, an empty string will be returned.
+   */
+  public static String join(@NonNull CharSequence delimiter, @NonNull Iterable tokens) {
+    final Iterator<?> it = tokens.iterator();
+    if (!it.hasNext()) {
+      return "";
+    }
+    final StringBuilder sb = new StringBuilder();
+    sb.append(it.next());
+    while (it.hasNext()) {
+      sb.append(delimiter);
+      sb.append(it.next());
     }
     return sb.toString();
   }
