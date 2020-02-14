e528cfa4f412952c64a5c8481ec2800ed4255a2f
juan <juan@pep-security.net>
2020-02-14 13:55:31 +0100

    SER 266 Modify method to wait after doing click on object  This solves a problem with Nokia 1 GO that was taking too long to   change the screen and was trying to do click again

--- k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/TestUtils.java
+++ k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/TestUtils.java
@@ -1588,6 +1588,7 @@ public class TestUtils {
                             while (object.getText().equals(resources.getString(resource))) {
                                 device.waitForIdle();
                                 object.longClick();
+                                waitForTooblar();
                             }
                             device.waitForIdle();
                             return;
