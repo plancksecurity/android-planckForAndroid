b477b97cb29253e80cfb1489c30d4306f65d58ec
francisco <fc@pep.security>
2020-03-04 14:26:19 +0100

    feature/P4A-901 - Fix contact badge first letter

--- k9mail/src/main/java/security/pEp/ui/PEpUIUtils.kt
+++ k9mail/src/main/java/security/pEp/ui/PEpUIUtils.kt
@@ -13,7 +13,7 @@ object PEpUIUtils {
 
     @JvmStatic
     fun accountNameSummary(word: String): String {
-        return word.take(0).ifBlank { "?" }
+        return word.take(1).ifBlank { "?" }
     }
 
     @JvmStatic
