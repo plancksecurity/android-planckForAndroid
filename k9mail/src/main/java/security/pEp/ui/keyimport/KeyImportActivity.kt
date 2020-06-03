2df73d8a17e317855b401ad6fbf8d993a71f4238
Francisco Cunha <fc@pep.security>
2020-06-03 14:11:59 +0200

    P4A-1022 - Wrong screen size

--- k9mail/src/main/java/security/pEp/ui/keyimport/KeyImportActivity.kt
+++ k9mail/src/main/java/security/pEp/ui/keyimport/KeyImportActivity.kt
@@ -3,7 +3,6 @@ package security.pEp.ui.keyimport
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.view.WindowManager
 import com.fsck.k9.R
 import com.fsck.k9.pEp.PepActivity
 import javax.inject.Inject
@@ -20,7 +19,6 @@ class KeyImportActivity : PepActivity(), KeyImportView {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.import_key_dialog)
-        setupFloatingWindow()
         if (isValidKeyImportIntent(intent)) {
             val account: String = intent.getStringExtra(ACCOUNT_EXTRA) ?: ""
             presenter.initialize(this, account)
@@ -40,16 +38,6 @@ class KeyImportActivity : PepActivity(), KeyImportView {
     override fun renderDialog() {}
 
     override fun showNegativeFeedback() {}
-
-    private fun setupFloatingWindow() {
-        val params = window.attributes
-        params.width = resources.getDimensionPixelSize(R.dimen.floating_width)
-        params.height = resources.getDimensionPixelSize(R.dimen.floating_height)
-        params.alpha = 1f
-        params.dimAmount = 0.4f
-        params.flags = params.flags or WindowManager.LayoutParams.FLAG_DIM_BEHIND
-        window.attributes = params
-    }
 
     fun onAccept() {
         presenter.onAccept()
