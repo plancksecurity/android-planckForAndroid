88eb39733da9c9c93245fa869220d44fca5c4943
Hussein Kasem <huss@pep-project.org>
2020-08-04 18:25:48 +0200

    P4A-1028 Move SplashActivity.kt to Kotlin

--- k9mail/src/main/java/com/fsck/k9/pEp/ui/activities/SplashActivity.kt
+++ k9mail/src/main/java/com/fsck/k9/pEp/ui/activities/SplashActivity.kt
@@ -1,17 +1,14 @@
-package com.fsck.k9.pEp.ui.activities;
+package com.fsck.k9.pEp.ui.activities
 
-import android.content.Intent;
-import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.fsck.k9.activity.SettingsActivity
 
-import com.fsck.k9.activity.SettingsActivity;
 
-public class SplashActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        SettingsActivity.Companion.actionBasicStart(this);
-        finish();
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        SettingsActivity.actionBasicStart(this)
+        finish()
     }
 }
