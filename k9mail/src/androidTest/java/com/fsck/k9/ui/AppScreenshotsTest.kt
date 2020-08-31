23fc4b6ba50c3bf623f2e6c60f9fd0bbaec8c0c3
Francisco Cunha <fc@pep.security>
2020-08-31 16:46:47 +0200

    P4A-1105 - Add first automatic account setup test

--- k9mail/src/androidTest/java/com/fsck/k9/ui/AppScreenshotsTest.kt
+++ k9mail/src/androidTest/java/com/fsck/k9/ui/AppScreenshotsTest.kt
@@ -1,19 +1,21 @@
 package com.fsck.k9.ui
 
 import android.content.Intent
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.swipeRight
+import android.os.Build
+import androidx.test.espresso.Espresso
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
+import com.fsck.k9.BuildConfig
 import com.fsck.k9.R
 import com.fsck.k9.pEp.ui.activities.SplashActivity
+import kotlinx.coroutines.runBlocking
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import security.pEp.ui.intro.WelcomeMessage
+import security.pEp.ui.permissions.PermissionsActivity
 
 
 @LargeTest
@@ -24,55 +26,78 @@ class AppScreenshotsTest : BaseScreenshotTest() {
     var splashRule =
             ActivityTestRule(SplashActivity::class.java, false, false)
 
-
     @Test
-    fun openFirstScreen() {
-        val intent = Intent()
-        splashRule.launchActivity(intent)
-
-        getScreenShot(splashRule.activity::class.java.simpleName, "splash")
-
-        Thread.sleep(2000)
-
-        getScreenShotCurrentActivity("after splash")
-
+    fun allTest() {
+        openFirstScreen()
+        passWelcomeScreen()
+        when {
+            showPermissionsScreen() -> acceptPermissions()
+            else -> addFirstAccountAutomatic()
+        }
     }
 
-    @Test
-    fun passWelcomeScreen() {
+    private fun openFirstScreen() {
         val intent = Intent()
         splashRule.launchActivity(intent)
+        getScreenShotCurrentActivity("splash")
+        runBlocking { waitForIdle() }
+    }
 
-        onView(withId(R.id.next)).perform(click())
+    private fun passWelcomeScreen() {
+        getScreenShotCurrentActivity("welcome screen")
+        click(R.id.next)
         getScreenShotCurrentActivity(" first click")
-        onView(withId(R.id.next)).perform(click())
+        click(R.id.next)
         getScreenShotCurrentActivity(" second click")
-        onView(withId(R.id.next)).perform(click())
+        click(R.id.next)
         getScreenShotCurrentActivity(" third click")
+        click(R.id.done)
+        runBlocking { waitForIdle() }
     }
 
     private fun swipeBackInWelcomeMessage() {
         val welcomeMessage = getCurrentActivity()
         if (welcomeMessage is WelcomeMessage) {
             val view = withId(R.id.view_pager)
-            onView(view).perform(swipeRight())
+            swipeRight(view)
             getScreenShotCurrentActivity(" swipe right once")
-            onView(view).perform(swipeRight())
+            swipeRight(view)
             getScreenShotCurrentActivity(" swipe right twice")
-            onView(view).perform(swipeRight())
+            swipeRight(view)
             getScreenShotCurrentActivity(" swipe right thrice")
-            onView(withId(R.id.skip)).perform(click())
-            getScreenShotCurrentActivity(" click skip")
+            click(R.id.skip)
         } else {
             throw Exception("Wrong activity on screen")
         }
     }
 
-    private fun acceptPermissions(){
-
+    private fun showPermissionsScreen(): Boolean {
+        getScreenShotCurrentActivity("show permissions")
+        if (getCurrentActivity() is PermissionsActivity)
+            click(R.id.action_continue)
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
     }
 
-    private fun addFirstAccountAuto(){
-
+    private fun acceptPermissions() {
+        getScreenShotCurrentActivity("accept permissions")
+        TODO("Not sure if possible, since no screenshots if there is no permissions")
+        // addFirstAccountAuto()
     }
+
+    private fun addFirstAccountAutomatic() {
+        getScreenShotCurrentActivity("without values")
+        addTextTo(R.id.account_email, BuildConfig.PEP_TEST_EMAIL_ADDRESS)
+        addTextTo(R.id.account_password, BuildConfig.PEP_TEST_EMAIL_PASSWORD)
+        getScreenShotCurrentActivity("with values")
+        Espresso.closeSoftKeyboard()
+        sleep(2000)
+        click(R.id.next)
+        sleep(2000)
+        getScreenShotCurrentActivity("without values")
+        addTextTo(R.id.account_name, "account name")
+        getScreenShotCurrentActivity("with values")
+        click(R.id.done)
+    }
+
+
 }
--- k9mail/src/androidTest/java/com/fsck/k9/ui/BaseScreenshotTest.kt
+++ k9mail/src/androidTest/java/com/fsck/k9/ui/BaseScreenshotTest.kt
@@ -1,8 +1,18 @@
 package com.fsck.k9.ui
 
 import android.app.Activity
+import android.app.ActivityManager
+import android.content.Context
+import android.content.Context.ACTIVITY_SERVICE
+import android.os.Build
 import android.util.Log
+import android.view.View
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.core.internal.deps.guava.collect.Iterables
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.assertThat
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.runner.lifecycle.ActivityLifecycleMonitorRegistry
@@ -14,15 +24,17 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
 import org.hamcrest.CoreMatchers
+import org.hamcrest.Matcher
 import org.junit.After
-import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
+import timber.log.Timber
 import java.io.File
-import java.util.regex.Pattern
+
 
 open class BaseScreenshotTest {
     lateinit var device: UiDevice
+
     /**
      * Wake up device and remove screenlock if needed
      * Set portrait orientation
@@ -45,30 +57,46 @@ open class BaseScreenshotTest {
         // Start from the home screen
         device.pressHome()
         try {
-            // Wait for launcher
-            val launcherPackage = device.launcherPackageName
-            Assert.assertThat(launcherPackage, CoreMatchers.notNullValue())
-            device.wait(
-                    Until.hasObject(By.pkg(launcherPackage).depth(0)),
-                    LAUNCH_TIMEOUT.toLong()
-            )
-
-            // Launch the app
-            val context = InstrumentationRegistry.getInstrumentation().context
-            val intent = context.packageManager.getLaunchIntentForPackage(BASIC_SAMPLE_PACKAGE)
-            // Clear out any previous instances
-            context.startActivity(intent)
-
-            // Wait for the app to appear
-            device.wait(
-                    Until.hasObject(By.pkg(BASIC_SAMPLE_PACKAGE).depth(0)),
-                    LAUNCH_TIMEOUT.toLong()
-            )
+            waitLauncher()
+            waitAppLaunch()
         } catch (e: Exception) {
-            Log.e("Test", "Launch failed ================>")
+            Timber.e( "Launch failed ================>")
+            Timber.e(e)
+        }
+        Timber.e("Test", "Launch successful ================>")
+    }
+
+    private fun waitAppLaunch() {
+        device.wait(Until.hasObject(By.pkg(BASIC_SAMPLE_PACKAGE).depth(0)), LAUNCH_TIMEOUT)
+    }
+
+    private fun waitLauncher() {
+        val launcherPackage = device.launcherPackageName
+        assertThat(launcherPackage, CoreMatchers.notNullValue())
+        device.wait(Until.hasObject(By.pkg(launcherPackage).depth(0)), LAUNCH_TIMEOUT)
+    }
+
+    private fun launchApp() {
+        // Launch the app
+        val context = InstrumentationRegistry.getInstrumentation().context
+        val intent = context.packageManager.getLaunchIntentForPackage(BASIC_SAMPLE_PACKAGE)
+        // Clear out any previous instances
+        context.startActivity(intent)
+    }
+
+    private fun clearAppData() {
+        try {
+            // clearing app data
+            if (Build.VERSION_CODES.KITKAT <= Build.VERSION.SDK_INT) {
+                (InstrumentationRegistry.getInstrumentation().context.getSystemService(ACTIVITY_SERVICE) as ActivityManager).clearApplicationUserData()
+            } else {
+                val packageName = getApplicationContext<Context>().packageName
+                val runtime = Runtime.getRuntime()
+                runtime.exec("pm clear $packageName")
+            }
+        } catch (e: java.lang.Exception) {
             e.printStackTrace()
         }
-        Log.e("Test", "Launch successful ================>")
     }
 
     @After
@@ -77,7 +105,7 @@ open class BaseScreenshotTest {
         // Default is natural orientation (if a testcase fails)
         device.setOrientationNatural()
         device.pressHome()
-        Log.e("Test", "Testcase stops here <===========================")
+        Timber.e( "Testcase stops here <===========================")
     }
 
     @Test
@@ -89,11 +117,11 @@ open class BaseScreenshotTest {
         sleep(500) // Wait for screen to change
         val imageDir = File(IMAGE_DIR)
         if (!imageDir.exists()) imageDir.mkdir()
-        device.takeScreenshot(File("$IMAGE_DIR${className}_${action}_${cnt++}.png"), 0.5f, 25)
-        Log.e("Test", "Screenshot #" + (cnt - 1))
+        device.takeScreenshot(File("$IMAGE_DIR${cnt++}_${className}_${action}.png"), 0.5f, 25)
+        Timber.e( "Screenshot #" + (cnt - 1))
     }
 
-    private fun sleep(time: Int) {
+    fun sleep(time: Int) {
         try {
             Thread.sleep(time.toLong())
         } catch (e: Exception) {
@@ -110,7 +138,7 @@ open class BaseScreenshotTest {
         }
     }
 
-    suspend fun waitForIdle() = withContext(Dispatchers.IO){
+    suspend fun waitForIdle() = withContext(Dispatchers.IO) {
         InstrumentationRegistry.getInstrumentation().waitForIdleSync()
     }
 
@@ -121,10 +149,21 @@ open class BaseScreenshotTest {
         return@runBlocking Iterables.getOnlyElement(activities)
     }
 
+    fun addTextTo(resourceId: Int, text: String) {
+        Espresso.onView(ViewMatchers.withId(resourceId)).perform(ViewActions.typeText(text))
+    }
+
+    fun click(resourceId: Int) {
+        Espresso.onView(ViewMatchers.withId(resourceId)).perform(click())
+    }
+
+    fun swipeRight(view: Matcher<View>) {
+        Espresso.onView(view).perform(ViewActions.swipeRight())
+    }
 
     companion object {
-        private const val BASIC_SAMPLE_PACKAGE = "com.yourown.app"
-        private const val LAUNCH_TIMEOUT = 5000
+        private const val BASIC_SAMPLE_PACKAGE = "com.fsck.k9"
+        private const val LAUNCH_TIMEOUT = 5000L
         private const val IMAGE_DIR = "/sdcard/Screenshots/"
         private var cnt = 0
     }
