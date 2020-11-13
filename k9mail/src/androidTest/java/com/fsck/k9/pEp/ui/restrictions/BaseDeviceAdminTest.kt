e2a28adf6622ae84f667630965ac32866ab65688
Francisco Cunha <fc@pep.security>
2020-11-13 17:04:09 +0100

    P4A-1181 - Add restriction related espresso tests

---
+++ k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/restrictions/BaseDeviceAdminTest.kt
@@ -0,0 +1,45 @@
+package com.fsck.k9.pEp.ui.restrictions
+
+import android.content.Intent
+import androidx.test.uiautomator.By
+import com.fsck.k9.common.BaseTest
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertFalse
+import org.junit.Test
+
+
+open class BaseDeviceAdminTest : BaseTest() {
+
+    @Test
+    fun emptyTest1() {
+    }
+
+    fun checkSwitchPreferenceStatus(textId: Int) {
+        val textViewFound = false
+        val selector = By.clazz("android.widget.TextView")
+        while (!textViewFound) {
+            device.findObjects(selector).forEach { obj ->
+                if (obj.text.contains(resources.getString(textId))) {
+                    device.waitForIdle()
+                    val checkbox = obj.parent.parent.children[1].children[0]
+                    assertFalse(checkbox.isChecked)
+                    return
+                }
+            }
+        }
+    }
+
+    fun openFirstScreen() {
+        val intent = Intent()
+        splashRule.launchActivity(intent)
+        sleep(2000)
+        runBlocking { waitForIdle() }
+    }
+
+    companion object {
+        const val ENFORCER_PACKAGE_NAME: String = "com.example.android.apprestrictionenforcer"
+    }
+
+
+}
+
