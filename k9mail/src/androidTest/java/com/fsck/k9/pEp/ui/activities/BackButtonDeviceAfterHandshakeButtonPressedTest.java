87ae8e29a2037022c6482655e8dd4422fab68fbf
ignaciogarcia <ig@pep.security>
2020-12-04 10:52:12 +0100

    test-cleanup Fix BackButtonDevice... test.
    Fix BackButtonDeviceAfterHandshakeButtonPressedTest.

--- k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/BackButtonDeviceAfterHandshakeButtonPressedTest.java
+++ k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/BackButtonDeviceAfterHandshakeButtonPressedTest.java
@@ -1,12 +1,10 @@
 package com.fsck.k9.pEp.ui.activities;
 
 
-import android.app.Instrumentation;
-
-import androidx.test.InstrumentationRegistry;
 import androidx.test.espresso.IdlingRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
-import androidx.test.runner.AndroidJUnit4;
 import androidx.test.uiautomator.UiDevice;
 
 import com.fsck.k9.R;
@@ -14,30 +12,24 @@ import com.fsck.k9.pEp.EspressoTestingIdlingResource;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.FixMethodOrder;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.MethodSorters;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static com.fsck.k9.pEp.ui.activities.TestUtils.TIMEOUT_TEST;
 
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @RunWith(AndroidJUnit4.class)
 public class BackButtonDeviceAfterHandshakeButtonPressedTest {
 
-    private static final String HOST = "test.pep-security.net";
+    private static final String HOST = "sq.pep.security";
     private static final String MESSAGE_SUBJECT = "Subject";
     private static final String MESSAGE_BODY = "Message";
 
     private UiDevice device;
     private TestUtils testUtils;
     private String messageTo;
-    private Instrumentation instrumentation;
-    private EspressoTestingIdlingResource espressoTestingIdlingResource;
 
     @Rule
     public ActivityTestRule<SplashActivity> splashActivityTestRule = new ActivityTestRule<>(SplashActivity.class);
@@ -45,41 +37,37 @@ public class BackButtonDeviceAfterHandshakeButtonPressedTest {
     @Before
     public void startpEpApp() {
         device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
-        instrumentation = InstrumentationRegistry.getInstrumentation();
-        testUtils = new TestUtils(device, instrumentation);
-        testUtils.increaseTimeoutWait();
-        espressoTestingIdlingResource = new EspressoTestingIdlingResource();
-        IdlingRegistry.getInstance().register(espressoTestingIdlingResource.getIdlingResource());
-        messageTo = Long.toString(System.currentTimeMillis()) + "@" + HOST;
-        testUtils.startActivity();
+        testUtils = new TestUtils(device, InstrumentationRegistry.getInstrumentation());
+        new EspressoTestingIdlingResource();
+        IdlingRegistry.getInstance().register(EspressoTestingIdlingResource.getIdlingResource());
+        messageTo = System.currentTimeMillis() + "@" + HOST;
+        testUtils.setupAccountIfNeeded();
     }
 
     @After
-    public void unregisterIdlingResource() {
-        IdlingRegistry.getInstance().unregister(espressoTestingIdlingResource.getIdlingResource());
+    public void tearDown() {
+        splashActivityTestRule.finishActivity();
+        IdlingRegistry.getInstance().unregister(EspressoTestingIdlingResource.getIdlingResource());
     }
 
-    @Test (timeout = TIMEOUT_TEST)
+    @Test
     public void backButtonDeviceAfterHandshakeButtonPressed() {
-        //testUtils.createAccount();
-        sendMessages(3);
+        sendMessage();
         device.waitForIdle();
-        testUtils.waitForMessageAndClickIt();
-        testUtils.clickMessageStatus();
+        testUtils.waitForNewMessage();
+        testUtils.clickFirstMessage();
+        testUtils.clickStatus();
         device.waitForIdle();
-        onView(withId(R.id.confirmTrustWords)).perform(click());
-        testUtils.goBackAndRemoveAccount();
+        onView(withId(R.id.confirmHandshake)).perform(click());
+        testUtils.pressBack();
     }
 
-    public void sendMessages(int totalMessages) {
+    private void sendMessage() {
         device.waitForIdle();
-        for (int message = 0; message < totalMessages; message++) {
-            testUtils.composeMessageButton();
-            device.waitForIdle();
-            testUtils.fillMessage(new TestUtils.BasicMessage("", MESSAGE_BODY, MESSAGE_SUBJECT, messageTo), false);
-            testUtils.sendMessage();
-            device.waitForIdle();
-            testUtils.waitForNewMessage();
-        }
+        testUtils.composeMessageButton();
+        device.waitForIdle();
+        testUtils.fillMessage(new TestUtils.BasicMessage("", MESSAGE_BODY, MESSAGE_SUBJECT, messageTo), false);
+        testUtils.sendMessage();
+        device.waitForIdle();
     }
 }
