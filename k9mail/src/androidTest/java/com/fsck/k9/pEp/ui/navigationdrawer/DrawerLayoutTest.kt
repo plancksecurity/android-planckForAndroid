448eac6f82470d05319760c2f22c20f135d215a5
Francisco Cunha <fc@pep.security>
2020-12-10 17:39:23 +0100

    P4A-1208 - Add navDrawerIsNotEmpty() test

--- k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/navigationdrawer/DrawerLayoutTest.kt
+++ k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/navigationdrawer/DrawerLayoutTest.kt
@@ -1,18 +1,46 @@
 package com.fsck.k9.pEp.ui.navigationdrawer
 
+import androidx.test.espresso.Espresso.onView
+import com.fsck.k9.R
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.filters.LargeTest
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.ActivityTestRule
+import androidx.test.uiautomator.UiDevice
+import com.fsck.k9.pEp.ui.activities.SplashActivity
+import com.fsck.k9.pEp.ui.activities.TestUtils
+import org.hamcrest.core.IsNot.not
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 
 
 @RunWith(AndroidJUnit4::class)
-@LargeTest
 class DrawerLayoutTest {
 
-    fun openDrawer() {
+    @get:Rule
+    var mActivityRule = ActivityTestRule(SplashActivity::class.java)
+
+    private lateinit var testUtils: TestUtils
+
+    @Before
+    fun before() {
+        testUtils = TestUtils(UiDevice.getInstance(InstrumentationRegistry.getInstrumentation()), InstrumentationRegistry.getInstrumentation())
+    }
+
+
+    @Test
+    fun navDrawerIsNotEmpty() {
+        testUtils.openHamburgerMenu()
         // click hamburger account
-        // check if drawer header is visible
-        // check if drawer folders are visible
+        onView(withId(R.id.menu_header)).check(matches(isDisplayed()))
+        onView(withId(R.id.navigation_bar_folders_layout)).check(matches(isDisplayed()))
+        onView(withId(R.id.unified_inbox)).check(matches(isDisplayed()))
+        onView(withId(R.id.all_messages_container)).check(matches(isDisplayed()))
+        onView(withId(R.id.navigation_folders)).check(matches(isDisplayed()))
+        onView(withId(R.id.navigation_folders)).check(matches(not(hasChildCount(0))))
     }
 
     fun clickAccountBall() {
