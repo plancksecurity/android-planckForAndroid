ac19036a6b3832ef3003ed1ecd52ae8b26854d7e
ignaciogarcia <ig@pep.security>
2020-12-12 00:48:03 +0100

    test-cleanup Fix SetupDragonAccountTest.

--- k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/SetupDragonAccountTest.java
+++ k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/SetupDragonAccountTest.java
@@ -1,14 +1,16 @@
 package com.fsck.k9.pEp.ui.activities;
 
 
-import androidx.annotation.NonNull;
-import androidx.test.filters.LargeTest;
-import androidx.test.rule.ActivityTestRule;
-import androidx.test.runner.AndroidJUnit4;
-
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
+
+import androidx.annotation.NonNull;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.uiautomator.UiDevice;
 
 import com.fsck.k9.BuildConfig;
 import com.fsck.k9.R;
@@ -17,6 +19,8 @@ import com.fsck.k9.activity.setup.AccountSetupBasics;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -27,14 +31,27 @@ import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static java.lang.Thread.sleep;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
 public class SetupDragonAccountTest {
+    private TestUtils testUtils;
 
     @Rule
     public ActivityTestRule<AccountSetupBasics> activityTestRule = new ActivityTestRule<>(AccountSetupBasics.class);
+
+    @Before
+    public void setUp() {
+        UiDevice device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+        testUtils = new TestUtils(device, InstrumentationRegistry.getInstrumentation());
+        testUtils.skipTutorialAndAllowPermissionsIfNeeded();
+    }
+
+    @After
+    public void tearDown() {
+        finishSetup();
+        testUtils.goToSettingsAndRemoveAllAccounts();
+    }
 
     @Test
     public void setupDragonAccountTest() {
@@ -47,7 +64,7 @@ public class SetupDragonAccountTest {
         onView(withId(R.id.account_server)).perform(scrollTo(), replaceText(getServer()), closeSoftKeyboard());
 
         clickNext();
-        doWait();
+        testUtils.doWaitForAlertDialog(R.string.account_setup_failed_dlg_invalid_certificate_title);
         clickAccept();
     }
 
@@ -56,6 +73,7 @@ public class SetupDragonAccountTest {
         onView(withId(R.id.account_username)).perform(scrollTo(), replaceText(getUsername()), closeSoftKeyboard());
 
         clickNext();
+        testUtils.doWaitForAlertDialog(R.string.account_setup_failed_dlg_invalid_certificate_title);
         clickAccept();
     }
 
@@ -63,7 +81,11 @@ public class SetupDragonAccountTest {
         onView(withId(R.id.account_email)).perform(scrollTo(), click());
         onView(withId(R.id.account_email)).perform(scrollTo(), replaceText(getEmail()), closeSoftKeyboard());
         onView(withId(R.id.account_password)).perform(scrollTo(), replaceText(getPassword()), closeSoftKeyboard());
-        clickNext();
+        clickManualSetup();
+    }
+
+    private void clickManualSetup() {
+        onView(withId(R.id.manual_setup)).perform(click());
     }
 
     private void clickAccept() {
@@ -113,11 +135,12 @@ public class SetupDragonAccountTest {
         };
     }
 
-    private void doWait() {
-        try {
-            sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+    private void finishSetup() {
+        testUtils.waitUntilViewDisplayed(R.id.next);
+        clickNext();
+        testUtils.waitUntilViewDisplayed(R.id.account_name);
+        onView(withId(R.id.account_name)).perform(replaceText("test"));
+        onView(withId(R.id.pep_enable_sync_account)).perform(click());
+        onView(withId(R.id.done)).perform(click());
     }
 }
