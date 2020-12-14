de875884e14030848f31a4e751aeddfaa26e7b26
ignaciogarcia <ig@pep.security>
2020-12-14 13:21:31 +0100

    P4A-1238 Rename files for clarity of purpose.
    Finish fixing both tests.

--- k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/ImportSettingsCancelTest.java
+++ k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/ImportSettingsCancelTest.java
@@ -1,6 +1,5 @@
 package com.fsck.k9.pEp.ui.activities;
 
-import androidx.test.core.app.ApplicationProvider;
 import androidx.test.espresso.IdlingRegistry;
 import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -8,30 +7,23 @@ import androidx.test.filters.LargeTest;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.uiautomator.UiDevice;
 
-import com.fsck.k9.Account;
-import com.fsck.k9.BuildConfig;
-import com.fsck.k9.Preferences;
 import com.fsck.k9.R;
 import com.fsck.k9.pEp.EspressoTestingIdlingResource;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.FixMethodOrder;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.MethodSorters;
-
-import java.util.List;
 
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static com.fsck.k9.pEp.ui.activities.TestUtils.TIMEOUT_TEST;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static com.fsck.k9.pEp.ui.activities.UtilsPackage.saveSizeInInt;
 import static junit.framework.TestCase.assertEquals;
 
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class ImportSettingsCancelTest {
@@ -49,10 +41,7 @@ public class ImportSettingsCancelTest {
         testUtils = new TestUtils(device, InstrumentationRegistry.getInstrumentation());
         new EspressoTestingIdlingResource();
         IdlingRegistry.getInstance().register(EspressoTestingIdlingResource.getIdlingResource());
-        testUtils.increaseTimeoutWait();
-
         testUtils.skipTutorialAndAllowPermissionsIfNeeded();
-        testUtils.externalAppRespondWithFile(R.raw.test);
     }
 
     @After
@@ -61,43 +50,23 @@ public class ImportSettingsCancelTest {
         IdlingRegistry.getInstance().unregister(EspressoTestingIdlingResource.getIdlingResource());
     }
 
-    // DO NOT RUN INDIVIDUALLY, RUN WHOLE CLASS.
-    @Test(timeout = TIMEOUT_TEST)
-    public void importSettingsWithAccountFromSettingsCancelTest() {
-        testUtils.doWaitForAlertDialog(R.string.settings_import_activate_account_header);
-        testUtils.clickCancelButton();
+    @Test
+    public void importSettingsFromAccountSetupCancelTest() {
+        testUtils.goToSettingsAndRemoveAllAccountsIfNeeded();
+        testUtils.selectFromMenu(R.string.settings_import);
+        testUtils.getActivityInstance();
+        onView(withText(R.string.account_setup_basics_title)).check(matches(isDisplayed()));
+    }
 
+    @Test
+    public void importSettingsFromSettingsCancelTest() {
+        testUtils.setupAccountIfNeeded();
+        testUtils.selectFromMenu(R.string.action_settings);
         onView(withId(R.id.accounts_list)).perform(saveSizeInInt(accountListSize, 0));
         testUtils.selectFromMenu(R.string.import_export_action);
         testUtils.selectFromScreen(R.string.settings_import);
-        testUtils.doWaitForAlertDialog(R.string.settings_import_selection);
-        testUtils.clickAcceptButton();
-        testUtils.doWaitForAlertDialog(R.string.settings_import_success_header);
-        testUtils.clickAcceptButton();
-        testUtils.doWaitForAlertDialog(R.string.settings_import_activate_account_header);
-        onView(withId(R.id.incoming_server_password)).perform(replaceText(BuildConfig.PEP_TEST_EMAIL_PASSWORD));
-        testUtils.clickCancelButton();
-        device.waitForIdle();
+        testUtils.getActivityInstance();
         onView(withId(R.id.accounts_list)).perform(saveSizeInInt(accountListSize, 1));
-        testUtils.removeAllAccounts();
-        assertEquals(accountListSize[0] + 1, accountListSize[1]);
-    }
-
-    // DO NOT RUN INDIVIDUALLY, RUN WHOLE CLASS.
-    @Test(timeout = TIMEOUT_TEST)
-    public void importSettingsWithAccountFromAccountSetupCancelTest() {
-        testUtils.goToSettingsAndRemoveAllAccountsIfNeeded();
-        testUtils.selectFromMenu(R.string.settings_import);
-        testUtils.doWaitForAlertDialog(R.string.settings_import_selection);
-        testUtils.clickAcceptButton();
-        testUtils.doWaitForAlertDialog(R.string.settings_import_success_header);
-        testUtils.clickAcceptButton();
-        testUtils.doWaitForAlertDialog(R.string.settings_import_activate_account_header);
-        onView(withId(R.id.incoming_server_password)).perform(replaceText(BuildConfig.PEP_TEST_EMAIL_PASSWORD));
-        testUtils.clickCancelButton();
-        device.waitForIdle();
-        List<Account> accounts =
-                Preferences.getPreferences(ApplicationProvider.getApplicationContext()).getAccounts();
-        assertEquals(1, accounts.size());
+        assertEquals(accountListSize[0], accountListSize[1]);
     }
 }
