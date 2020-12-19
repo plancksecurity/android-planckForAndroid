8347d53610de71f538748f2bf8ef5970c8bd8861
ignaciogarcia <ig@pep.security>
2020-12-19 13:28:04 +0100

    P4A-1238 Fix AccountSetupNamesGenerationTest.

--- k9mail/src/androidTest/java/com/fsck/k9/activity/setup/AccountSetupNamesGenerationTest.kt
+++ k9mail/src/androidTest/java/com/fsck/k9/activity/setup/AccountSetupNamesGenerationTest.kt
@@ -4,31 +4,40 @@ import android.content.Context
 import android.content.Intent
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.rule.ActivityTestRule
-import com.fsck.k9.*
+import com.fsck.k9.Account
+import com.fsck.k9.BuildConfig
+import com.fsck.k9.Identity
+import com.fsck.k9.Preferences
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.times
 import com.nhaarman.mockito_kotlin.verify
-import junit.framework.Assert.assertEquals
+import org.junit.After
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(androidx.test.ext.junit.runners.AndroidJUnit4::class)
 class AccountSetupNamesGenerationTest {
-
+    private val context: Context = ApplicationProvider.getApplicationContext()
     private lateinit var account: Account
+    private val preferences = Preferences.getPreferences(context)
 
     @get:Rule
     var namesRule = object : ActivityTestRule<AccountSetupNames>(AccountSetupNames::class.java) {
         override fun getActivityIntent(): Intent {
-            val context: Context = ApplicationProvider.getApplicationContext()
-            account = Preferences.getPreferences(context).newAccount().apply {
+            account = preferences.newAccount().apply {
                 identities = listOf(Identity().apply { this.email = BuildConfig.PEP_TEST_EMAIL_ADDRESS })
             }
+
             return Intent(ApplicationProvider.getApplicationContext(), AccountSetupNames::class.java).apply {
                 putExtra(AccountSetupNames.EXTRA_ACCOUNT, account.uuid)
             }
         }
+    }
+
+    @After
+    fun tearDown() {
+        preferences.deleteAccount(account)
     }
 
     @Test
@@ -40,7 +49,7 @@ class AccountSetupNamesGenerationTest {
 
 
         activity.runOnUiThread {
-            activity.launchGenerateAccountKeysTask(generateAccountKeysTask)
+            activity.launchGenerateAccountKeysTask(generateAccountKeysTask, false)
             activity.recreate()
             activity.recreate()
             activity.recreate()
