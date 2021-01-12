e6e7e22daab51842d51c7aaa5ca112ac5d7a8427
Francisco Cunha <fc@pep.security>
2021-01-12 10:33:17 +0100

    test-compose - changes to compile jetpack compose

--- k9mail/src/main/java/security/pEp/ui/nav_view/NavFolderAccountButton.kt
+++ k9mail/src/main/java/security/pEp/ui/nav_view/NavFolderAccountButton.kt
@@ -7,7 +7,7 @@ import android.view.animation.AnimationUtils
 import androidx.appcompat.widget.AppCompatImageView
 import com.fsck.k9.R
 
-class NavFolderAccountButton(context: Context?, attrs: AttributeSet?) : AppCompatImageView(context, attrs) {
+class NavFolderAccountButton(context: Context, attrs: AttributeSet?) : AppCompatImageView(context, attrs) {
 
     private var clockwise: Animation? = AnimationUtils.loadAnimation(context,R.anim.rotate_clockwise);
     private var anticlockwise: Animation? = AnimationUtils.loadAnimation(context,R.anim.rotate_anti_clockwise);
