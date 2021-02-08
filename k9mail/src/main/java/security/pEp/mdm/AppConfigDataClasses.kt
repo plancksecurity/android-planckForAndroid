60ae1f01a0852a271e7d2ce70b8682926b32e15e
Francisco Cunha <fc@pep.security>
2021-02-08 11:17:28 +0100

    P4A-1181 - Rename and Abstract type to JsonAppConfig

--- k9mail/src/main/java/security/pEp/mdm/AppConfigDataClasses.kt
+++ k9mail/src/main/java/security/pEp/mdm/AppConfigDataClasses.kt
@@ -1,6 +1,6 @@
 package security.pEp.mdm
 
-import kotlinx.serialization.*
+import kotlinx.serialization.Serializable
 
 data class AppConfig(
         val key: String,
@@ -8,7 +8,7 @@ data class AppConfig(
 )
 
 @Serializable
-data class PEpGeneralConfigBoolean(
-        val value: Boolean = true,
+data class JsonAppConfig<SETTING_TYPE>(
+        val value: SETTING_TYPE,
         val blocked: Boolean = false,
 )
