1076e7f30e7d156632a7ff4f2d31489c86bc9381
Francisco Cunha <fc@pep.security>
2021-02-10 15:28:10 +0100

    P4A-1181 - Catch wrong json from MDM

--- k9mail/src/main/java/security/pEp/mdm/AppConfigSettings.kt
+++ k9mail/src/main/java/security/pEp/mdm/AppConfigSettings.kt
@@ -9,16 +9,20 @@ import kotlinx.serialization.json.Json
 data class AppConfigEntry(
         val key: String,
         val value: String?,
-){
-    inline fun <reified TYPE> getValue(): ManageableSettingMDMEntry<TYPE> =
-            Json.decodeFromString(value!!)
+) {
+    inline fun <reified TYPE> getValue(): ManageableSettingMDMEntry<TYPE>? {
+        return Json.runCatching { decodeFromString<ManageableSettingMDMEntry<TYPE>>(value!!) }
+                .getOrNull()
+
+    }
+
 }
 
 @Serializable
 data class ManageableSettingMDMEntry<TYPE>(
         @SerialName("locked") val locked: Boolean,
         @SerialName("value") val value: TYPE,
-){
+) {
     fun toManageableSetting(): ManageableSetting<TYPE> =
             ManageableSetting(value = value, locked = locked)
 }
