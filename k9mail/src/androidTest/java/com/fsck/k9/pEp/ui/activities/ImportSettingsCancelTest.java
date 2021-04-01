266852047555b2665a08009c36fb4b176d9ef231
ignaciogarcia <ig@pep.security>
2021-04-01 17:20:18 +0200

    P4A-1238 Fix ImportSettingsCancelTest.

--- k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/ImportSettingsCancelTest.java
+++ k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/ImportSettingsCancelTest.java
@@ -1,33 +1,44 @@
 package com.fsck.k9.pEp.ui.activities;
 
-import androidx.test.InstrumentationRegistry;
+import androidx.test.core.app.ApplicationProvider;
 import androidx.test.espresso.IdlingRegistry;
 import androidx.test.espresso.intent.rule.IntentsTestRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.uiautomator.UiDevice;
 
+import com.fsck.k9.Account;
+import com.fsck.k9.BuildConfig;
+import com.fsck.k9.Preferences;
 import com.fsck.k9.R;
 import com.fsck.k9.pEp.EspressoTestingIdlingResource;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 
-import static androidx.test.espresso.Espresso.onData;
+import java.util.List;
+
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static com.fsck.k9.pEp.ui.activities.TestUtils.TIMEOUT_TEST;
-import static org.hamcrest.Matchers.anything;
+import static com.fsck.k9.pEp.ui.activities.UtilsPackage.saveSizeInInt;
+import static junit.framework.TestCase.assertEquals;
 
-
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@RunWith(AndroidJUnit4.class)
+@LargeTest
 public class ImportSettingsCancelTest {
 
     private TestUtils testUtils;
     private UiDevice device;
-    private EspressoTestingIdlingResource espressoTestingIdlingResource;
+    private final int[] accountListSize = new int[2];
 
     @Rule
     public IntentsTestRule<SplashActivity> splashActivityTestRule = new IntentsTestRule<>(SplashActivity.class);
@@ -36,63 +47,57 @@ public class ImportSettingsCancelTest {
     public void startpEpApp() {
         device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
         testUtils = new TestUtils(device, InstrumentationRegistry.getInstrumentation());
-        espressoTestingIdlingResource = new EspressoTestingIdlingResource();
-        IdlingRegistry.getInstance().register(espressoTestingIdlingResource.getIdlingResource());
+        new EspressoTestingIdlingResource();
+        IdlingRegistry.getInstance().register(EspressoTestingIdlingResource.getIdlingResource());
         testUtils.increaseTimeoutWait();
-        testUtils.startActivity();
+
+        testUtils.skipTutorialAndAllowPermissionsIfNeeded();
+        testUtils.externalAppRespondWithFile(R.raw.test);
     }
 
     @After
-    public void unregisterIdlingResource() {
-        IdlingRegistry.getInstance().unregister(espressoTestingIdlingResource.getIdlingResource());
+    public void tearDown() {
+        splashActivityTestRule.finishActivity();
+        IdlingRegistry.getInstance().unregister(EspressoTestingIdlingResource.getIdlingResource());
     }
 
-    @Test (timeout = TIMEOUT_TEST)
-    public void importSettings() {
-        importSettingsTest(false);
-    }
+    // DO NOT RUN INDIVIDUALLY, RUN WHOLE CLASS.
+    @Test(timeout = TIMEOUT_TEST)
+    public void importSettingsWithAccountFromSettingsCancelTest() {
+        testUtils.doWaitForAlertDialog(R.string.settings_import_activate_account_header);
+        testUtils.clickCancelButton();
 
-    private void importSettingsTest(boolean isGmail) {
-        testUtils.increaseTimeoutWait();
-        testUtils.externalAppRespondWithFile(R.raw.settingsthemedark);
-        //testUtils.createAccount();
-        testUtils.pressBack();
-        device.waitForIdle();
-        testUtils.pressBack();
-        device.waitForIdle();
-        testUtils.openOptionsMenu();
-        device.waitForIdle();
-        testUtils.selectFromScreen(R.string.import_export_action);
-        device.waitForIdle();
+        onView(withId(R.id.accounts_list)).perform(saveSizeInInt(accountListSize, 0));
+        testUtils.selectFromMenu(R.string.import_export_action);
         testUtils.selectFromScreen(R.string.settings_import);
-        device.waitForIdle();
-        testUtils.doWaitForAlertDialog(splashActivityTestRule, R.string.settings_import_selection);
+        testUtils.doWaitForAlertDialog(R.string.settings_import_selection);
         testUtils.clickAcceptButton();
-        testUtils.doWaitForAlertDialog(splashActivityTestRule, R.string.settings_import_success_header);
+        testUtils.doWaitForAlertDialog(R.string.settings_import_success_header);
         testUtils.clickAcceptButton();
-        testUtils.doWaitForAlertDialog(splashActivityTestRule, R.string.settings_import_activate_account_header);
+        testUtils.doWaitForAlertDialog(R.string.settings_import_activate_account_header);
+        onView(withId(R.id.incoming_server_password)).perform(replaceText(BuildConfig.PEP_TEST_EMAIL_PASSWORD));
         testUtils.clickCancelButton();
         device.waitForIdle();
-        assertExistsTest();
-        testUtils.goBackAndRemoveAccount();
-        device.waitForIdle();
-        assertExistsTest();
-        testUtils.goBackAndRemoveAccount();
-        device.waitForIdle();
-        testUtils.doWaitForResource(R.id.skip);
-        assertThereAreNoAccounts();
+        onView(withId(R.id.accounts_list)).perform(saveSizeInInt(accountListSize, 1));
+        testUtils.removeAllAccounts();
+        assertEquals(accountListSize[0] + 1, accountListSize[1]);
     }
 
-    private void assertExistsTest(){
-        onData(anything())
-                .inAdapterView(withId(R.id.accounts_list))
-                .atPosition(0)
-                .onChildView(withId(R.id.description))
-                .check(matches(withText(testUtils.getAccountDescription())));
-    }
-
-    private void assertThereAreNoAccounts(){
-        onView(withId(R.id.skip))
-                .check(matches(isDisplayed()));
+    // DO NOT RUN INDIVIDUALLY, RUN WHOLE CLASS.
+    @Test(timeout = TIMEOUT_TEST)
+    public void importSettingsWithAccountFromAccountSetupCancelTest() {
+        testUtils.goToSettingsAndRemoveAllAccountsIfNeeded();
+        testUtils.selectFromMenu(R.string.settings_import);
+        testUtils.doWaitForAlertDialog(R.string.settings_import_selection);
+        testUtils.clickAcceptButton();
+        testUtils.doWaitForAlertDialog(R.string.settings_import_success_header);
+        testUtils.clickAcceptButton();
+        testUtils.doWaitForAlertDialog(R.string.settings_import_activate_account_header);
+        onView(withId(R.id.incoming_server_password)).perform(replaceText(BuildConfig.PEP_TEST_EMAIL_PASSWORD));
+        testUtils.clickCancelButton();
+        device.waitForIdle();
+        List<Account> accounts =
+                Preferences.getPreferences(ApplicationProvider.getApplicationContext()).getAccounts();
+        assertEquals(1, accounts.size());
     }
 }
