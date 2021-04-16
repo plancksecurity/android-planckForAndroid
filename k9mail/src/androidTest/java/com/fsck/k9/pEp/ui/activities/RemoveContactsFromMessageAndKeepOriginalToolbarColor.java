fc8cb4ad18323a3d8371d6c00140559d6d2b0b07
ignaciogarcia <ig@pep.security>
2021-04-16 12:19:08 +0200

    P4A-1238 Fix RemoveContacts... test.
    Fix RemoveContactsFromMessageAndKeepOriginalToolbarColor.

--- k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/RemoveContactsFromMessageAndKeepOriginalToolbarColor.java
+++ k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/RemoveContactsFromMessageAndKeepOriginalToolbarColor.java
@@ -1,16 +1,11 @@
 package com.fsck.k9.pEp.ui.activities;
 
-import android.app.Instrumentation;
-
-import androidx.test.InstrumentationRegistry;
-import androidx.test.espresso.Espresso;
 import androidx.test.espresso.IdlingRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
-import androidx.test.runner.AndroidJUnit4;
-import androidx.test.uiautomator.By;
 import androidx.test.uiautomator.UiDevice;
 
-import com.fsck.k9.BuildConfig;
 import com.fsck.k9.R;
 import com.fsck.k9.pEp.EspressoTestingIdlingResource;
 
@@ -20,28 +15,25 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import foundation.pEp.jniadapter.Rating;
+
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.action.ViewActions.typeText;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static com.fsck.k9.pEp.ui.activities.TestUtils.TIMEOUT_TEST;
-import static com.fsck.k9.pEp.ui.activities.UtilsPackage.withBackgroundColor;
-import static org.hamcrest.Matchers.allOf;
 
 
 @RunWith(AndroidJUnit4.class)
 public class RemoveContactsFromMessageAndKeepOriginalToolbarColor {
 
-    private static final String APP_ID = BuildConfig.APPLICATION_ID;
+    private static final String HOST = "sq.pep.security";
+    private static final String MESSAGE_SUBJECT = "Subject";
+    private static final String MESSAGE_BODY = "Message";
+    private static final String UNKNOWN_ADDRESS = "unkown@user.is";
 
     private TestUtils testUtils;
     private UiDevice device;
-    private String messageFrom;
-    private Instrumentation instrumentation;
-    private EspressoTestingIdlingResource espressoTestingIdlingResource;
 
     @Rule
     public ActivityTestRule<SplashActivity> splashActivityTestRule = new ActivityTestRule<>(SplashActivity.class);
@@ -49,52 +41,69 @@ public class RemoveContactsFromMessageAndKeepOriginalToolbarColor {
     @Before
     public void startpEpApp() {
         device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
-        instrumentation = InstrumentationRegistry.getInstrumentation();
-        espressoTestingIdlingResource = new EspressoTestingIdlingResource();
-        IdlingRegistry.getInstance().register(espressoTestingIdlingResource.getIdlingResource());
-        testUtils = new TestUtils(device, instrumentation);
-        testUtils.startActivity();
+        new EspressoTestingIdlingResource();
+        IdlingRegistry.getInstance().register(EspressoTestingIdlingResource.getIdlingResource());
+        testUtils = new TestUtils(device, InstrumentationRegistry.getInstrumentation());
+        testUtils.setupAccountIfNeeded();
     }
 
     @After
-    public void unregisterIdlingResource() {
-        IdlingRegistry.getInstance().unregister(espressoTestingIdlingResource.getIdlingResource());
+    public void tearDown() {
+        splashActivityTestRule.finishActivity();
+        IdlingRegistry.getInstance().unregister(EspressoTestingIdlingResource.getIdlingResource());
     }
 
-    @Test (timeout = TIMEOUT_TEST)
-    public void assertRemoveTwoDifferentColorContactsAndKeepOriginalToolbarColor() {
-        testUtils.increaseTimeoutWait();
-        //testUtils.createAccount();
-        messageFrom = testUtils.getTextFromTextViewThatContainsText("@");
-        assertToolBarHasNoColorWhenUnkownReceiver(messageFrom);
-        assertToolBarHasNoColorWhenUnkownReceiver("random@test.pep-security.net");
-        testUtils.goBackAndRemoveAccount();
+    @Test
+    public void assertRemoveTwoDifferentColorContactsAndKeepOriginalPrivacyStatus() {
+        testUtils.composeMessageButton();
+        device.waitForIdle();
+        String messageFrom = testUtils.getTextFromTextViewThatContainsText("@");
+
+        assertStatusWhenUnkownRecipient(messageFrom, false);
+        assertStatusGoesBackToNormalOnRemovingRecipient(Rating.pEpRatingTrustedAndAnonymized, false);
+
+        String botRecipient = System.currentTimeMillis() + "@" + HOST;
+        sendMessage(botRecipient);
+        testUtils.waitForNewMessage();
+        device.waitForIdle();
+
+
+        testUtils.composeMessageButton();
+        device.waitForIdle();
+
+        assertStatusWhenUnkownRecipient(botRecipient, false);
+        assertStatusGoesBackToNormalOnRemovingRecipient(Rating.pEpRatingReliable, true);
     }
 
     private void fillMessageWithOneKnownReceiverAndOneUnknown(String to, String subject, String message){
         testUtils.doWait("to");
         device.waitForIdle();
-        device.findObject(By.res(APP_ID, "to")).longClick();
+        onView(withId(R.id.subject)).perform(replaceText(subject));
         device.waitForIdle();
-        onView(withId(R.id.to)).perform(typeText(to), closeSoftKeyboard());
-        device.findObject(By.res(APP_ID, "subject")).click();
-        device.findObject(By.res(APP_ID, "subject")).setText(subject);
-        onView(withId(R.id.to)).perform(typeText("unkown@user.is"), closeSoftKeyboard());
-        device.findObject(By.res(APP_ID, "message_content")).click();
-        device.findObject(By.res(APP_ID, "message_content")).setText(message);
-        Espresso.closeSoftKeyboard();
+        onView(withId(R.id.message_content)).perform(typeText(message));
+        device.waitForIdle();
+        onView(withId(R.id.to)).perform(typeText(to +  "\n" + UNKNOWN_ADDRESS + "\n"), closeSoftKeyboard());
     }
 
-    private void assertToolBarHasNoColorWhenUnkownReceiver(String messageTo){
-        testUtils.composeMessageButton();
-        device.waitForIdle();
+    private void assertStatusWhenUnkownRecipient(String messageTo, boolean clickableExpected){
         fillMessageWithOneKnownReceiverAndOneUnknown(messageTo, "Subject", "Message");
         device.waitForIdle();
-        onView(allOf(withId(R.id.toolbar))).check(matches(withBackgroundColor(R.color.pep_no_color)));
+        testUtils.assertMessageStatus(Rating.pEpRatingUndefined, clickableExpected);
         device.waitForIdle();
-        testUtils.pressBack();
+    }
+
+    private void assertStatusGoesBackToNormalOnRemovingRecipient(Rating originalStatus, boolean clickableExpected) {
+        testUtils.removeTextFromTextView(R.id.to, UNKNOWN_ADDRESS + "\n");
         device.waitForIdle();
-        onView(withText(R.string.discard_action)).perform(click());
-        onView(allOf(withId(R.id.toolbar))).check(matches(withBackgroundColor(R.color.pep_green)));
+        testUtils.assertMessageStatus(originalStatus, clickableExpected);
+        testUtils.goBackFromMessageCompose(false);
+    }
+
+    private void sendMessage(String messageTo) {
+        testUtils.composeMessageButton();
+        device.waitForIdle();
+        testUtils.fillMessage(new TestUtils.BasicMessage("", MESSAGE_SUBJECT, MESSAGE_BODY, messageTo), false);
+        testUtils.sendMessage();
+        device.waitForIdle();
     }
 }
