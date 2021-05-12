21f28a81749037a85e4019af490e8349bf580017
ignaciogarcia <ig@pep.security>
2021-05-12 18:46:40 +0200

    P4A-1351 Add test for DuplicateAttachmentConfirmationPresenter.

---
+++ k9mail/src/test/java/com/fsck/k9/ui/messageview/dupplicateattachment/DuplicateAttachmentConfirmationPresenterTest.kt
@@ -0,0 +1,260 @@
+package com.fsck.k9.ui.messageview.dupplicateattachment
+
+import android.os.Bundle
+import com.fsck.k9.pEp.testutils.CoroutineTestRule
+import com.fsck.k9.ui.messageview.duplicateattachment.*
+import com.nhaarman.mockito_kotlin.*
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.ArgumentMatchers.anyBoolean
+import org.mockito.ArgumentMatchers.anyString
+import java.io.File
+
+@ExperimentalCoroutinesApi
+class DuplicateAttachmentConfirmationPresenterTest {
+    @get:Rule
+    val coroutinesTestRule = CoroutineTestRule()
+
+    private val view: DuplicateAttachmentConfirmationView = mock()
+    private val listener: DuplicationAttachmentConfirmationListener = mock()
+    private val savedInstanceState: Bundle = mock()
+    private val fileWrapper: FileWrapper = mock()
+    private lateinit var presenter: DuplicateAttachmentConfirmationPresenter
+    private var wrapperCalls = 0
+
+    @Before
+    fun setUp() {
+        doReturn(File("hi there")).`when`(fileWrapper).createFile(anyString(), anyString())
+        presenter = DuplicateAttachmentConfirmationPresenter(
+            coroutinesTestRule.testDispatcherProvider,
+            fileWrapper
+        )
+    }
+
+    @Test
+    fun `when presenter displays mode OVERWRITE, view displays overwrite screen`() {
+        presenter.initialize(
+            view,
+            listener,
+            ScreenMode.OVERWRITE,
+            DEFAULT_NAME,
+            SAVE_PATH
+        )
+
+        presenter.displayScreen(ScreenMode.OVERWRITE)
+
+        verify(view).displayOverwriteScreen()
+    }
+
+    @Test
+    fun `when presenter displays mode RENAME, view displays rename screen`() =
+        coroutinesTestRule.testDispatcher.runBlockingTest {
+            presenter.initialize(
+                view,
+                listener,
+                ScreenMode.RENAME,
+                DEFAULT_NAME,
+                SAVE_PATH
+            )
+
+            presenter.displayScreen(ScreenMode.RENAME)
+
+            verify(view).displayRenameScreen(anyBoolean(), anyString())
+        }
+
+    @Test
+    fun `when presenter has NO saved state, view displays screen in same mode presenter was initialized with`() {
+        presenter.initialize(
+            view,
+            listener,
+            ScreenMode.OVERWRITE,
+            DEFAULT_NAME,
+            SAVE_PATH
+        )
+
+        presenter.displayInitialScreen(null)
+
+        verify(view).displayOverwriteScreen()
+    }
+
+    @Test
+    fun `when presenter has saved state, view displays screen in mode saved in the state`() {
+        doReturn(ScreenMode.OVERWRITE.name).`when`(savedInstanceState).getString(STATE_CURRENT_SCREEN_MODE)
+
+        presenter.initialize(
+            view,
+            listener,
+            ScreenMode.RENAME,
+            DEFAULT_NAME,
+            SAVE_PATH
+        )
+
+        presenter.displayInitialScreen(savedInstanceState)
+
+        verify(view).displayOverwriteScreen()
+    }
+
+    @Test
+    fun `when rename button is clicked, view displays screen in mode RENAME`() =
+        coroutinesTestRule.testDispatcher.runBlockingTest {
+            presenter.initialize(
+                view,
+                listener,
+                ScreenMode.RENAME,
+                DEFAULT_NAME,
+                SAVE_PATH
+            )
+
+            presenter.renameButtonClicked()
+
+            verify(view).displayRenameScreen(eq(false), anyString())
+        }
+
+    @Test
+    fun `when negative button is clicked and current screen mode is OVERWRITE, view just finishes`() {
+        presenter.initialize(
+            view,
+            listener,
+            ScreenMode.OVERWRITE,
+            DEFAULT_NAME,
+            SAVE_PATH
+        )
+        presenter.displayScreen(ScreenMode.OVERWRITE)
+
+        presenter.negativeButtonClicked()
+
+        verify(view).finish()
+    }
+
+    @Test
+    fun `when negative button is clicked, current screen mode is RENAME and initial mode was RENAME, view just finishes`() {
+        presenter.initialize(
+            view,
+            listener,
+            ScreenMode.RENAME,
+            DEFAULT_NAME,
+            SAVE_PATH
+        )
+        presenter.displayScreen(ScreenMode.RENAME)
+
+        presenter.negativeButtonClicked()
+
+        verify(view).finish()
+    }
+
+    @Test
+    fun `when negative button is clicked, current screen mode is RENAME and initial mode was OVERWRITE, view displays OVERWRITE mode`() {
+        presenter.initialize(
+            view,
+            listener,
+            ScreenMode.OVERWRITE,
+            DEFAULT_NAME,
+            SAVE_PATH
+        )
+        presenter.displayScreen(ScreenMode.RENAME)
+
+        presenter.negativeButtonClicked()
+
+        verify(view).displayOverwriteScreen()
+    }
+
+    @Test
+    fun `when positive button is clicked, view finishes`() {
+        presenter.initialize(
+            view,
+            listener,
+            ScreenMode.OVERWRITE,
+            DEFAULT_NAME,
+            SAVE_PATH
+        )
+        presenter.displayScreen(ScreenMode.OVERWRITE)
+
+        presenter.positiveButtonClicked(DEFAULT_NAME)
+
+        verify(view).finish()
+    }
+
+    @Test
+    fun `when positive button is clicked and screen mode is OVERWRITE, listener overwrites attachment name`() {
+        presenter.initialize(
+            view,
+            listener,
+            ScreenMode.OVERWRITE,
+            DEFAULT_NAME,
+            SAVE_PATH
+        )
+        presenter.displayScreen(ScreenMode.OVERWRITE)
+
+        presenter.positiveButtonClicked(DEFAULT_NAME)
+
+        verify(listener).overwriteAttachmentName()
+    }
+
+    @Test
+    fun `when positive button is clicked and screen mode is RENAME, listener confirms new attachment name`() {
+        presenter.initialize(
+            view,
+            listener,
+            ScreenMode.OVERWRITE,
+            DEFAULT_NAME,
+            SAVE_PATH
+        )
+        presenter.displayScreen(ScreenMode.RENAME)
+
+        presenter.positiveButtonClicked(DEFAULT_NAME)
+
+        verify(listener).attachmentNameConfirmed(anyString())
+    }
+
+    @Test
+    fun `when presenter displays mode RENAME, view displays rename screen with modified name`() =
+        coroutinesTestRule.testDispatcher.runBlockingTest {
+            presenter.initialize(
+                view,
+                listener,
+                ScreenMode.RENAME,
+                DEFAULT_NAME,
+                SAVE_PATH
+            )
+
+            presenter.displayScreen(ScreenMode.RENAME)
+
+            verify(view).displayRenameScreen(false, MODIFIED_NAME_1)
+        }
+
+    @Test
+    fun `when presenter displays mode RENAME with name(1)ext, view displays rename screen with name(2)ext`() =
+        coroutinesTestRule.testDispatcher.runBlockingTest {
+            stubFileWrapper()
+
+            presenter.initialize(
+                view,
+                listener,
+                ScreenMode.RENAME,
+                DEFAULT_NAME,
+                SAVE_PATH
+            )
+
+            presenter.displayScreen(ScreenMode.RENAME)
+
+            verify(view).displayRenameScreen(false, MODIFIED_NAME_4)
+        }
+
+    private fun stubFileWrapper() {
+        doAnswer {
+            wrapperCalls++ < NUMBER_OF_EXISTING_FILES
+        }.`when`(fileWrapper).fileExists(any())
+    }
+
+    companion object {
+        private const val STATE_CURRENT_SCREEN_MODE = "currentScreenMode"
+        private const val DEFAULT_NAME = "defaultName.extension"
+        private const val MODIFIED_NAME_1 = "defaultName(1).extension"
+        private const val MODIFIED_NAME_4 = "defaultName(4).extension"
+        private const val SAVE_PATH = "save path"
+        private const val NUMBER_OF_EXISTING_FILES = 3
+    }
+}
