4634ffd70b499515b99292a5d0c984b967b3fccf
Francisco Cunha <fc@pep.security>
2021-07-12 13:30:45 +0200

    P4A-1433 - Add system webview availability check

--- k9mail/src/main/java/com/fsck/k9/pEp/ui/activities/SplashActivity.kt
+++ k9mail/src/main/java/com/fsck/k9/pEp/ui/activities/SplashActivity.kt
@@ -1,14 +1,24 @@
 package com.fsck.k9.pEp.ui.activities
 
 import android.os.Bundle
+import android.webkit.CookieManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.fsck.k9.R
 import com.fsck.k9.activity.SettingsActivity
 
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        SettingsActivity.actionBasicStart(this)
+        if (!isWebViewEnabled) {
+            Toast.makeText(this, getString(R.string.webview_not_installed), Toast.LENGTH_LONG)
+                .show()
+        } else {
+            SettingsActivity.actionBasicStart(this)
+        }
         finish()
     }
+
+    val isWebViewEnabled: Boolean = kotlin.runCatching { CookieManager.getInstance() }.isSuccess
 }
