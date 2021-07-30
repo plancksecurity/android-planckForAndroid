34f461e3347155be219d67e1d67f245e172e1f11
ignaciogarcia <ig@pep.security>
2021-07-30 12:50:33 +0200

    P4A-1101 Use LayoutStateMonitor in SplashActivity.
    The first time we run the app (after install or update), if screen width is big enough, app will choose by default split view mode "always". If we are using a foldable device and device is unfolded when installing/updating app, the app will be deceived to think we are on a tablet. So by using LayoutStateMonitor we can tell apart the situation of a foldable device in unfolded mode.
    In the case of a foldable device, we use same default value for split view mode as in a normal device that has at least 600dp as its greatest width.

--- k9mail/src/main/java/com/fsck/k9/pEp/ui/activities/SplashActivity.kt
+++ k9mail/src/main/java/com/fsck/k9/pEp/ui/activities/SplashActivity.kt
@@ -2,13 +2,40 @@ package com.fsck.k9.pEp.ui.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.window.WindowLayoutInfo
+import com.fsck.k9.K9
 import com.fsck.k9.activity.SettingsActivity
+import com.fsck.k9.pEp.ui.LayoutStateMonitor
+import security.pEp.ui.PEpUIUtils.isUnfoldedFoldableDevice
 
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        SettingsActivity.actionBasicStart(this)
-        finish()
+        layoutStateMonitor.initialize(this)
+        layoutStateMonitor.register()
+    }
+
+    private val layoutStateMonitor: LayoutStateMonitor = object : LayoutStateMonitor() {
+        override fun onLayoutStateChanged(windowLayoutInfo: WindowLayoutInfo) {
+            val isFoldableScreenUnfolded = windowLayoutInfo.isUnfoldedFoldableDevice()
+            if (isFoldableScreenUnfolded) {
+                if (K9.getSplitViewMode() == K9.SplitViewMode.ALWAYS) {
+                    K9.setSplitViewMode(K9.SplitViewMode.NEVER)
+                }
+            }
+            K9.setIsTablet(
+                !isFoldableScreenUnfolded
+                        && resources.configuration.smallestScreenWidthDp >=
+                        K9.MINIMUM_WIDTH_FOR_SPLIT_SCREEN
+            )
+            SettingsActivity.actionBasicStart(this@SplashActivity)
+            finish()
+        }
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        layoutStateMonitor.unRegister()
     }
 }
