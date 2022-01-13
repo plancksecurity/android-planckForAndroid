0a76b12439a4a1583a38b90cbb286d5a65f87143
ignaciogarcia <ig@pep.security>
2022-01-13 16:19:24 +0100

    P4A-1468 Add ExportPEpDatabasesActivity with presenter and view.

---
+++ k9mail/src/main/java/security/pEp/ui/support/export/ExportpEpSupportDataPresenter.kt
@@ -0,0 +1,11 @@
+package security.pEp.ui.support.export
+
+import javax.inject.Inject
+
+class ExportpEpSupportDataPresenter @Inject constructor() {
+    private lateinit var view: ExportpEpSupportDataView
+
+    fun initialize(view: ExportpEpSupportDataView) {
+        this.view = view
+    }
+}
