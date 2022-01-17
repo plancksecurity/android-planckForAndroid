1b2d1ea310da623c53d7a170352997a4ebf426dc
ignaciogarcia <ig@pep.security>
2022-01-17 11:42:10 +0100

    P4A-1468 Add message to exception. Log exception in any case.

--- k9mail/src/main/java/com/fsck/k9/pEp/infrastructure/exceptions/SupportExceptions.kt
+++ k9mail/src/main/java/com/fsck/k9/pEp/infrastructure/exceptions/SupportExceptions.kt
@@ -3,4 +3,7 @@ package com.fsck.k9.pEp.infrastructure.exceptions
 class NotEnoughSpaceInDeviceException(
     val neededSpace: Long,
     val availableSpace: Long,
-) : RuntimeException()
+) : RuntimeException(
+    "ERROR: Not enough space available to export pEp data: " +
+            "needed space is $neededSpace, available space is $availableSpace"
+)
