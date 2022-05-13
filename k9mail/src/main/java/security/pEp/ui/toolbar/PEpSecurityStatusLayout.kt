fed34c0a3d90f0cb5993c26c1af94f17587a4467
ignaciogarcia <ig@pep.security>
2022-05-13 06:20:29 +0200

    PEPA-26 Separate second line
    Add a second TextView for security status second line when text is long enough.

--- k9mail/src/main/java/security/pEp/ui/toolbar/PEpSecurityStatusLayout.kt
+++ k9mail/src/main/java/security/pEp/ui/toolbar/PEpSecurityStatusLayout.kt
@@ -2,6 +2,7 @@ package security.pEp.ui.toolbar
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.View
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.constraintlayout.widget.ConstraintLayout
@@ -13,16 +14,19 @@ import security.pEp.ui.PEpUIUtils.getRatingColorRes
 import security.pEp.ui.PEpUIUtils.getRatingTextRes
 import security.pEp.ui.PEpUIUtils.getToolbarRatingVisibility
 
+private const val MIN_LENGTH_BEFORE_LINEBREAK = 8
 class PEpSecurityStatusLayout(context: Context, attrs: AttributeSet?) : ConstraintLayout(context, attrs) {
 
     private var securityStatusIcon: AppCompatImageView? = null
     private var securityStatusText: AppCompatTextView? = null
+    private var secondLineText: AppCompatTextView? = null
     var encrypt = true
 
     public override fun onFinishInflate() {
         super.onFinishInflate()
         securityStatusIcon = findViewById(R.id.securityStatusIcon)
         securityStatusText = findViewById(R.id.securityStatusText)
+        secondLineText = findViewById(R.id.securityStatusSecondLine)
     }
 
     fun setRating(rating: Rating?) {
@@ -35,8 +39,25 @@ class PEpSecurityStatusLayout(context: Context, attrs: AttributeSet?) : Constrai
         else
             securityStatusIcon?.clearColorFilter()
 
-        securityStatusText?.setText(getRatingTextRes(rating, encrypt))
+        var firstLine = context.getString(getRatingTextRes(rating, encrypt))
+        secondLineText ?.let { secondTextView ->
+            var secondLine = ""
+            if(firstLine.length > MIN_LENGTH_BEFORE_LINEBREAK) {
+                val afterLimit = firstLine.substring(MIN_LENGTH_BEFORE_LINEBREAK)
+                if(afterLimit.contains(" ")) {
+                    secondLine = afterLimit.substringAfter(" ")
+                    firstLine = firstLine.substringBefore(secondLine)
+                }
+            }
+            if(secondLine.isNotBlank()) {
+                secondTextView.visibility = View.VISIBLE
+                secondTextView.text = secondLine
+            } else {
+                secondTextView.visibility = View.GONE
+            }
+        }
+        securityStatusText?.text = firstLine
         securityStatusText?.setTextColor(ContextCompat.getColor(context, getRatingColorRes(rating, encrypt)))
-
+        secondLineText?.setTextColor(ContextCompat.getColor(context, getRatingColorRes(rating, encrypt)))
     }
 }
