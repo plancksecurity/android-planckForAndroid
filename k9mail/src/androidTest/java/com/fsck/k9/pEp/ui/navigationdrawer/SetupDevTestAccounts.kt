5b13a200d40a8f32d9bb9a4ba49173af2f617aee
ignaciogarcia <ig@pep.security>
2022-05-20 14:32:25 +0200

    PEPA-22 Replace text

--- k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/navigationdrawer/SetupDevTestAccounts.kt
+++ k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/navigationdrawer/SetupDevTestAccounts.kt
@@ -69,7 +69,7 @@ open class SetupDevTestAccounts {
 
         uiDevice.waitForIdle()
         Thread.sleep(1000)
-        onView(withId(R.id.account_name)).check(matches(isDisplayed())).perform(clearText(), typeText(accountName))
+        onView(withId(R.id.account_name)).perform(replaceText(accountName))
         if(BuildConfig.WITH_KEY_SYNC) {
             onView(withId(R.id.pep_enable_sync_account)).check(matches(isChecked())).perform(scrollTo(), click())
         }
