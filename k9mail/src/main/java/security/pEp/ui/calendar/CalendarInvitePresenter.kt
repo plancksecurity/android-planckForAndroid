6efac24a9c975d3147c6430c448eebbb5b1d4078
ignaciogarcia <ig@pep.security>
2022-05-25 11:06:32 +0200

    P4A-1460 Move extensions to presenter
    Remove now unused Extensions.kt.

--- k9mail/src/main/java/security/pEp/ui/calendar/CalendarInvitePresenter.kt
+++ k9mail/src/main/java/security/pEp/ui/calendar/CalendarInvitePresenter.kt
@@ -67,6 +67,35 @@ class CalendarInvitePresenter @Inject constructor(
         showShortInvitees(event)
     }
 
+    private fun VEvent.getInvitees(): List<String> {
+        val invitees = attendees.mapNotNull { invitee ->
+            val displayName = getInviteeDisplayName(invitee.commonName, invitee.email)
+            if (organizer != null && invitee.email != null && invitee.email == organizer.email) {
+                displayName?.plus(" [organizer]")
+            } else displayName
+        }
+        organizer?.let { organizer ->
+            if (attendees.find { it.email == organizer.email } == null) {
+                getInviteeDisplayName(organizer.commonName, organizer.email)?.let {
+                    return invitees + "$it [organizer]"
+                }
+            }
+        }
+        return invitees
+    }
+
+    private fun getInviteeDisplayName(name: String?, email: String?): String? {
+        return if (name.isNullOrBlank()) {
+            if (!email.isNullOrBlank()) email else null
+        } else {
+            if (email.isNullOrBlank() || name == email) {
+                name
+            } else {
+                "$name ($email)"
+            }
+        }
+    }
+
     private fun renderCalendarInvite() {
         coroutineScope.launch {
             view.showLoading()
