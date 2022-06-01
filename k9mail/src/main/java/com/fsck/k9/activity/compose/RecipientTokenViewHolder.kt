c56058b5a055d1cb12d82348cca0cee49f2a6c00
ignaciogarcia <ig@pep.security>
2022-06-01 13:48:51 +0200

    PEPA-122 Change recipient color depending on rating

--- k9mail/src/main/java/com/fsck/k9/activity/compose/RecipientTokenViewHolder.kt
+++ k9mail/src/main/java/com/fsck/k9/activity/compose/RecipientTokenViewHolder.kt
@@ -2,14 +2,16 @@ package com.fsck.k9.activity.compose
 
 import android.view.View
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import com.fsck.k9.Account
 import com.fsck.k9.R
+import com.fsck.k9.pEp.PEpUtils
 import com.fsck.k9.pEp.ui.PEpContactBadge
 import com.fsck.k9.ui.contacts.ContactPictureLoader
 import foundation.pEp.jniadapter.Rating
 
 class RecipientTokenViewHolder internal constructor(
-    view: View,
+    private val view: View,
     private val contactPictureLoader: ContactPictureLoader,
     private val account: Account,
     private val cryptoProvider: String?
@@ -32,7 +34,7 @@ class RecipientTokenViewHolder internal constructor(
     }
 
     fun updateRating(rating: Rating) {
-        contactPhoto.setPepRating(rating, account.ispEpPrivacyProtected())
+        setpEpRating(rating)
         val hasCryptoProvider = cryptoProvider != null
         if (!hasCryptoProvider) {
             cryptoStatusRed.visibility = View.GONE
@@ -63,4 +65,15 @@ class RecipientTokenViewHolder internal constructor(
             }
     }
 
+    private fun setpEpRating(rating: Rating) {
+        when {
+            account.ispEpPrivacyProtected() && PEpUtils.isRatingUnsecure(rating) -> {
+                view.setBackgroundResource(R.drawable.recipient_unsecure_token_shape)
+                name.setTextColor(ContextCompat.getColor(name.context, R.color.pep_red))
+            }
+            else -> {
+                view.setBackgroundResource(R.drawable.recipient_token_shape)
+            }
+        }
+    }
 }
