fe7a4fb1a9f4826d23569c71cf2aaebfff241402
ignaciogarcia <ig@pep.security>
2022-06-01 14:39:54 +0200

    PEPA-122 Add method in FeedbackTools to show custom snackbar

--- k9mail/src/main/java/com/fsck/k9/pEp/ui/tools/FeedbackTools.java
+++ k9mail/src/main/java/com/fsck/k9/pEp/ui/tools/FeedbackTools.java
@@ -3,8 +3,13 @@ package com.fsck.k9.pEp.ui.tools;
 import com.fsck.k9.R;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.content.res.ColorStateList;
 import android.content.res.Resources;
 import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.ColorInt;
+import androidx.core.content.ContextCompat;
 
 import timber.log.Timber;
 
@@ -47,6 +52,38 @@ public class FeedbackTools {
             snackbar.show();
         } else {
             Timber.e(message);
+        }
+    }
+
+    public static Snackbar showIndefiniteFeedback(
+            View rootView,
+            String message,
+            @ColorInt int backgroundColor,
+            @ColorInt int textColor
+    ) {
+        if (rootView != null) {
+            Resources resources = rootView.getResources();
+            Snackbar snackbar = Snackbar.make(rootView, message, Snackbar.LENGTH_INDEFINITE);
+            View snackbarView = snackbar.getView();
+            int padding = Math.round(resources.getDimension(R.dimen.custom_snackbar_margin));
+            snackbarView.setPadding(padding, 0, padding,padding);
+            snackbarView.setBackgroundColor(
+                    ContextCompat.getColor(rootView.getContext(), android.R.color.transparent));
+
+            TextView snackTextView = snackbarView.findViewById(R.id.snackbar_text);
+            snackTextView.setMaxLines(10);
+            snackTextView.setBackgroundResource(R.drawable.custom_snackbar);
+            ColorStateList backgroundColorStateList = new ColorStateList(new int[][]{
+                    new int[] {}
+            }, new int[]{backgroundColor});
+            snackTextView.setBackgroundTintList(backgroundColorStateList);
+            snackTextView.setTextColor(textColor);
+
+            snackbar.show();
+            return snackbar;
+        } else {
+            Timber.e(message);
+            return null;
         }
     }
 }
