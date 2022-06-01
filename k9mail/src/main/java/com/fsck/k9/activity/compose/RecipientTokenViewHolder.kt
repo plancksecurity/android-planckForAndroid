da4800ce1775c1a907ea3ad9dd4a5b74061a1586
ignaciogarcia <ig@pep.security>
2022-06-01 13:45:40 +0200

    PEPA-122 Fix formatting in RecipientTokenViewHolder

--- k9mail/src/main/java/com/fsck/k9/activity/compose/RecipientTokenViewHolder.kt
+++ k9mail/src/main/java/com/fsck/k9/activity/compose/RecipientTokenViewHolder.kt
@@ -1,25 +1,28 @@
 package com.fsck.k9.activity.compose
 
-import android.R
 import android.view.View
 import android.widget.TextView
 import com.fsck.k9.Account
-import com.fsck.k9.activity.compose.RecipientSelectView
+import com.fsck.k9.R
 import com.fsck.k9.pEp.ui.PEpContactBadge
 import com.fsck.k9.ui.contacts.ContactPictureLoader
 import foundation.pEp.jniadapter.Rating
 
 class RecipientTokenViewHolder internal constructor(
-        view: View,
-        private val contactPictureLoader: ContactPictureLoader,
-        private val account: Account,
-        private val cryptoProvider: String?) {
+    view: View,
+    private val contactPictureLoader: ContactPictureLoader,
+    private val account: Account,
+    private val cryptoProvider: String?
+) {
 
-    private val name: TextView = view.findViewById(R.id.text1)
-    private val contactPhoto: PEpContactBadge = view.findViewById(com.fsck.k9.R.id.contact_photo)
-    private val cryptoStatusRed: View = view.findViewById(com.fsck.k9.R.id.contact_crypto_status_red)
-    private val cryptoStatusOrange: View = view.findViewById(com.fsck.k9.R.id.contact_crypto_status_orange)
-    private val cryptoStatusGreen: View = view.findViewById(com.fsck.k9.R.id.contact_crypto_status_green)
+    private val name: TextView = view.findViewById(android.R.id.text1)
+    private val contactPhoto: PEpContactBadge = view.findViewById(R.id.contact_photo)
+    private val cryptoStatusRed: View =
+        view.findViewById(R.id.contact_crypto_status_red)
+    private val cryptoStatusOrange: View =
+        view.findViewById(R.id.contact_crypto_status_orange)
+    private val cryptoStatusGreen: View =
+        view.findViewById(R.id.contact_crypto_status_green)
     private lateinit var recipient: Recipient
 
     fun bind(recipient: Recipient) {
