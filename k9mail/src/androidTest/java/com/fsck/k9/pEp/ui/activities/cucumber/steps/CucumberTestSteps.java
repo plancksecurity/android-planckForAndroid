cd17c62a6d838f4325066beb0f01f8a74f1e1128
juan <jgb@pep.security>
2022-07-11 13:50:59 +0200

    Modify method to use Enterprise colors

--- k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/cucumber/steps/CucumberTestSteps.java
+++ k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/cucumber/steps/CucumberTestSteps.java
@@ -89,6 +89,7 @@ import static com.fsck.k9.pEp.ui.activities.TestUtils.waitForIdle;
 import static com.fsck.k9.pEp.ui.activities.UtilsPackage.containstText;
 import static com.fsck.k9.pEp.ui.activities.UtilsPackage.exists;
 import static com.fsck.k9.pEp.ui.activities.UtilsPackage.getTextFromView;
+import static com.fsck.k9.pEp.ui.activities.UtilsPackage.getViewColorHSV;
 import static com.fsck.k9.pEp.ui.activities.UtilsPackage.saveSizeInInt;
 import static com.fsck.k9.pEp.ui.activities.UtilsPackage.viewIsDisplayed;
 import static com.fsck.k9.pEp.ui.activities.UtilsPackage.waitUntilIdle;
@@ -1454,8 +1455,25 @@ public class CucumberTestSteps {
     }
 
     private void checkPrivacyStatus(String status) {
-        if (BuildConfig.IS_ENTERPRISE && status.equals("pEpRatingUndefined")) {
-            status = "pep_no_color";
+        if (BuildConfig.IS_ENTERPRISE) {
+            switch (status) {
+                case "pEpRatingUndefined":
+                    if (viewIsDisplayed(onView(withId(R.id.securityStatusText)))) {
+                        assertFailWithMessage("Showing a rating that is not " + status);
+                    }
+                    return;
+                case "pep_red":
+                    if (!viewIsDisplayed(onView(withId(R.id.securityStatusText)))) {
+                        assertFailWithMessage("Showing a rating that is not " + status);
+                    }
+                    if (!getTextFromView(onView(withId(R.id.securityStatusText))).equals(resources.getString(testUtils.stringToID("enterprise_unsecure")))) {
+                        assertFailWithMessage("Showing a text that is not " + resources.getString(testUtils.stringToID("enterprise_unsecure")));
+                    }
+                    //status = "pEpRatingUnsecure";
+                    break;
+                    //int color = getViewColorHSV(onView(withId(R.id.securityStatusText)));
+                    //return;
+            }
         }
         Rating[] statusRating = new Rating[1];
         BySelector selector = By.clazz("android.widget.ScrollView");
