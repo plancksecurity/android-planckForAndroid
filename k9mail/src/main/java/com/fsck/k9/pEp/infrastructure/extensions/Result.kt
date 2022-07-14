1175bc94f409765a197cfe616559f03b631b9d44
ignaciogarcia <ig@pep.security>
2022-07-14 14:00:22 +0200

    PEPA-135 Add Result extension functions

---
+++ k9mail/src/main/java/com/fsck/k9/pEp/infrastructure/extensions/Result.kt
@@ -0,0 +1,13 @@
+package com.fsck.k9.pEp.infrastructure.extensions
+
+suspend fun <Type, NewType> Result<Type>.flatMapSuspend(block: suspend (Type) -> Result<NewType>): Result<NewType> =
+    fold(
+        onSuccess = { block(it) },
+        onFailure = { Result.failure(it) }
+    )
+
+fun <Type> Result<Type>.mapError(block: (Throwable) -> Throwable): Result<Type> =
+    fold(
+        onSuccess = { this },
+        onFailure = { Result.failure(block(it)) }
+    )
