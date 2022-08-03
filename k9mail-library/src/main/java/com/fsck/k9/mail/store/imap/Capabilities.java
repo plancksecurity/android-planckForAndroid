23a33f96c75aac8f292f4878f6c18fb22ccafce6
ignaciogarcia <ig@pep.security>
2022-08-03 14:30:51 +0200

    WIP

--- k9mail-library/src/main/java/com/fsck/k9/mail/store/imap/Capabilities.java
+++ k9mail-library/src/main/java/com/fsck/k9/mail/store/imap/Capabilities.java
@@ -3,8 +3,10 @@ package com.fsck.k9.mail.store.imap;
 
 class Capabilities {
     public static final String IDLE = "IDLE";
+    public static final String CONDSTORE = "CONDSTORE";
     public static final String SASL_IR = "SASL-IR";
     public static final String AUTH_XOAUTH2 = "AUTH=XOAUTH2";
+    public static final String AUTH_OAUTHBEARER = "AUTH=OAUTHBEARER";
     public static final String AUTH_CRAM_MD5 = "AUTH=CRAM-MD5";
     public static final String AUTH_PLAIN = "AUTH=PLAIN";
     public static final String AUTH_EXTERNAL = "AUTH=EXTERNAL";
@@ -13,4 +15,6 @@ class Capabilities {
     public static final String COMPRESS_DEFLATE = "COMPRESS=DEFLATE";
     public static final String STARTTLS = "STARTTLS";
     public static final String SPECIAL_USE = "SPECIAL-USE";
+    public static final String UID_PLUS = "UIDPLUS";
+    public static final String LIST_EXTENDED = "LIST-EXTENDED";
 }
