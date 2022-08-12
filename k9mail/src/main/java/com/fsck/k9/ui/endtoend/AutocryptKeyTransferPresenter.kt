b3a09f69acd7d43d9894a49f9ccc20bef5badb7e
ignaciogarcia <ig@pep.security>
2022-08-12 12:57:26 +0200

    PEPA-208 Get Oauth2TokenProvider from Account

--- k9mail/src/main/java/com/fsck/k9/ui/endtoend/AutocryptKeyTransferPresenter.kt
+++ k9mail/src/main/java/com/fsck/k9/ui/endtoend/AutocryptKeyTransferPresenter.kt
@@ -84,7 +84,8 @@ class AutocryptKeyTransferPresenter internal constructor(
         view.setLoadingStateSending()
         view.sceneGeneratingAndSending()
 
-        val transport = transportProvider.getTransport(context, account, K9.oAuth2TokenStore)
+        val transport = transportProvider
+            .getTransport(context, account, account.oAuth2TokenProvider)
         viewModel.autocryptSetupTransferLiveEvent.sendMessageAsync(transport, setupMsg)
     }
 
