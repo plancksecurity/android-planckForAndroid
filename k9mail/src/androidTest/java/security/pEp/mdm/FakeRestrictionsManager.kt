3b5f7bcd84592a440e7cb326b43e77d451b8aa54
juan <jgb@pep.security>
2022-08-22 15:50:36 +0200

    Add new restriction entry for allow_pep_sync_new_devices

--- k9mail/src/androidTest/java/security/pEp/mdm/FakeRestrictionsManager.kt
+++ k9mail/src/androidTest/java/security/pEp/mdm/FakeRestrictionsManager.kt
@@ -389,6 +389,11 @@ class FakeRestrictionsManager @Inject constructor() : RestrictionsProvider {
                         DEFAULT_ACCOUNT_STORE_MESSAGES_SECURELY
                     ),
                     RestrictionEntry(RESTRICTION_ACCOUNT_ENABLE_SYNC, DEFAULT_ACCOUNT_ENABLE_SYNC),
+                    RestrictionEntry(
+                        RESTRICTION_ALLOW_PEP_SYNC_NEW_DEVICES,
+                        DEFAULT_ALLOW_PEP_SYNC_NEW_DEVICES
+                    ),
+
                     getMailSettingsRestrictionEntry()
                 )
             } else emptyList()
