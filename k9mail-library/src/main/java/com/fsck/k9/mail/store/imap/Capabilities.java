9341da358409f76f2cbe2334409fcb070a360ae4
ignaciogarcia <ig@pep.security>
2022-08-29 08:32:34 +0200

    PEPA-208 Remove unused constants

--- k9mail-library/src/main/java/com/fsck/k9/mail/store/imap/Capabilities.java
+++ k9mail-library/src/main/java/com/fsck/k9/mail/store/imap/Capabilities.java
@@ -3,7 +3,6 @@ package com.fsck.k9.mail.store.imap;
 
 class Capabilities {
     public static final String IDLE = "IDLE";
-    public static final String CONDSTORE = "CONDSTORE";
     public static final String SASL_IR = "SASL-IR";
     public static final String AUTH_XOAUTH2 = "AUTH=XOAUTH2";
     public static final String AUTH_OAUTHBEARER = "AUTH=OAUTHBEARER";
@@ -15,6 +14,4 @@ class Capabilities {
     public static final String COMPRESS_DEFLATE = "COMPRESS=DEFLATE";
     public static final String STARTTLS = "STARTTLS";
     public static final String SPECIAL_USE = "SPECIAL-USE";
-    public static final String UID_PLUS = "UIDPLUS";
-    public static final String LIST_EXTENDED = "LIST-EXTENDED";
 }
