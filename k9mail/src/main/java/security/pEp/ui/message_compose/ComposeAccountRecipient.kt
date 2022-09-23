cacbebbbd330374bd9d1c4ed1ee937d470fc2b8b
ignaciogarcia <ig@pep.security>
2022-09-23 14:43:42 +0200

    Remove static view references in ComposeAccountRecipient

--- k9mail/src/main/java/security/pEp/ui/message_compose/ComposeAccountRecipient.kt
+++ k9mail/src/main/java/security/pEp/ui/message_compose/ComposeAccountRecipient.kt
@@ -3,10 +3,13 @@ package security.pEp.ui.message_compose
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.LinearLayout
-import kotlinx.android.synthetic.main.compose_account_item.view.*
+import android.widget.TextView
+import com.fsck.k9.R
 
 
 class ComposeAccountRecipient(context: Context?, attrs: AttributeSet?) : LinearLayout(context, attrs) {
+
+    private var accountName: TextView = findViewById(R.id.accountName)
 
     fun bindView(name: String) {
         accountName.text = name
