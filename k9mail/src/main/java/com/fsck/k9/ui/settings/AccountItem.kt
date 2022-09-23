f9a8a02687f9ce31978fda0a20d4b9df33ec1b01
ignaciogarcia <ig@pep.security>
2022-09-23 14:01:17 +0200

    Remove static view references in AccountItem

--- k9mail/src/main/java/com/fsck/k9/ui/settings/AccountItem.kt
+++ k9mail/src/main/java/com/fsck/k9/ui/settings/AccountItem.kt
@@ -1,17 +1,17 @@
 package com.fsck.k9.ui.settings
 
+import android.widget.TextView
 import com.fsck.k9.Account
 import com.fsck.k9.R
 import com.xwray.groupie.kotlinandroidextensions.Item
 import com.xwray.groupie.kotlinandroidextensions.ViewHolder
-import kotlinx.android.synthetic.main.account_list_item.*
 
 internal class AccountItem(val account: Account) : Item() {
 
     override fun getLayout(): Int = R.layout.account_list_item
 
     override fun bind(viewHolder: ViewHolder, position: Int) {
-        viewHolder.name.text = account.description
-        viewHolder.email.text = account.email
+        viewHolder.itemView.findViewById<TextView>(R.id.name).text = account.description
+        viewHolder.itemView.findViewById<TextView>(R.id.email).text = account.email
     }
 }
