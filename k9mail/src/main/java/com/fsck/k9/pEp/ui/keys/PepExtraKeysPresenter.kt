a661f1df3639d68c3c14b676f49ad237e3016916
ignaciogarcia <ig@pep.security>
2022-10-14 09:45:03 +0200

    PEMA-87 Save selected extra keys to disk on leaving screen
    Move dependencies from PepExtraKeys to PEpExtraKeysPresenter.

--- k9mail/src/main/java/com/fsck/k9/pEp/ui/keys/PepExtraKeysPresenter.kt
+++ k9mail/src/main/java/com/fsck/k9/pEp/ui/keys/PepExtraKeysPresenter.kt
@@ -1,5 +1,7 @@
 package com.fsck.k9.pEp.ui.keys
 
+import com.fsck.k9.K9
+import com.fsck.k9.Preferences
 import com.fsck.k9.pEp.DispatcherProvider
 import com.fsck.k9.pEp.PEpProvider
 import com.fsck.k9.pEp.ui.blacklist.KeyListItem
@@ -7,20 +9,44 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import java.util.*
 import javax.inject.Inject
+import javax.inject.Named
 
 class PepExtraKeysPresenter @Inject constructor(
-        private val dispatcherProvider: DispatcherProvider
+    @Named("MainUI") private val pEp: PEpProvider,
+    private val preferences: Preferences,
+    private val dispatcherProvider: DispatcherProvider,
 ) {
 
+    private lateinit var keys: MutableSet<String>
     private lateinit var view: PepExtraKeysView
-    private lateinit var pEp: PEpProvider
 
-    fun initialize(view: PepExtraKeysView, pEp: PEpProvider, keys: Set<String>) {
+    fun initialize(view: PepExtraKeysView) {
         this.view = view
-        this.pEp = pEp
+        keys = HashSet(K9.getMasterKeys())
         setupMasterKeys(keys)
+    }
+
+    fun addMasterKey(key: String) {
+        keys.add(key)
+        K9.setMasterKeys(keys)
+    }
+
+    fun removeMasterKey(key: String) {
+        keys.remove(key)
+        K9.setMasterKeys(keys)
+    }
+
+    fun onPause() {
+        CoroutineScope(dispatcherProvider.main() + SupervisorJob()).launch {
+            save()
+        }
+    }
+
+    private suspend fun save() = withContext(dispatcherProvider.io()) {
+        val editor = preferences.storage.edit()
+        K9.save(editor)
+        editor.commit()
     }
 
     private fun setupMasterKeys(keys: Set<String>) {
@@ -37,7 +63,8 @@ class PepExtraKeysPresenter @Inject constructor(
         }
     }
 
-    private suspend fun getMasterKeyInfo(): List<KeyListItem>? = withContext(dispatcherProvider.io()) {
-        pEp.masterKeysInfo
-    }
+    private suspend fun getMasterKeyInfo(): List<KeyListItem>? =
+        withContext(dispatcherProvider.io()) {
+            pEp.masterKeysInfo
+        }
 }
