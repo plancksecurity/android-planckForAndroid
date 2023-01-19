240083c4985fb6501f132d79b277245064ad4580
ignaciogarcia <ig@pep.security>
2023-01-19 11:55:41 +0100

    PEMA-182 Use Koin injection and cleanup

--- k9mail/src/main/java/com/fsck/k9/pEp/ui/fragments/AccountSetupBasicsFragment.kt
+++ k9mail/src/main/java/com/fsck/k9/pEp/ui/fragments/AccountSetupBasicsFragment.kt
@@ -1,48 +1,34 @@
 package com.fsck.k9.pEp.ui.fragments
 
 import android.app.Activity
-import android.app.Dialog
 import android.content.Intent
-import android.content.res.XmlResourceParser
-import android.net.Uri
 import android.os.Bundle
-import android.os.Handler
 import android.text.Editable
-import android.util.Log
 import android.view.*
 import android.widget.Button
 import android.widget.CheckBox
-import android.widget.CompoundButton
 import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
 import androidx.core.view.isVisible
 import androidx.core.widget.ContentLoadingProgressBar
-import butterknife.OnTextChanged
 import com.fsck.k9.*
-import com.fsck.k9.BuildConfig
-import com.fsck.k9.R
 import com.fsck.k9.account.AccountCreator
 import com.fsck.k9.activity.setup.AccountSetupBasics
-import com.fsck.k9.activity.setup.AccountSetupBasics.AccountSetupSettingsCheckerFragment
 import com.fsck.k9.activity.setup.AccountSetupCheckSettings
 import com.fsck.k9.activity.setup.AccountSetupCheckSettings.CheckDirection
 import com.fsck.k9.activity.setup.AccountSetupCheckSettings.Companion.actionCheckSettings
 import com.fsck.k9.activity.setup.AccountSetupNames
 import com.fsck.k9.activity.setup.OAuthFlowActivity
-import com.fsck.k9.activity.setup.OAuthFlowActivity.Companion.buildLaunchIntent
-import com.fsck.k9.auth.createOAuthConfigurationProvider
 import com.fsck.k9.autodiscovery.providersxml.ProvidersXmlDiscovery
-import com.fsck.k9.autodiscovery.providersxml.ProvidersXmlProvider
 import com.fsck.k9.helper.SimpleTextWatcher
-import com.fsck.k9.helper.UrlEncodingHelper
 import com.fsck.k9.helper.Utility
-import com.fsck.k9.mail.*
-import com.fsck.k9.mail.filter.Hex.encodeHex
+import com.fsck.k9.mail.AuthType
+import com.fsck.k9.mail.ConnectionSecurity
+import com.fsck.k9.mail.ServerSettings
+import com.fsck.k9.mail.Transport
 import com.fsck.k9.mail.store.RemoteStore
 import com.fsck.k9.pEp.PePUIArtefactCache
 import com.fsck.k9.pEp.ui.ConnectionSettings
-import com.fsck.k9.pEp.ui.infrastructure.exceptions.PEpCertificateException
-import com.fsck.k9.pEp.ui.infrastructure.exceptions.PEpSetupException
 import com.fsck.k9.pEp.ui.settings.ExtraAccountDiscovery
 import com.fsck.k9.pEp.ui.tools.AccountSetupNavigator
 import com.fsck.k9.pEp.ui.tools.FeedbackTools
@@ -50,31 +36,16 @@ import com.fsck.k9.pEp.ui.tools.SetupAccountType
 import com.fsck.k9.ui.getEnum
 import com.fsck.k9.ui.putEnum
 import com.fsck.k9.view.ClientCertificateSpinner
-import com.fsck.k9.view.ClientCertificateSpinner.OnClientCertificateChangedListener
+import org.koin.android.ext.android.inject
 import security.pEp.provisioning.ProvisioningSettings
-import security.pEp.provisioning.SimpleMailSettings
 import timber.log.Timber
-import java.net.URI
 import java.net.URISyntaxException
-import java.security.MessageDigest
-import java.security.NoSuchAlgorithmException
-import java.security.cert.CertificateEncodingException
-import java.security.cert.CertificateException
-import java.security.cert.X509Certificate
 import javax.inject.Inject
 
 class AccountSetupBasicsFragment : PEpFragment() {
-    private lateinit var providersXmlDiscovery: ProvidersXmlDiscovery
-
-    //private lateinit var emailView: TextInputEditText
-    //private lateinit var passwordView: TextInputEditText
-    //private lateinit var passwordLayout: View
-    //private lateinit var clientCertificateCheckBox: CheckBox
-    //private lateinit var clientCertificateSpinner: ClientCertificateSpinner
-    //private lateinit var advancedOptionsContainer: View
-    //private lateinit var nextButton: Button
-    //private lateinit var manualSetupButton: Button
-    //private lateinit var allowClientCertificateView: ViewGroup
+    private val providersXmlDiscovery: ProvidersXmlDiscovery by inject()
+    private val preferences: Preferences by inject()
+    private val emailValidator: EmailAddressValidator by inject()
 
     private lateinit var emailView: EditText
     private lateinit var passwordView: EditText
@@ -87,16 +58,12 @@ class AccountSetupBasicsFragment : PEpFragment() {
     private lateinit var passwordLayout: View
     private var uiState = UiState.EMAIL_ADDRESS_ONLY
     private var account: Account? = null
-    //private var mProvider: Provider? = null
-    private val emailValidator = EmailAddressValidator()
     private var checkedIncoming = false
     private lateinit var nextProgressBar: ContentLoadingProgressBar
     private lateinit var rootView: View
     private lateinit var accountSetupNavigator: AccountSetupNavigator
     private lateinit var pEpUIArtefactCache: PePUIArtefactCache
     private var errorDialog: AlertDialog? = null
-    private var errorDialogTitle = 0
-    private var errorDialogMessage: String? = null
     private var errorDialogWasShowing = false
     private var wasLoading = false
 
@@ -108,11 +75,6 @@ class AccountSetupBasicsFragment : PEpFragment() {
 
     @Inject
     lateinit var provisioningSettings: ProvisioningSettings
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        providersXmlDiscovery = ProvidersXmlDiscovery(ProvidersXmlProvider(requireContext()), createOAuthConfigurationProvider()) // TODO: fix injection
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -135,21 +97,13 @@ class AccountSetupBasicsFragment : PEpFragment() {
 
         manualSetupButton.setOnClickListener { onManualSetup() }
 
-
-
-
-        //mOAuth2CheckBox = rootView.findViewById(R.id.account_oauth2)
-
         nextProgressBar = rootView.findViewById(R.id.next_progressbar)
-
-        //nextButton.setOnClickListener(this)
-        //manualSetupButton.setOnClickListener(this)
 
         initializeViewListeners()
         validateFields()
         pEpUIArtefactCache = PePUIArtefactCache.getInstance(requireContext().applicationContext)
-        val email = pEpUIArtefactCache.getEmailInPreferences()
-        val password = pEpUIArtefactCache.getPasswordInPreferences()
+        val email = pEpUIArtefactCache.emailInPreferences
+        val password = pEpUIArtefactCache.passwordInPreferences
         if (email != null && password != null) {
             emailView.setText(email)
             passwordView.setText(password)
@@ -175,9 +129,9 @@ class AccountSetupBasicsFragment : PEpFragment() {
     }
 
     private fun updateUiFromProvisioningSettings() {
-        emailView.setText(provisioningSettings!!.email)
+        emailView.setText(provisioningSettings.email)
         emailView.isFocusable = false
-        val provisionSettings = provisioningSettings!!.provisionedMailSettings
+        val provisionSettings = provisioningSettings.provisionedMailSettings
         if (provisionSettings != null) {
             val isOAuth = (provisionSettings.incoming.authType === security.pEp.mdm.AuthType.XOAUTH2
                     && provisioningSettings.oAuthType != null)
@@ -247,21 +201,6 @@ class AccountSetupBasicsFragment : PEpFragment() {
         outState.putEnum(STATE_KEY_UI_STATE, uiState)
         outState.putString(EXTRA_ACCOUNT, account?.uuid)
         outState.putBoolean(STATE_KEY_CHECKED_INCOMING, checkedIncoming)
-        //if (account != null) {
-        //    outState.putString(EXTRA_ACCOUNT, account!!.uuid)
-        //}
-        //if (mProvider != null) {
-        //    outState.putSerializable(STATE_KEY_PROVIDER, mProvider)
-        //}
-        //outState.putBoolean(STATE_KEY_CHECKED_INCOMING, checkedIncoming)
-        //saveErrorDialogState(outState)
-        //outState.putBoolean(WAS_LOADING, wasLoading)
-    }
-
-    private fun saveErrorDialogState(outState: Bundle) {
-        outState.putBoolean(ERROR_DIALOG_SHOWING_KEY, errorDialogWasShowing)
-        outState.putInt(ERROR_DIALOG_TITLE, errorDialogTitle)
-        outState.putString(ERROR_DIALOG_MESSAGE, errorDialogMessage)
     }
 
     override fun onViewStateRestored(savedInstanceState: Bundle?) {
@@ -271,7 +210,7 @@ class AccountSetupBasicsFragment : PEpFragment() {
 
             val accountUuid = savedInstanceState.getString(EXTRA_ACCOUNT)
             if (accountUuid != null) {
-                account = Preferences.getPreferences(activity).getAccountAllowingIncomplete(accountUuid)
+                account = preferences.getAccountAllowingIncomplete(accountUuid)
             }
 
             checkedIncoming = savedInstanceState.getBoolean(STATE_KEY_CHECKED_INCOMING)
@@ -308,6 +247,7 @@ class AccountSetupBasicsFragment : PEpFragment() {
 
     private fun attemptAutoSetupUsingOnlyEmailAddress() {
         val email = emailView.text?.toString() ?: error("Email missing")
+        if (accountWasAlreadySet(email)) return
 
         val extraConnectionSettings = ExtraAccountDiscovery.discover(email)
         if (extraConnectionSettings != null) {
@@ -344,13 +284,14 @@ class AccountSetupBasicsFragment : PEpFragment() {
     }
 
     private fun attemptAutoSetup() {
+        val email = emailView.text?.toString() ?: error("Email missing")
+        if (accountWasAlreadySet(email)) return
+
         if (clientCertificateCheckBox.isChecked) {
             // Auto-setup doesn't support client certificates.
             onManualSetup()
             return
         }
-
-        val email = emailView.text?.toString() ?: error("Email missing")
 
         val extraConnectionSettings = ExtraAccountDiscovery.discover(email)
         if (extraConnectionSettings != null) {
@@ -371,7 +312,7 @@ class AccountSetupBasicsFragment : PEpFragment() {
         val account = createAccount(connectionSettings)
 
         // Check incoming here. Then check outgoing in onActivityResult()
-        AccountSetupCheckSettings.actionCheckSettings(this, account, CheckDirection.INCOMING)
+        actionCheckSettings(requireActivity(), account, CheckDirection.INCOMING)
     }
 
     private fun createAccount(connectionSettings: ConnectionSettings): Account {
@@ -402,11 +343,11 @@ class AccountSetupBasicsFragment : PEpFragment() {
     }
 
     private fun createAccount(): Account {
-        return Preferences.getPreferences(requireActivity()).newAccount()
+        return preferences.newAccount()
     }
 
     private fun getOwnerName(): String {
-        return Preferences.getPreferences(requireActivity()).defaultAccount?.name ?: ""
+        return preferences.defaultAccount?.name ?: ""
     }
 
     private fun providersXmlDiscoveryDiscover(email: String): ConnectionSettings? {
@@ -442,7 +383,6 @@ class AccountSetupBasicsFragment : PEpFragment() {
         if (resultCode != Activity.RESULT_OK) return
 
         checkNotNull(account) { "Account instance missing" }
-        //showLoading(true)
         if (!checkedIncoming) {
             // We've successfully checked incoming. Now check outgoing.
             checkedIncoming = true
@@ -451,7 +391,6 @@ class AccountSetupBasicsFragment : PEpFragment() {
             // We've successfully checked outgoing as well.
             AccountSetupNames.actionSetNames(requireActivity(), account, false)
         }
-        //showLoading(false)
     }
 
     private fun handleSignInResult(resultCode: Int) {
@@ -463,7 +402,6 @@ class AccountSetupBasicsFragment : PEpFragment() {
 
     private fun checkSettings(direction: CheckDirection = CheckDirection.INCOMING) {
         actionCheckSettings(requireActivity(), account!!, direction)
-        //showLoading(false)
     }
 
     private fun saveCredentialsInPreferences() {
@@ -478,45 +416,21 @@ class AccountSetupBasicsFragment : PEpFragment() {
         accountSetupNavigator = (activity as AccountSetupBasics?)!!.accountSetupNavigator
         accountSetupNavigator.setCurrentStep(AccountSetupNavigator.Step.BASICS, account)
         validateFields()
-        //restoreErrorDialogIfNeeded()
-        //restoreViewsEnabledState()
     }
 
-    private fun restoreViewsEnabledState() {
-        //showLoading(wasLoading)
-        wasLoading = false
-    }
-
-    private fun isAValidAddress(email: String): Boolean {
-        return avoidAddingAlreadyExistingAccount(email) ||
-                isEmailNull(email)
-    }
-
-    private fun isEmailNull(email: String?): Boolean {
-        if (email == null || email.isEmpty()) {
-            resetView("You must enter an email address")
-            return true
-        }
-        return false
-    }
-
-    private fun avoidAddingAlreadyExistingAccount(email: String): Boolean {
+    private fun accountWasAlreadySet(email: String): Boolean {
         if (accountAlreadyExists(email)) {
-            resetView(getString(R.string.account_already_exists))
+            showFeedback(getString(R.string.account_already_exists))
             return true
         }
         return false
     }
 
-    private fun resetView(feedback: String) {
+    private fun showFeedback(feedback: String) {
         FeedbackTools.showLongFeedback(view, feedback)
-        //nextProgressBar.hide()
-        //nextButton.visibility = View.VISIBLE
-        //enableViewGroup(true, rootView as ViewGroup?)
     }
 
     private fun accountAlreadyExists(email: String): Boolean {
-        val preferences = Preferences.getPreferences(activity)
         val accounts = preferences.accounts
         for (account in accounts) {
             if (account.email.equals(email, ignoreCase = true)) {
@@ -526,19 +440,7 @@ class AccountSetupBasicsFragment : PEpFragment() {
         return false
     }
 
-    private fun enableViewGroup(enable: Boolean, viewGroup: ViewGroup?) {
-        for (i in 0 until viewGroup!!.childCount) {
-            val child = viewGroup.getChildAt(i)
-            if (child is ViewGroup) {
-                enableViewGroup(enable, child)
-            } else {
-                child.isEnabled = enable
-            }
-        }
-    }
-
     private fun goForward() {
-        //showLoading(false)
         try {
             setupAccountType.setupStoreAndSmtpTransport(
                 account,
@@ -554,20 +456,12 @@ class AccountSetupBasicsFragment : PEpFragment() {
     private fun onManualSetup() {
         (activity as AccountSetupBasics?)!!.setManualSetupRequired(true)
         val email = emailView.text?.toString() ?: error("Email missing")
-        if (!emailValidator.isValidAddressOnly(email)) {
+        if (accountWasAlreadySet(email)) {
             return
         }
         var password: String? = null
         var clientCertificateAlias: String? = null
-        var authenticationType: AuthType
-
-        //if (clientCertificateCheckBox.isChecked) {
-        //    clientCertificateAlias = clientCertificateSpinner.alias
-        //    if (password.isNullOrEmpty()) {
-        //        authenticationType = AuthType.EXTERNAL
-        //        password = null
-        //    }
-        //}
+        val authenticationType: AuthType
 
         val account = initAccount(email)
 
@@ -587,7 +481,6 @@ class AccountSetupBasicsFragment : PEpFragment() {
             authenticationType = AuthType.PLAIN
             password = passwordView.text.toString()
         }
-        //initializeAccount()
         account.name = getOwnerName()
         account.email = email
 
@@ -622,15 +515,6 @@ class AccountSetupBasicsFragment : PEpFragment() {
         goForward()
     }
 
-//    private fun initializeAccount() {
-//        if (account == null || Preferences.getPreferences(activity).getAccountAllowingIncomplete(
-//                account!!.uuid
-//            ) == null
-//        ) {
-//            account = Preferences.getPreferences(activity).newAccount()
-//        }
-//    }
-
     private fun setupFolderNames(domain: String?) {
         account!!.draftsFolderName = getString(R.string.special_mailbox_name_drafts)
         account!!.trashFolderName = getString(R.string.special_mailbox_name_trash)
@@ -648,22 +532,13 @@ class AccountSetupBasicsFragment : PEpFragment() {
     private fun splitEmail(email: String): Array<String?> {
         val retParts = arrayOfNulls<String>(2)
         val emailParts = email.split("@").toTypedArray()
-        retParts[0] = if (emailParts.size > 0) emailParts[0] else ""
+        retParts[0] = if (emailParts.isNotEmpty()) emailParts[0] else ""
         retParts[1] = if (emailParts.size > 1) emailParts[1] else ""
         return retParts
     }
 
     override fun injectFragment() {
         getpEpComponent().inject(this)
-    }
-
-    private fun showErrorDialog(stringResource: Int, message: String?) {
-        errorDialogTitle = stringResource
-        errorDialogMessage = message
-        errorDialog = AlertDialog.Builder(requireActivity())
-            .setTitle(resources.getString(stringResource))
-            .setMessage(message)
-            .show()
     }
 
     override fun onPause() {
@@ -690,17 +565,9 @@ class AccountSetupBasicsFragment : PEpFragment() {
         private const val ACTIVITY_REQUEST_PICK_SETTINGS_FILE = 0
         private const val EXTRA_ACCOUNT = "com.fsck.k9.AccountSetupBasics.account"
         private const val STATE_KEY_UI_STATE = "com.fsck.k9.AccountSetupBasics.uiState"
-        private const val DIALOG_NOTE = 1
-        private const val STATE_KEY_PROVIDER = "com.fsck.k9.AccountSetupBasics.provider"
         private const val STATE_KEY_CHECKED_INCOMING =
             "com.fsck.k9.AccountSetupBasics.checkedIncoming"
-        const val GMAIL = "gmail"
-        private const val ERROR_DIALOG_SHOWING_KEY = "errorDialogShowing"
-        private const val ERROR_DIALOG_TITLE = "errorDialogTitle"
-        private const val ERROR_DIALOG_MESSAGE = "errorDialogMessage"
-        private const val WAS_LOADING = "wasLoading"
         private const val REQUEST_CODE_OAUTH = Activity.RESULT_FIRST_USER + 1
-        private const val GMAIL_DOMAIN = "gmail.com"
         private const val REQUEST_CODE_CHECK_SETTINGS = AccountSetupCheckSettings.ACTIVITY_REQUEST_CODE
     }
 }
