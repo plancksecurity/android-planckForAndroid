391edefc9cdb04356822a7543792f6b7a5902a9a
ignaciogarcia <ig@pep.security>
2023-02-02 09:04:29 +0100

    PEMA-193 Add method in MediaStoreUtils.kt

--- k9mail/src/main/java/com/fsck/k9/pEp/MediaStoreUtils.kt
+++ k9mail/src/main/java/com/fsck/k9/pEp/MediaStoreUtils.kt
@@ -74,3 +74,67 @@ fun InputStream.saveToDocuments(
         outputStream?.close()
     }
 }
+
+@RequiresApi(Build.VERSION_CODES.Q)
+@Throws(IOException::class)
+fun InputStream.saveToDownloads(
+    context: Context,
+    mimeType: String,
+    displayName: String,
+    subFolder: String? = null,
+): Uri? {
+    val inputStream = this
+    var relativeLocation = Environment.DIRECTORY_DOWNLOADS
+    if (!subFolder.isNullOrEmpty()) {
+        relativeLocation += File.separator + subFolder
+    }
+    val contentValues = ContentValues()
+    contentValues.put(MediaStore.Downloads.DISPLAY_NAME, displayName)
+    contentValues.put(MediaStore.Downloads.MIME_TYPE, mimeType)
+    contentValues.put(MediaStore.Downloads.RELATIVE_PATH, relativeLocation)
+    contentValues.put(MediaStore.Downloads.TITLE, displayName)
+    contentValues.put(MediaStore.Downloads.DATE_MODIFIED, System.currentTimeMillis())
+
+    val resolver: ContentResolver = context.contentResolver
+    var outputStream: OutputStream? = null
+    var uri: Uri? = null
+    try {
+        val contentUri: Uri = MediaStore.Downloads.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
+        uri = resolver.insert(contentUri, contentValues) ?: let {
+            Timber.e("Error: insertion query in Downloads collection returned null")
+            return null
+        }
+        Timber.d("inserted uri in Downloads collection is $uri")
+
+        try {
+            val pfd = context.contentResolver.openFileDescriptor(uri, "w")
+                ?: throw IllegalStateException("Error: parcel file descriptor is null!!")
+
+            val out = FileOutputStream(pfd.fileDescriptor)
+            val buffer = ByteArray(4 * 1024)
+            var length: Int
+            while (inputStream.read(buffer).also { length = it } > 0) {
+                out.write(buffer, 0, length)
+            }
+            out.close()
+            inputStream.close()
+            pfd.close()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            throw e
+        }
+        outputStream = resolver.openOutputStream(uri)
+        if (outputStream == null) {
+            throw IOException("Failed to get output stream.")
+        }
+        return uri
+    } catch (e: IOException) {
+        // Don't leave an orphan entry in the MediaStore
+        uri?.let {
+            resolver.delete(it, null, null)
+        }
+        throw e
+    } finally {
+        outputStream?.close()
+    }
+}
