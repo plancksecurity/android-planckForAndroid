c69624a4ec51f53aa3f7b0f7b924ad177d1d0882
ignaciogarcia <ig@pep.security>
2023-02-03 13:19:49 +0100

    PEMA-193 Use DownloadManager to save linked images
    Based on commit k9/ffc69c9

--- k9mail/src/main/java/com/fsck/k9/ui/messageview/MessageContainerView.java
+++ k9mail/src/main/java/com/fsck/k9/ui/messageview/MessageContainerView.java
@@ -1,8 +1,11 @@
 package com.fsck.k9.ui.messageview;
 
 
+import static android.app.DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED;
+
 import android.animation.LayoutTransition;
 import android.app.Activity;
+import android.app.DownloadManager;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
@@ -201,8 +204,7 @@ public class MessageContainerView extends LinearLayout implements OnLayoutChange
                                 if (inlineImage) {
                                     attachmentCallback.onSaveAttachment(attachmentViewInfo);
                                 } else {
-                                    //TODO: Use download manager
-                                    new DownloadImageTask(getContext()).execute(uri.toString());
+                                    downloadImage(uri);
                                 }
                                 break;
                             }
@@ -329,6 +331,14 @@ public class MessageContainerView extends LinearLayout implements OnLayoutChange
                 break;
             }
         }
+    }
+
+    private void downloadImage(Uri uri) {
+        DownloadManager.Request request = new DownloadManager.Request(uri);
+        request.setNotificationVisibility(VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+
+        DownloadManager downloadManager = (DownloadManager) getContext().getSystemService(Context.DOWNLOAD_SERVICE);
+        downloadManager.enqueue(request);
     }
 
     private AttachmentViewInfo getAttachmentViewInfoIfCidUri(Uri uri) {
