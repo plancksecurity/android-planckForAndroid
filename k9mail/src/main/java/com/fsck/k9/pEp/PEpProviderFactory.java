26c0ae2946d1f5ff0f1da8336da030e417f2b6f7
ignaciogarcia <ig@pep.security>
2023-03-07 11:52:01 +0100

    PEMA-373 Refactor PEpProviderFactory to use new constructor

--- k9mail/src/main/java/com/fsck/k9/pEp/PEpProviderFactory.java
+++ k9mail/src/main/java/com/fsck/k9/pEp/PEpProviderFactory.java
@@ -2,10 +2,9 @@ package com.fsck.k9.pEp;
 
 import android.content.Context;
 
-import com.fsck.k9.pEp.infrastructure.threading.JobExecutor;
 import com.fsck.k9.pEp.infrastructure.threading.PostExecutionThread;
-import com.fsck.k9.pEp.infrastructure.threading.ThreadExecutor;
 import com.fsck.k9.pEp.infrastructure.threading.UIThread;
+import com.fsck.k9.pEp.threads.EngineThreadLocal;
 
 /**
  * Factory for real providers
@@ -14,15 +13,9 @@ public class PEpProviderFactory {
     // FIXME: a little ugly, pep engine needs ctx once but I don't have it everywhere. Hopefully the following will work...
     // (I cache the context, but am not complely sure, wether it becomes invalid once...)
 
-    static public PEpProvider createAndSetupProvider(Context ctx) {
-        PEpProvider pEpProvider = createProvider(ctx);
-        pEpProvider.setup();
-        return pEpProvider;
-    }
-
     static public PEpProvider createProvider(Context context) {
-        ThreadExecutor threadExecutor = new JobExecutor();
         PostExecutionThread postExecutionThread = new UIThread();
-        return new PEpProviderImplKotlin(threadExecutor, postExecutionThread, context);
+        EngineThreadLocal engineThreadLocal = new EngineThreadLocal(context);
+        return new PEpProviderImplKotlin(postExecutionThread, context, engineThreadLocal);
     }
 }
