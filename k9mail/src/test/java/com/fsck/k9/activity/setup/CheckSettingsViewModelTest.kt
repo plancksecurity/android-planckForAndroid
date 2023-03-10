c48f77bf67210f8028a5418109c8a987132068eb
ignaciogarcia <ig@pep.security>
2023-03-10 09:16:22 +0100

    PEMA-361 Add CheckSettingsViewModelTest

---
+++ k9mail/src/test/java/com/fsck/k9/activity/setup/CheckSettingsViewModelTest.kt
@@ -0,0 +1,175 @@
+package com.fsck.k9.activity.setup
+
+import android.content.Context
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.fsck.k9.Account
+import com.fsck.k9.RobolectricTest
+import com.fsck.k9.activity.setup.AccountSetupCheckSettings.CheckDirection
+import com.fsck.k9.activity.setup.CheckSettingsState.*
+import com.fsck.k9.pEp.testutils.CoroutineTestRule
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import junit.framework.TestCase.*
+import kotlinx.coroutines.*
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestRule
+import security.pEp.serversettings.ServerSettingsChecker
+
+@ExperimentalCoroutinesApi
+class CheckSettingsViewModelTest : RobolectricTest() {
+    @get:Rule
+    var rule: TestRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    var coroutinesTestRule = CoroutineTestRule()
+
+    private val serverSettingsChecker: ServerSettingsChecker = mockk()
+    private val context: Context = mockk()
+    private val account: Account = mockk()
+
+    private val viewModel: CheckSettingsViewModel = CheckSettingsViewModel(
+        serverSettingsChecker,
+        coroutinesTestRule.testDispatcherProvider
+    )
+    private val receivedUiStates = mutableListOf<CheckSettingsState>()
+
+    @Before
+    fun setUp() {
+        receivedUiStates.clear()
+        observeViewModel(viewModel)
+        assertEquals(listOf(Idle), receivedUiStates)
+    }
+
+    @Test
+    fun `start() checks settings using ServerSettingsChecker`() {
+        coEvery { serverSettingsChecker.checkServerSettings(context, account, any(), any()) }
+            .returns(Result.success(Unit))
+
+
+        viewModel.start(context, account, CheckDirection.INCOMING, true)
+
+
+        coVerify {
+            serverSettingsChecker.checkServerSettings(
+                context,
+                account,
+                CheckDirection.INCOMING,
+                true
+            )
+        }
+    }
+
+    @Test
+    fun `start() sets state to CheckingIncoming if checking incoming settings`() {
+        coEvery { serverSettingsChecker.checkServerSettings(context, account, any(), any()) }
+            .returns(Result.success(Unit))
+
+
+        viewModel.start(
+            context, account,
+            CheckDirection.INCOMING, true
+        )
+
+
+        assertTrue(CheckingIncoming in receivedUiStates)
+    }
+
+    @Test
+    fun `start() sets state to CheckingOutgoing if checking outgoing settings`() {
+        coEvery { serverSettingsChecker.checkServerSettings(context, account, any(), any()) }
+            .returns(Result.success(Unit))
+
+
+        viewModel.start(
+            context, account,
+            CheckDirection.OUTGOING, true
+        )
+
+
+        assertTrue(CheckingOutgoing in receivedUiStates)
+    }
+
+    @Test
+    fun `start() sets state to Success if settings check is successful`() {
+        coEvery { serverSettingsChecker.checkServerSettings(context, account, any(), any()) }
+            .returns(Result.success(Unit))
+
+
+        viewModel.start(
+            context, account,
+            CheckDirection.INCOMING, true
+        )
+
+
+        assertEquals(
+            listOf(
+                Idle,
+                CheckingIncoming,
+                Success
+            ),
+            receivedUiStates
+        )
+    }
+
+    @Test
+    fun `start() sets state to Error if there was an error checking settings`() {
+        coEvery { serverSettingsChecker.checkServerSettings(context, account, any(), any()) }
+            .returns(Result.failure(TestException("test")))
+
+
+        viewModel.start(
+            context, account,
+            CheckDirection.INCOMING, true
+        )
+
+
+        assertEquals(
+            listOf(
+                Idle,
+                CheckingIncoming,
+                Error(TestException("test"))
+            ),
+            receivedUiStates
+        )
+    }
+
+    @Test
+    fun `cancel() cancels start() operation`() {
+        coEvery { serverSettingsChecker.checkServerSettings(context, account, any(), any()) }
+            .coAnswers {
+                runBlocking {
+                    delay(200)
+                    Result.success(Unit)
+                }
+            }
+
+
+        CoroutineScope(Dispatchers.IO).launch {
+            viewModel.cancel()
+        }
+        viewModel.start(
+            context, account,
+            CheckDirection.INCOMING, true
+        )
+
+
+        assertNotFinished()
+    }
+
+    private fun assertNotFinished() {
+        assertFalse(receivedUiStates.any { it == Success })
+        assertFalse(receivedUiStates.any { it is Error })
+    }
+
+    private fun observeViewModel(viewModel: CheckSettingsViewModel) {
+        viewModel.state.observeForever { value ->
+            receivedUiStates.add(value)
+        }
+    }
+
+    private data class TestException(override val message: String) : Exception(message)
+}
+
