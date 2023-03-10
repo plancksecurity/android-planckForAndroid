def3ce4d63b5289f19aa85ee8487e8142b43b01b
ignaciogarcia <ig@pep.security>
2023-03-10 09:07:37 +0100

    PEMA-361 Update CoroutineTestRule

--- k9mail/src/test/java/com/fsck/k9/pEp/testutils/CoroutineTestRule.kt
+++ k9mail/src/test/java/com/fsck/k9/pEp/testutils/CoroutineTestRule.kt
@@ -4,14 +4,12 @@ import com.fsck.k9.pEp.DispatcherProvider
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.TestCoroutineDispatcher
-import kotlinx.coroutines.test.resetMain
-import kotlinx.coroutines.test.setMain
+import kotlinx.coroutines.test.*
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 
 @ExperimentalCoroutinesApi
-class CoroutineTestRule(val testDispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()) : TestWatcher() {
+class CoroutineTestRule(val testDispatcher: TestDispatcher = StandardTestDispatcher()) : TestWatcher() {
 
     val testDispatcherProvider = object : DispatcherProvider {
         override fun default(): CoroutineDispatcher = testDispatcher
@@ -20,14 +18,13 @@ class CoroutineTestRule(val testDispatcher: TestCoroutineDispatcher = TestCorout
         override fun unconfined(): CoroutineDispatcher = testDispatcher
     }
 
-    override fun starting(description: Description?) {
+    override fun starting(description: Description) {
         super.starting(description)
         Dispatchers.setMain(testDispatcher)
     }
 
-    override fun finished(description: Description?) {
+    override fun finished(description: Description) {
         super.finished(description)
         Dispatchers.resetMain()
-        testDispatcher.cleanupTestCoroutines()
     }
 }
