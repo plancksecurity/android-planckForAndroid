4d8654bff5bc948352bb522786ddfcfab7763cc2
ignaciogarcia <ig@pep.security>
2023-03-17 07:35:57 +0100

    PEMA-408 Add 2 stress tests to evaluate performance

---
+++ k9mail/src/androidTest/java/com/fsck/k9/pEp/ui/activities/ManyMailsTest.kt
@@ -0,0 +1,52 @@
+package com.fsck.k9.pEp.ui.activities
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.fsck.k9.R
+import com.fsck.k9.common.BaseAndroidTest
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+private const val MESSAGE_SUBJECT = "subject"
+private const val MESSAGE_BODY = "body"
+
+@RunWith(AndroidJUnit4::class)
+class ManyMailsTest : BaseAndroidTest() {
+
+    @Before
+    fun startpEpApp() {
+        testUtils.setupAccountIfNeeded()
+        testUtils.doWaitForResource(R.id.message_list)
+    }
+
+    @Test
+    fun runTest() {
+        sendManyMails()
+    }
+
+    private fun sendManyMails() {
+        while (true) {
+            sendMesssageToMyself()
+            TestUtils.waitForIdle()
+        }
+    }
+
+    private fun sendMesssageToMyself() {
+        testUtils.composeMessageButton()
+        fillMessage()
+        testUtils.sendMessage()
+    }
+
+    private fun fillMessage() {
+        val email = testUtils.getTextFromTextViewThatContainsText("@")
+        testUtils.fillMessage(
+            TestUtils.BasicMessage(
+                email,
+                MESSAGE_SUBJECT,
+                MESSAGE_BODY,
+                email
+            ),
+            false
+        )
+    }
+}
