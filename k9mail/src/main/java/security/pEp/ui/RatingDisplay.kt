d3a11db2e8bca75387e95ccf124e7a6207f0df56
ignaciogarcia <ig@pep.security>
2023-03-21 15:59:15 +0100

    PEMA-395 Add RatingDisplay and implementation

---
+++ k9mail/src/main/java/security/pEp/ui/RatingDisplay.kt
@@ -0,0 +1,30 @@
+package security.pEp.ui
+
+import com.fsck.k9.R
+import com.fsck.k9.pEp.PEpUtils
+import foundation.pEp.jniadapter.Rating
+
+abstract class RatingDisplay {
+    protected abstract val ratingDisplayHolderList: List<RatingDisplayHolder>
+
+    fun getForRating(rating: Rating?, pEpEnabled: Boolean = true): RatingDisplayHolder {
+        val originalHolder = ratingDisplayHolderList.first { it.matchesRating(rating) }
+        return when {
+            rating == null
+                    || pEpEnabled
+                    || PEpUtils.isRatingUnsecure(rating) -> originalHolder
+            else -> {
+                originalHolder.copy(
+                    textRes = DISABLED_TEXT_RESOURCE,
+                    colorRes = DISABLED_COLOR
+                )
+            }
+        }
+    }
+
+    companion object {
+        fun getInstance(): RatingDisplay = RatingDisplayImpl()
+        private const val DISABLED_TEXT_RESOURCE = R.string.pep_rating_forced_unencrypt
+        private const val DISABLED_COLOR = R.color.pep_no_color
+    }
+}
