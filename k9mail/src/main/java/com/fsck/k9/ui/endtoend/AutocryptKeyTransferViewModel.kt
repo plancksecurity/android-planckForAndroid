97ac7c4c9de0b41a84bdf07337a54f018151ce54
ignaciogarcia <ig@pep.security>
2023-05-10 14:33:40 +0200

    EFA-91 Inject AutocryptKeyTransferViewModel

--- k9mail/src/main/java/com/fsck/k9/ui/endtoend/AutocryptKeyTransferViewModel.kt
+++ k9mail/src/main/java/com/fsck/k9/ui/endtoend/AutocryptKeyTransferViewModel.kt
@@ -1,7 +1,10 @@
 package com.fsck.k9.ui.endtoend
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-internal class AutocryptKeyTransferViewModel(
+@HiltViewModel
+internal class AutocryptKeyTransferViewModel @Inject constructor(
         val autocryptSetupMessageLiveEvent: AutocryptSetupMessageLiveEvent,
         val autocryptSetupTransferLiveEvent: AutocryptSetupTransferLiveEvent) : ViewModel()
