3cc3ad2cc232952bacc2a023ad80dc93da91fd88
ignaciogarcia <ig@pep.security>
2023-05-10 13:33:36 +0200

    EFA-91 Inject FolderRepositoryManager
    Make it singleton following previous behavior using Koin.

--- k9mail/src/main/java/com/fsck/k9/mailstore/FolderRepositoryManager.kt
+++ k9mail/src/main/java/com/fsck/k9/mailstore/FolderRepositoryManager.kt
@@ -2,7 +2,9 @@ package com.fsck.k9.mailstore
 
 import com.fsck.k9.Account
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class FolderRepositoryManager @Inject constructor() {
     fun getFolderRepository(account: Account) = FolderRepository(account)
 }
