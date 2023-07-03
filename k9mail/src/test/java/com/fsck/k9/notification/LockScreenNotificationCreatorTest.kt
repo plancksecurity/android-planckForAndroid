17e35ca1c22267d2e0880e477bf96a2f75becd87
ignaciogarcia <ig@pep.security>
2023-07-03 16:14:20 +0200

    EFA-34 Add LockScreenNotificationCreatorTest

---
+++ k9mail/src/test/java/com/fsck/k9/notification/LockScreenNotificationCreatorTest.kt
@@ -0,0 +1,125 @@
+package com.fsck.k9.notification
+
+import androidx.core.app.NotificationCompat
+import com.fsck.k9.Account
+import com.fsck.k9.MockHelper.mockBuilder
+import com.fsck.k9.RobolectricTest
+import org.junit.Test
+import org.mockito.Mockito.verify
+import org.mockito.kotlin.any
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
+
+private const val ACCOUNT_NUMBER = 1
+private const val ACCOUNT_NAME = "TestAccount"
+
+class LockScreenNotificationCreatorTest : RobolectricTest() {
+    private val account = createFakeAccount()
+    private val resourceProvider = TestNotificationResourceProvider()
+    private val builder = createFakeNotificationBuilder()
+    private val publicBuilder = createFakeNotificationBuilder()
+    private val notificationHelper = createFakeNotificationHelper(publicBuilder)
+    private var notificationCreator = LockScreenNotificationCreator(
+        notificationHelper = notificationHelper,
+        resourceProvider = resourceProvider
+    )
+
+    @Test
+    fun `no lock screen notification`() {
+        val baseNotificationData = createBaseNotificationData(LockScreenNotificationData.None)
+
+        notificationCreator.configureLockScreenNotification(builder, baseNotificationData)
+
+        verify(builder).setVisibility(NotificationCompat.VISIBILITY_SECRET)
+    }
+
+    @Test
+    fun `app name`() {
+        val baseNotificationData = createBaseNotificationData(LockScreenNotificationData.AppName)
+
+        notificationCreator.configureLockScreenNotification(builder, baseNotificationData)
+
+        verify(builder).setVisibility(NotificationCompat.VISIBILITY_PRIVATE)
+    }
+
+    @Test
+    fun `regular notification on lock screen`() {
+        val baseNotificationData = createBaseNotificationData(LockScreenNotificationData.Public)
+
+        notificationCreator.configureLockScreenNotification(builder, baseNotificationData)
+
+        verify(builder).setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+    }
+
+    @Test
+    fun `list of sender names`() {
+        val baseNotificationData = createBaseNotificationData(
+            lockScreenNotificationData = LockScreenNotificationData.SenderNames("Alice, Bob"),
+            newMessagesCount = 2
+        )
+
+        notificationCreator.configureLockScreenNotification(builder, baseNotificationData)
+
+        verify(publicBuilder).setSmallIcon(resourceProvider.iconNewMail)
+        verify(publicBuilder).setNumber(2)
+        verify(publicBuilder).setContentTitle("2 new messages")
+        verify(publicBuilder).setContentText("Alice, Bob")
+        verify(builder).setPublicVersion(publicBuilder.build())
+    }
+
+    @Test
+    fun `new message count`() {
+        val baseNotificationData = createBaseNotificationData(
+            lockScreenNotificationData = LockScreenNotificationData.MessageCount,
+            accountName = "Account name",
+            newMessagesCount = 23
+        )
+
+        notificationCreator.configureLockScreenNotification(builder, baseNotificationData)
+
+        verify(publicBuilder).setSmallIcon(resourceProvider.iconNewMail)
+        verify(publicBuilder).setNumber(23)
+        verify(publicBuilder).setContentTitle("23 new messages")
+        verify(publicBuilder).setContentText("Account name")
+        verify(builder).setPublicVersion(publicBuilder.build())
+    }
+
+    private fun createFakeNotificationHelper(builder: NotificationCompat.Builder): NotificationHelper {
+        return mock {
+            on { createNotificationBuilder(any(), any()) } doReturn builder
+        }
+    }
+
+    private fun createFakeNotificationBuilder(): NotificationCompat.Builder {
+        return mockBuilder {
+            on { build() } doReturn mock()
+        }
+    }
+
+    private fun createFakeAccount(): Account {
+        return mock {
+            on { accountNumber } doReturn ACCOUNT_NUMBER
+            on { name } doReturn ACCOUNT_NAME
+        }
+    }
+
+    private fun createBaseNotificationData(
+        lockScreenNotificationData: LockScreenNotificationData,
+        accountName: String = "irrelevant",
+        newMessagesCount: Int = 0
+    ): BaseNotificationData {
+        return BaseNotificationData(
+            account = account,
+            accountName = accountName,
+            groupKey = "irrelevant",
+            color = 0,
+            notificationsCount = newMessagesCount,
+            lockScreenNotificationData = lockScreenNotificationData,
+            appearance = NotificationAppearance(
+                ringtone = null,
+                vibrationPattern = longArrayOf(),
+                ledColor = 0
+            )
+        )
+    }
+}
