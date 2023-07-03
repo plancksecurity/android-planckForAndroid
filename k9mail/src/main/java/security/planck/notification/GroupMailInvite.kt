f7944e4310bf49e0903648322e22a3602d2309ec
ignaciogarcia <ig@pep.security>
2023-07-03 06:29:57 +0200

    EFA-34 Notify on sync signal

--- k9mail/src/main/java/security/planck/notification/GroupMailInvite.kt
+++ k9mail/src/main/java/security/planck/notification/GroupMailInvite.kt
@@ -1,10 +1,25 @@
 package security.planck.notification
 
 
+import com.fsck.k9.Account
 import com.fsck.k9.notification.NotificationReference
+import foundation.pEp.jniadapter.Identity
 
 data class GroupMailInvite(
     val groupAddress: String,
     val senderAddress: String,
     val accountUuid: String,
-): NotificationReference
+) : NotificationReference {
+    companion object {
+        @JvmStatic
+        fun fromSignal(
+            groupIdentity: Identity,
+            senderIdentity: Identity,
+            account: Account
+        ): GroupMailInvite = GroupMailInvite(
+            groupIdentity.address,
+            senderIdentity.address,
+            account.uuid
+        )
+    }
+}
