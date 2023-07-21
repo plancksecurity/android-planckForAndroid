d8ee2fe0c8dc12d422f7ffdcee86d01223660998
ignaciogarcia <ig@pep.security>
2023-07-21 06:44:45 +0200

    EFA-179 Extract methods in PlanckSyncWizard

--- k9mail/src/main/java/com/fsck/k9/planck/manualsync/PlanckSyncWizard.kt
+++ k9mail/src/main/java/com/fsck/k9/planck/manualsync/PlanckSyncWizard.kt
@@ -3,6 +3,7 @@ package com.fsck.k9.planck.manualsync
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import androidx.activity.viewModels
@@ -10,10 +11,13 @@ import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
+import com.fsck.k9.BuildConfig
 import com.fsck.k9.R
 import com.fsck.k9.databinding.ActivityImportWizzardFromPgpBinding
 import com.fsck.k9.planck.ui.tools.ThemeManager
 import dagger.hilt.android.AndroidEntryPoint
+
+private const val NO_RESOURCE = 0
 
 @AndroidEntryPoint
 class PlanckSyncWizard : WizardActivity() {
@@ -30,6 +34,13 @@ class PlanckSyncWizard : WizardActivity() {
         observeViewModel()
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        if (viewModel.isHandshaking()) {
+            menuInflater.inflate(R.menu.menu_add_device, menu)
+        }
+        return true
+    }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.english -> viewModel.changeTrustwordsLanguage(0)
@@ -39,7 +50,28 @@ class PlanckSyncWizard : WizardActivity() {
     }
 
     private fun setupViews() {
+        binding.dissmissActionButton.setOnClickListener {
+            viewModel.cancelHandshake()
+            finish()
+        }
+        binding.negativeActionButton.setOnClickListener {
+            viewModel.rejectHandshake()
+            finish()
+        }
+        binding.showLongTrustwords.setOnClickListener {
+            binding.showLongTrustwords.isVisible = false
+            viewModel.switchTrustwordsLength()
+        }
 
+        binding.trustwords.setOnLongClickListener {
+            viewModel.switchTrustwordsLength()
+            true
+        }
+        binding.afirmativeActionButton.setTextColor(
+            ThemeManager.getColorFromAttributeResource(
+                this, R.attr.defaultColorOnBackground
+            )
+        )
     }
 
     private fun observeViewModel() {
@@ -50,11 +82,11 @@ class PlanckSyncWizard : WizardActivity() {
 
     private fun renderSyncState(syncState: SyncScreenState) {
         binding.syncStateFeedback.text = "State: ${syncState::class.java.simpleName}"
+        binding.showLongTrustwords.isVisible = viewModel.shortTrustWords
         when (syncState) {
             SyncScreenState.Idle,
             SyncScreenState.AwaitingOtherDevice -> {
-                binding.waitingForSync.isVisible = true
-                binding.syncStateFeedback.setText(R.string.sync_dialog_awaiting_other_device)
+                showAwaitingOtherDevice()
             }
 
             SyncScreenState.HandshakeReadyAwaitingUser -> {
@@ -66,7 +98,7 @@ class PlanckSyncWizard : WizardActivity() {
             }
 
             is SyncScreenState.AwaitingHandshakeCompletion -> {
-                showAwaitingHandshakeCompletion()
+                showAwaitingHandshakeCompletion(syncState)
             }
 
             is SyncScreenState.Done -> {
@@ -76,61 +108,60 @@ class PlanckSyncWizard : WizardActivity() {
             SyncScreenState.Cancelled -> {
                 finish()
             }
+
             SyncScreenState.TimeoutError -> {
                 showSomethingWentWrong()
             }
         }
     }
 
+    private fun showAwaitingOtherDevice() {
+        showScreen(
+            waitingForSyncVisible = true,
+            dismissButtonVisible = true,
+            syncStateFeedbackVisible = true
+        )
+        binding.syncStateFeedback.setText(R.string.sync_dialog_awaiting_other_device)
+    }
+
     private fun showSomethingWentWrong() {
-        binding.description.setText(R.string.keysync_wizard_error_message)
-        binding.dissmissActionButton.isVisible = false
-        binding.afirmativeActionButton.isVisible = true
-        binding.afirmativeActionButton.setTextColor(ThemeManager.getColorFromAttributeResource(this, R.attr.defaultColorOnBackground))
-        binding.afirmativeActionButton.setOnClickListener {
+        showScreen(
+            description = R.string.keysync_wizard_error_message,
+            currentState = getAwaitingUserStateDrawable(),
+            positiveButtonText = R.string.key_import_accept
+        ) {
             viewModel.cancelHandshake()
             finish()
         }
-        binding.negativeActionButton.isVisible = false
-        binding.trustwordsContainer.isVisible = false
-        binding.currentState.isVisible = true
-        binding.loading.isVisible = false
     }
 
     private fun showKeySyncDone() {
-        binding.description.setText(getKeySyncDoneDescription())
-        binding.currentState.setImageResource(getKeySyncDoneStateDrawable())
-        binding.loading.isVisible = false
-        binding.dissmissActionButton.isVisible = false
-        binding.afirmativeActionButton.isVisible = true
-        binding.afirmativeActionButton.setTextColor(
-            ThemeManager.getColorFromAttributeResource(
-                this,
-                R.attr.defaultColorOnBackground
-            )
-        )
-        binding.afirmativeActionButton.setOnClickListener { finish() }
-        binding.currentState.isVisible = true
-    }
-
-    private fun showAwaitingHandshakeCompletion() {
-        invalidateOptionsMenu()
-        binding.description.setText(R.string.keysync_wizard_waiting_message)
-        binding.trustwordsContainer.isVisible = false
-        binding.loading.isVisible = true
-        binding.afirmativeActionButton.isVisible = false
-        binding.negativeActionButton.isVisible = false
-        binding.dissmissActionButton.setOnClickListener {
-            viewModel.cancelHandshake()
+        showScreen(
+            description = getKeySyncDoneDescription(),
+            currentState = getKeySyncDoneStateDrawable(),
+            positiveButtonText = R.string.key_import_accept
+        ) {
             finish()
         }
     }
 
+    private fun showAwaitingHandshakeCompletion(syncState: SyncScreenState.AwaitingHandshakeCompletion) {
+        invalidateOptionsMenu()
+        showScreen(
+            description = R.string.keysync_wizard_waiting_message,
+            ownFpr = syncState.ownFpr,
+            partnerFpr = syncState.partnerFpr,
+            loadingAnimation = getLoadingAnimationDrawable(),
+            dismissButtonVisible = true
+        )
+    }
+
     private fun showAwaitingUserToStartHandshake() {
-        binding.waitingForSync.isVisible = false
-        binding.description.setText(getAwaitingUserDescription())
-        binding.currentState.setImageResource(getAwaitingUserStateDrawable())
-        binding.afirmativeActionButton.setOnClickListener {
+        showScreen(
+            description = getAwaitingUserDescription(),
+            currentState = getAwaitingUserStateDrawable(),
+            positiveButtonText = R.string.keysync_wizard_action_next
+        ) {
             viewModel.next()
         }
     }
@@ -138,15 +169,86 @@ class PlanckSyncWizard : WizardActivity() {
     private fun showHandshake(syncState: SyncScreenState.UserHandshaking) {
         invalidateOptionsMenu()
         showLangIcon()
-        binding.description.setText(R.string.keysync_wizard_handshake_message)
-        binding.afirmativeActionButton.setText(R.string.key_import_accept)
-        binding.afirmativeActionButton.setOnClickListener { viewModel.acceptHandshake() }
-        binding.fprContainer.isVisible = true
+        showScreen(
+            description = R.string.keysync_wizard_handshake_message,
+            ownFpr = syncState.ownFpr,
+            partnerFpr = syncState.partnerFpr,
+            trustwords = syncState.trustwords,
+            negativeButtonVisible = true,
+            dismissButtonVisible = true,
+            positiveButtonText = R.string.key_import_accept
+        ) {
+            viewModel.acceptHandshake()
+        }
+    }
+
+    private fun showScreen(
+        @StringRes description: Int = NO_RESOURCE,
+        ownFpr: String = "",
+        partnerFpr: String = "",
+        trustwords: String = "",
+        @DrawableRes currentState: Int = NO_RESOURCE,
+        @DrawableRes loadingAnimation: Int = NO_RESOURCE,
+        waitingForSyncVisible: Boolean = false,
+        syncStateFeedbackVisible: Boolean = BuildConfig.DEBUG,
+        negativeButtonVisible: Boolean = false,
+        dismissButtonVisible: Boolean = false,
+        @StringRes positiveButtonText: Int = NO_RESOURCE,
+        positiveButtonClick: () -> Unit = {},
+    ) {
+        if (description == NO_RESOURCE) {
+            binding.description.isVisible = false
+        } else {
+            binding.description.isVisible = true
+            binding.description.setText(description)
+        }
+        if (ownFpr.isNotBlank()) {
+            showFprs(ownFpr, partnerFpr)
+        } else {
+            binding.fprContainer.isVisible = false
+        }
+        if (trustwords.isNotBlank()) {
+            showTrustwords(trustwords)
+        } else {
+            binding.trustwordsContainer.isVisible = false
+        }
+        if (currentState == NO_RESOURCE) {
+            binding.currentState.isVisible = false
+        } else {
+            binding.currentState.isVisible = true
+            binding.currentState.setImageResource(currentState)
+        }
+        if (loadingAnimation == NO_RESOURCE) {
+            binding.loading.isVisible = false
+        } else {
+            binding.loading.isVisible = true
+            binding.loading.indeterminateDrawable = ContextCompat.getDrawable(
+                this, getLoadingAnimationDrawable()
+            )
+        }
+        binding.waitingForSync.isVisible = waitingForSyncVisible
+        binding.syncStateFeedback.isVisible = syncStateFeedbackVisible
+        binding.negativeActionButton.isVisible = negativeButtonVisible
+        binding.dissmissActionButton.visibility =
+            if (dismissButtonVisible) View.VISIBLE else View.INVISIBLE
+        if (positiveButtonText == NO_RESOURCE) {
+            binding.afirmativeActionButton.isVisible = false
+        } else {
+            binding.afirmativeActionButton.isVisible = true
+            binding.afirmativeActionButton.setText(positiveButtonText)
+        }
+        binding.afirmativeActionButton.setOnClickListener { positiveButtonClick() }
+    }
+
+    private fun showTrustwords(trustwords: String) {
         binding.trustwordsContainer.isVisible = true
-        binding.trustwords.text = syncState.trustwords
-        binding.currentState.isVisible = false
-        binding.negativeActionButton.isVisible = true
-        binding.negativeActionButton.setOnClickListener { viewModel.rejectHandshake() }
+        binding.trustwords.text = trustwords
+    }
+
+    private fun showFprs(ownFpr: String, partnerFpr: String) {
+        binding.fprContainer.isVisible = true
+        binding.fprCurrentDeviceValue.text = ownFpr
+        binding.fprNewDeviceValue.text = partnerFpr
     }
 
     private fun showLangIcon() {
@@ -176,6 +278,12 @@ class PlanckSyncWizard : WizardActivity() {
     private fun getKeySyncDoneStateDrawable(): Int {
         return if (viewModel.formingGroup) R.drawable.ic_sync_2nd_device_synced
         else R.drawable.ic_sync_3rd_device_synced
+    }
+
+    @DrawableRes
+    private fun getLoadingAnimationDrawable(): Int {
+        return if (viewModel.formingGroup) R.drawable.add_second_device
+        else R.drawable.add_device_to_group
     }
 
     companion object {
