6e89f7c54061ac40a8e958d711c8834cc04f520f
Peter Z <pz@pep.security>
2023-08-09 13:49:54 +0200

    EFA-168 apply unique recipients only

--- k9mail/src/main/java/com/fsck/k9/activity/compose/RecipientSelectPresenter.kt
+++ k9mail/src/main/java/com/fsck/k9/activity/compose/RecipientSelectPresenter.kt
@@ -23,7 +23,7 @@ class RecipientSelectPresenter @Inject constructor(
         get() = unsecureAddresses.size
 
     val addresses: List<Address>
-        get() = recipients.map { it.address }
+        get() = recipients.map { it.address }.distinct()
 
     val recipients: List<Recipient>
         get() = view.recipients
