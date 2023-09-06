49ad2d64f6da5daece6f92c0311945031f1d6f80
ignaciogarcia <ig@pep.security>
2023-09-06 10:18:56 +0200

    EFA-248 Do not update unlocked manageable settings from MDM

--- k9mail/src/main/java/com/fsck/k9/K9.java
+++ k9mail/src/main/java/com/fsck/k9/K9.java
@@ -1920,7 +1920,7 @@ public class K9 extends MultiDexApplication {
         return planckForwardWarningEnabled;
     }
 
-    public static void setPlanckForwardWarningEnabled(ManageableSetting<Boolean> planckForwardWarningEnabled) {
+    public void setPlanckForwardWarningEnabled(ManageableSetting<Boolean> planckForwardWarningEnabled) {
         K9.planckForwardWarningEnabled = planckForwardWarningEnabled;
     }
 
