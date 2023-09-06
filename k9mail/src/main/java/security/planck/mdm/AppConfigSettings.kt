7d1de5b7b5f9427b6841d3ef2238868a22cccb75
ignaciogarcia <ig@pep.security>
2023-09-06 04:33:44 +0200

    EFA-248 Remove serializable in ManageableSettingMdmEntry
    ManageableSettingMdmEntry does not need to be serialized anymore since we are using restriction bundles instead of json strings.

--- k9mail/src/main/java/security/planck/mdm/AppConfigSettings.kt
+++ k9mail/src/main/java/security/planck/mdm/AppConfigSettings.kt
@@ -1,14 +1,9 @@
 package security.planck.mdm
 
-import com.fsck.k9.BuildConfig
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
 
-
-@Serializable
 data class ManageableSettingMdmEntry<TYPE>(
-        @SerialName("value") val value: TYPE,
-        @SerialName("locked") val locked: Boolean = BuildConfig.IS_ENTERPRISE,
+    val value: TYPE,
+    val locked: Boolean,
 ) {
     fun toManageableSetting(): ManageableSetting<TYPE> =
         ManageableSetting(value = value, locked = locked)
