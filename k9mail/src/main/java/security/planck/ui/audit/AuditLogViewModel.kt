32911f1e2a22ef7209ed86a5dbb532ef534cc914
ignaciogarcia <ig@pep.security>
2023-10-05 09:51:48 +0200

    EFA-230 Add simple MVVM pattern for warning

---
+++ k9mail/src/main/java/security/planck/ui/audit/AuditLogViewModel.kt
@@ -0,0 +1,27 @@
+package security.planck.ui.audit
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.fsck.k9.planck.infrastructure.livedata.Event
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import security.planck.audit.AuditLogger
+import javax.inject.Inject
+
+@HiltViewModel
+class AuditLogViewModel @Inject constructor(
+    auditLogger: AuditLogger,
+) : ViewModel() {
+    private val tamperAlertLD: MutableLiveData<Event<Int>> = MutableLiveData(Event(0))
+    val tamperAlert: LiveData<Event<Int>> = tamperAlertLD
+
+    init {
+        auditLogger.tamperAlertFlow
+            .onEach {
+                tamperAlertLD.value = Event(it)
+            }.launchIn(viewModelScope)
+    }
+}
