d5c34c32d9d743273d6d34de44ab993cc80bcd81
ignaciogarcia <ig@pep.security>
2023-11-10 11:44:05 +0100

    EFA-150 Refactor test from Mockito to Mockk

--- k9mail/src/test/java/com/fsck/k9/fabmenu/PlanckFabMenuPresenterIntegrationTest.kt
+++ k9mail/src/test/java/com/fsck/k9/fabmenu/PlanckFabMenuPresenterIntegrationTest.kt
@@ -2,59 +2,51 @@ package com.fsck.k9.fabmenu
 
 import com.fsck.k9.planck.ui.infrastructure.MessageAction
 import com.fsck.k9.planck.ui.listeners.OnMessageOptionsListener
-import org.mockito.kotlin.never
-import org.mockito.kotlin.times
-import org.mockito.kotlin.verify
+import io.mockk.mockk
+import io.mockk.verify
 import org.junit.Before
 import org.junit.Test
-import org.mockito.Mock
-import org.mockito.MockitoAnnotations
 import security.planck.ui.message_compose.PlanckFabMenuPresenter
 import security.planck.ui.message_compose.PlanckFabMenuView
 
 class PlanckFabMenuPresenterIntegrationTest {
-
-    @Mock
-    private lateinit var view: PlanckFabMenuView
-    @Mock
-    private lateinit var listener: OnMessageOptionsListener
+    private val view: PlanckFabMenuView = mockk(relaxed = true)
+    private val listener: OnMessageOptionsListener = mockk(relaxed = true)
 
     private lateinit var presenter: PlanckFabMenuPresenter
 
     @Before
     fun setup() {
-        MockitoAnnotations.openMocks(this)
-        presenter = PlanckFabMenuPresenter(view)
-        presenter.listener = listener
+        presenter = PlanckFabMenuPresenter(view).also { it.listener = listener }
     }
 
     @Test
     fun presenterStartShouldShowInitialState() {
         presenter.init()
-        verify(view).showInitialState()
-        verify(view, never()).closeMenu()
-        verify(view, never()).openMenu()
+        verify { view.showInitialState() }
+        verify(exactly = 0) { view.closeMenu() }
+        verify(exactly = 0) { view.openMenu() }
     }
 
     @Test
     fun onLongClickShouldOpenMenu() {
         presenter.onLongClicked()
 
-        verify(view, times(1)).openMenu()
-        verify(view, never()).closeMenu()
+        verify(exactly = 1) { view.openMenu() }
+        verify(exactly = 0) { view.closeMenu() }
     }
 
     @Test
     fun onDoubleLongClickShouldCloseMenu() {
         presenter.onLongClicked()
 
-        verify(view, times(1)).openMenu()
-        verify(view, never()).closeMenu()
+        verify(exactly = 1) { view.openMenu() }
+        verify(exactly = 0) { view.closeMenu() }
 
         presenter.onLongClicked()
 
-        verify(view, times(1)).closeMenu()
-        verify(view, times(1)).openMenu()
+        verify(exactly = 1) { view.closeMenu() }
+        verify(exactly = 1) { view.openMenu() }
     }
 
     @Test
@@ -62,45 +54,43 @@ class PlanckFabMenuPresenterIntegrationTest {
         presenter.onLongClicked()
         presenter.onMainActionClicked()
 
-        verify(view, times(1)).openMenu()
-        verify(view, times(1)).closeMenu()
+        verify(exactly = 1) { view.openMenu() }
+        verify(exactly = 1) { view.closeMenu() }
     }
 
     @Test
     fun onClickMainActionShouldReplyAction() {
         presenter.onMainActionClicked()
 
-        verify(listener, times(1)).OnMessageOptionsListener(MessageAction.REPLY)
-        verify(view, never()).closeMenu()
-        verify(view, never()).openMenu()
+        verify(exactly = 1) { listener.OnMessageOptionsListener(MessageAction.REPLY) }
+        verify(exactly = 0) { view.closeMenu() }
+        verify(exactly = 0) { view.openMenu() }
     }
 
     @Test
     fun onReplyActionShouldReplyAction() {
         presenter.onReplyClicked()
 
-        verify(listener, times(1)).OnMessageOptionsListener(MessageAction.REPLY)
-        verify(view, never()).closeMenu()
-        verify(view, never()).openMenu()
+        verify(exactly = 1) { listener.OnMessageOptionsListener(MessageAction.REPLY) }
+        verify(exactly = 0) { view.closeMenu() }
+        verify(exactly = 0) { view.openMenu() }
     }
 
     @Test
     fun onReplyAllActionShouldReplyAllAction() {
         presenter.onReplyAllClicked()
 
-        verify(listener, times(1)).OnMessageOptionsListener(MessageAction.REPLY_ALL)
-        verify(view, never()).closeMenu()
-        verify(view, never()).openMenu()
+        verify(exactly = 1) { listener.OnMessageOptionsListener(MessageAction.REPLY_ALL) }
+        verify(exactly = 0) { view.closeMenu() }
+        verify(exactly = 0) { view.openMenu() }
     }
 
     @Test
     fun onForwardActionShouldForwardAction() {
         presenter.onForwardClicked()
 
-        verify(listener, times(1)).OnMessageOptionsListener(MessageAction.FORWARD)
-        verify(view, never()).closeMenu()
-        verify(view, never()).openMenu()
+        verify(exactly = 1) { listener.OnMessageOptionsListener(MessageAction.FORWARD) }
+        verify(exactly = 0) { view.closeMenu() }
+        verify(exactly = 0) { view.openMenu() }
     }
-
-
 }
