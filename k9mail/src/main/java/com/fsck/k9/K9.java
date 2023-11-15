8823fb11868f4354eae1c16e21033f0df2891d2a
ignaciogarcia <ig@pep.security>
2023-11-15 15:13:46 +0100

    EFA-420 Only allow to start or stop sync if value changed

--- k9mail/src/main/java/com/fsck/k9/K9.java
+++ k9mail/src/main/java/com/fsck/k9/K9.java
@@ -1921,8 +1921,9 @@ public class K9 extends MultiDexApplication implements DefaultLifecycleObserver 
     }
 
     public void setPlanckSyncEnabled(ManageableSetting<Boolean> planckSyncEnabled) {
+        boolean changed = K9.planckSyncEnabled.getValue() != planckSyncEnabled.getValue();
         K9.planckSyncEnabled = planckSyncEnabled;
-        if (planckProvider != null) {
+        if (planckProvider != null && changed) {
             setPlanckSyncEnabledInRepository(planckSyncEnabled.getValue());
         }
     }
@@ -1933,8 +1934,10 @@ public class K9 extends MultiDexApplication implements DefaultLifecycleObserver 
 
 
     public void setPlanckSyncEnabled(boolean enabled) {
-        planckSyncEnabled.setValue(enabled);
-        setPlanckSyncEnabledInRepository(enabled);
+        if (enabled != planckSyncEnabled.getValue()) {
+            planckSyncEnabled.setValue(enabled);
+            setPlanckSyncEnabledInRepository(enabled);
+        }
     }
 
     private void setPlanckSyncEnabledInRepository(boolean enabled) {
