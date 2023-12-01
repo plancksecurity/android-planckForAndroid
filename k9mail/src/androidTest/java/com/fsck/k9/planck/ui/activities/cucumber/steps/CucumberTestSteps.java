409dd868af56f96bb88940242831c7fc083fdc38
jgb <jgb@pep.security>
2023-12-01 17:50:24 +0100

    Small modifications in CucumberTestSteps - Use new bots in all the cases - Check text has been copied - Use new URL in calendar test - Add some waits for idle - "Discard the message" now checks the title - Navigation menu retries to go to a folder in case it fails - SwipeDown the folder list to move/copy a email

--- k9mail/src/androidTest/java/com/fsck/k9/planck/ui/activities/cucumber/steps/CucumberTestSteps.java
+++ k9mail/src/androidTest/java/com/fsck/k9/planck/ui/activities/cucumber/steps/CucumberTestSteps.java
@@ -2368,13 +2368,14 @@ public class CucumberTestSteps {
                     folderID = R.string.special_mailbox_name_archive;
                     break;
 
-        }
-        testUtils.openHamburgerMenu();
-        if (folder.equals("Suspicious")||folder.equals("suspicious")) {
-            testUtils.scrollUpNavigation();
-            testUtils.selectFromScreen("Suspicious");
-        } else {
-            testUtils.selectFromScreen(folderID);
+            }
+            testUtils.openHamburgerMenu();
+            if (folder.equals("Suspicious") || folder.equals("suspicious")) {
+                testUtils.scrollUpNavigation();
+                testUtils.selectFromScreen("Suspicious");
+            } else {
+                testUtils.selectFromScreen(folderID);
+            }
         }
     }
 
