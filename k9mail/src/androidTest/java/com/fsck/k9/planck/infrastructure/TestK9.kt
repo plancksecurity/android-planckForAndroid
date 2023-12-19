837d4a3aa5ce53c30c49bda7056628ec2bd9d886
jgb <jgb@pep.security>
2023-12-19 12:15:19 +0100

    Update config file for MDM - Use new bots in all the cases - Check text has been copied - Use new URL in calendar test - Add some waits for idle - "Discard the message" now checks the title - Navigation menu retries to go to a folder in case it fails - SwipeDown the folder list to move/copy a email

---
+++ k9mail/src/androidTest/java/com/fsck/k9/planck/infrastructure/TestK9.kt
@@ -0,0 +1,14 @@
+package com.fsck.k9.planck.infrastructure
+/*
+import com.fsck.k9.K9
+import com.fsck.k9.planck.infrastructure.components.ApplicationComponent
+import com.fsck.k9.planck.infrastructure.components.DaggerTestApplicationComponent
+
+class TestK9 : K9() {
+    override fun createApplicationComponent(): ApplicationComponent {
+        return DaggerTestApplicationComponent
+            .factory()
+            .create(this)
+    }
+}
+*/
