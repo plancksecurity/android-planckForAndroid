19eacf4f9dd9f8a3abd5e792f5ee1a372ed80680
ignaciogarcia <ig@pep.security>
2024-01-04 13:06:11 +0100

    EFA-491 Remove runBlocking from deviceGrouped

--- k9mail/src/main/java/security/planck/ui/leavedevicegroup/LeaveDeviceGroupViewModel.kt
+++ k9mail/src/main/java/security/planck/ui/leavedevicegroup/LeaveDeviceGroupViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.fsck.k9.BuildConfig
 import com.fsck.k9.planck.PlanckProvider
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.delay
@@ -14,7 +15,7 @@ import timber.log.Timber
 import javax.inject.Inject
 
 private const val DEVICE_GROUPED_CHECK_ITERATIONS = 5
-private const val DEVICE_GROUPED_CHECK_INTERVAL = 100L
+private const val DEVICE_GROUPED_CHECK_INTERVAL = 200L
 
 /**
  * LeaveDeviceGroupViewModel
@@ -47,14 +48,19 @@ class LeaveDeviceGroupViewModel @Inject constructor(
             stateLD.value = BackgroundTaskDialogView.State.LOADING
             planckProvider.leaveDeviceGroup().onSuccessSuspend {
                 repeat(DEVICE_GROUPED_CHECK_ITERATIONS) {
-                    if (!planckProvider.isDeviceGrouped) {
+                    if (!planckProvider.isDeviceGrouped()) {
                         return@repeat
                     }
                     Timber.e("planckProvider.isDeviceGrouped returned true, trying again...")
                     delay(DEVICE_GROUPED_CHECK_INTERVAL)
                 }
                 syncRepository.isGrouped = false
-                stateLD.value = BackgroundTaskDialogView.State.SUCCESS
+                stateLD.value =
+                    if (BuildConfig.DEBUG && planckProvider.isDeviceGrouped()) {
+                        Timber.e("CANNOT LEAVE DEVICE GROUP!!!")
+                        BackgroundTaskDialogView.State.ERROR
+                    } else
+                        BackgroundTaskDialogView.State.SUCCESS
             }.onFailure {
                 Timber.e(it)
                 stateLD.value = BackgroundTaskDialogView.State.ERROR
