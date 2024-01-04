3c774f5a076c05599e7c8ae556705be0b1b6bb6f
ignaciogarcia <ig@pep.security>
2024-01-04 09:34:49 +0100

    EFA-491 Make sure the closeable reference is freed

--- k9mail/src/main/java/com/fsck/k9/planck/infrastructure/threading/AutoCloseableThread.kt
+++ k9mail/src/main/java/com/fsck/k9/planck/infrastructure/threading/AutoCloseableThread.kt
@@ -1,12 +1,13 @@
 package com.fsck.k9.planck.infrastructure.threading
 
 open class AutoCloseableThread<T : AutoCloseable>(
-    private val closeable: T,
+    private var closeable: T?,
     target: Runnable?
 ) : Thread(target) {
     override fun run() {
         closeable.use {
             super.run()
         }
+        closeable = null
     }
 }
