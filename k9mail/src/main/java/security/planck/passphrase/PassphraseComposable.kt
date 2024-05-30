eda72d1c21046db87eae5463176737c51e8d7c24
ignaciogarcia <ig@pep.security>
2024-05-30 12:35:47 +0200

    EFA-601 Save state across config changes

--- k9mail/src/main/java/security/planck/passphrase/PassphraseComposable.kt
+++ k9mail/src/main/java/security/planck/passphrase/PassphraseComposable.kt
@@ -37,6 +37,7 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
@@ -123,7 +124,7 @@ fun PassphraseManagementDialogContent(
             }
 
             is PassphraseMgmtState.UnlockingPassphrases -> {
-                val passwordStates = remember {
+                val passwordStates = rememberSaveable {
                     state.accountsUsingPassphrase.map { account ->
                         TextFieldState( isError = state.accountUnlockErrors.contains(account.email)) // initial errors for retries
                     }
