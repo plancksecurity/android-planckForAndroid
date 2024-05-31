fc1feabd9e555069893c90979c4017f6cbbffce8
ignaciogarcia <ig@pep.security>
2024-05-31 15:17:02 +0200

    EFA-601 Cleanup in PassphraseRepository

--- k9mail/src/main/java/security/planck/passphrase/PassphraseRepository.kt
+++ k9mail/src/main/java/security/planck/passphrase/PassphraseRepository.kt
@@ -2,12 +2,11 @@ package security.planck.passphrase
 
 import android.util.Log
 import com.fsck.k9.Account
+import com.fsck.k9.BuildConfig
 import com.fsck.k9.K9
 import com.fsck.k9.Preferences
-import com.fsck.k9.planck.DispatcherProvider
 import com.fsck.k9.planck.PlanckProvider
 import kotlinx.coroutines.runBlocking
-import timber.log.Timber
 import javax.inject.Inject
 import javax.inject.Provider
 import javax.inject.Singleton
@@ -17,21 +16,16 @@ class PassphraseRepository @Inject constructor(
     private val planckProvider: Provider<PlanckProvider>,
     private val preferences: Preferences,
     private val k9: K9,
-    private val dispatcherProvider: DispatcherProvider,
 ) {
-    //private val passphraseUnlockNeededMF: MutableStateFlow<Boolean> = MutableStateFlow(false)
-
     fun initializeBlocking() {
         runBlocking {
             getAccountsWithPassPhrase().onFailure {
-                Log.e("EFA-601", "EFA-601 FAILURE: ${it.stackTraceToString()}")
+                if (BuildConfig.DEBUG) {
+                    Log.e("PASSPHRASE", "LOAD FAILURE: ${it.stackTraceToString()}")
+                }
             }.onSuccess { list ->
                 if (list.isEmpty()) {
-                    Log.e("EFA-601", "EFA-601 NO PASSPHRASE NEEDED, UNLOCKING....")
                     unlockPassphrase()
-                } else {
-                    Log.e("EFA-601", "EFA-601 PASSPHRASE REQUIRED, LOCKED")
-                    //timeToStartOrRetryMF.value = PassphraseUnlockNeeded(start = true)
                 }
             }
         }
@@ -62,14 +56,5 @@ class PassphraseRepository @Inject constructor(
         @JvmStatic
         var passphraseUnlocked = false
             private set
-    }
-}
-
-data class PassphraseUnlockNeeded(
-    val failedAttempts: Int = 0,
-    val start: Boolean = false,
-) {
-    companion object {
-        val noError = PassphraseUnlockNeeded(0,)
     }
 }
